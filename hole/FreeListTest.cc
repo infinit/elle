8c5c0bc8baff7609f0bdfe4ca40da87d41f00a7f
Alexandre Bique <bique.alexandre@gmail.com>
2009-07-22 00:04:46 +0000

    Updated test against the FreeList template

--- hole/FreeListTest.cc
+++ hole/FreeListTest.cc
@@ -11,6 +11,12 @@ public:
     puts(__PRETTY_FUNCTION__);
   }
 
+  Chiche(const Chiche &)
+    : hole::FreeList<Chiche>()
+  {
+    puts(__PRETTY_FUNCTION__);
+  }
+
   ~Chiche()
   {
     puts(__PRETTY_FUNCTION__);
@@ -22,7 +28,7 @@ int main()
   Chiche * c1 = new Chiche;
   delete c1;
   c1 = new Chiche;
-  Chiche * c2 = new Chiche;
+  Chiche * c2 = new Chiche(*c1);
   delete c1;
   delete c2;
   return 0;
