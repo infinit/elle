35029c1e5fd1879791ec67c6d3234857177ce0a9
Alexandre Bique <alexandre.bique@citrix.com>
2009-07-23 18:51:09 +0100

    Added some code to allow to create a network or join an existing one.

--- hole/main.cc
+++ hole/main.cc
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <QCoreApplication>
 
 #include "hole/DHT.hh"
@@ -7,6 +8,21 @@ int main(int argc, char **argv)
   QCoreApplication app(argc, argv);
   hole::DHT dht;
 
-  dht.Port(4242);
+  /* Checking for specified local port */
+  char * HOLE_PORT = getenv("HOLE_PORT");
+  if (HOLE_PORT)
+    dht.Port(strtol(HOLE_PORT, 0, 0));
+  else
+    dht.Port(0);
+
+  /* Do we join a network ? */
+  char * HOLE_PEER_ADDR = getenv("HOLE_PEER_ADDR");
+  char * HOLE_PEER_PORT = getenv("HOLE_PEER_PORT");
+  if (HOLE_PEER_ADDR && HOLE_PEER_PORT)
+    dht.Join(QHostAddress(HOLE_PEER_ADDR),
+             strtol(HOLE_PEER_PORT, 0, 0));
+  else
+    dht.Create();
+
   return app.exec();
 }
