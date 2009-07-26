7f29fdcde8f78c007013b96cf5122cfaaaf6b536
Julien Quintard <julien.quintard@gmail.com>
2009-07-26 15:35:11 +0100

    Added a simple Andrew Benchmark script which is most common benchmark when it comes to file systems.

---
+++ benchmarks/andrew-benchmark.sh
@@ -0,0 +1,69 @@
+#! /bin/bash
+
+#
+# andrew benchmark
+#
+
+Prepare()
+{
+  echo "---[ Prepare"
+
+  directories=$(find "${from}" -type d)
+
+  time								\
+    (for directory in ${directories} ; do
+      mkdir "${to}/${directory}"
+    done) >andrew-benchmark.log
+}
+
+Copy()
+{
+  echo "---[ Copy"
+
+  files=$(find "${from}" -type f)
+
+  time								\
+    (for file in ${files} ; do
+      cp "${file}" "${to}/${file}"
+    done) >andrew-benchmark.log
+}
+
+List()
+{
+  echo "---[ List"
+
+  time								\
+    (ls -Rla "${to}") >andrew-benchmark.log
+}
+
+Search()
+{
+  echo "---[ Search"
+
+  time								\
+    (grep -R "teton" "${to}") >andrew-benchmark.log
+}
+
+Compile()
+{
+  echo "---[ Compile"
+
+  cd "${to}/$(basename ${from})/"
+
+  time								\
+    (./config && sed -ri "s/^AR=(.*)$/AR=echo \1/" Makefile && sed -ri "s/^RANLIB=(.*)$/RANLIB=echo \1/" Makefile && make) >andrew-benchmark.log
+}
+
+if [ ${#} -ne 2 ] ; then
+  echo "[usage] andrew-benchmark.sh {from} {to}"
+  exit 0
+fi
+
+from=${1}
+to=${2}
+
+Prepare
+Copy
+List
+Search
+Compile
