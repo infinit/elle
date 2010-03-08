99ad3ec63f0af5aef64b9b364a40c50f5678461f
Julien Quintard <julien.quintard@gmail.com>
2010-03-08 23:10:21 +0000

    o Temporary modifications...

---
+++ Plain.hh
@@ -0,0 +1,43 @@
+//
+// ---------- header ----------------------------------------------------------
+//
+// project       elle
+//
+// license       infinit (c)
+//
+// file          /home/mycure/infinit/elle/cryptography/Plain.hh
+//
+// created       julien quintard   [wed feb 11 12:39:47 2009]
+// updated       julien quintard   [sat mar  7 22:03:03 2009]
+//
+
+#ifndef ELLE_CRYPTO_PLAIN_HH
+#define ELLE_CRYPTO_PLAIN_HH
+
+//
+// ---------- includes --------------------------------------------------------
+//
+
+#include <elle/misc/Region.hh>
+
+namespace elle
+{
+  using namespace misc;
+
+  namespace cryptography
+  {
+
+//
+// ---------- types -----------------------------------------------------------
+//
+
+    ///
+    /// this class represents plain data that can be used for cryptographic
+    /// purposes.
+    ///
+    typedef Region	Plain;
+
+  }
+}
+
+#endif
