eca8827d30634085a7f2e5d29877bcc943f338a7
mefyl <mefyl@gruntech.org>
2011-03-11 14:06:21 +0100

    Add source tree in the path before launching doctest.

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python
 
+import os
+import os.path
+import sys
+
+myself = os.getcwd()
+dir = os.path.dirname(__file__)
+if dir:
+  myself += '/%s' % dir
+sys.path = [myself + '/../src'] + sys.path
+
 import doctest
 import drake
 import drake.utils
 
 doctest.testmod(drake)
 doctest.testmod(drake.utils)
+
