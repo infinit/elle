7389f9ef0c09998efecce8a4849003d1481735b5
Julien Quintard <julien.quintard@gmail.com>
2011-06-01 10:05:00 +0200

    Added a version string.

--- Infinit.cc
+++ Infinit.cc
@@ -8,7 +8,7 @@
 // file          /home/mycure/infinit/Infinit.cc
 //
 // created       julien quintard   [tue may  4 22:01:45 2010]
-// updated       julien quintard   [tue apr 26 14:08:06 2011]
+// updated       julien quintard   [wed jun  1 10:00:25 2011]
 //
 
 //
@@ -27,3 +27,8 @@
 /// populating the Infinit system.
 ///
 const elle::String		Infinit::Authority("RUxMAQ0AAAAAAgAAsKKVNXszNo9F2vug8A-TcYCn4mVUeZorx5vGN6OACFSh18COd1e8+Iu0j-0+Ja0e52b9qKeEQ3UHZUJ1+1bKxmOsQTvUOLFkgBXbmwnOtqg1KDNpFaHqtzAgVN3uQEcYKlBWnA-26otBZTgTxXMOMgLWog+s0wqhTwhiZ1YkUygy+Ld8ljvOOccyVxHyLOxhx8NKybnuPvBKz6JvSEUvmk4tLLyx82japPu0ck3HQCRQGaIliAnFWG70M02ME8+80RO3zMkkmxhQiI5z8GIo0uvWwXlA7Cv4DEh9ej74XvbyXQ0iaO0Evo1zG79xoCJ8XNdZeyL+DmHFf65D8mLxq9GL8lAipImuBrJVcueNrKNqIDCwvpnJM9pLQ0ZKKTiumjPQH9rLTVTzKxONPlHXdVw-okniAmFtCZTpo1sYF4q-Tnb0yLOfL5YVq+y8GNGUp-6P9hoDhLUTU5CoWyrV1UznK4PhT-TtUkqi8KfnrNY8jZBYhmD2ChUa+7YCBvCgRu4VIcVynUvHCPYKAVhgCH9DAWkGkZbPMv5uB7HGGxQVtmv2JHkZKtK+BxvkTY2roLGm9sJPozwQblfPD24G-ASgHmyakkvINR8S4j1Di6VTpErw4IUKe9IXRHOWsFx2v4Wh9CUQjmE0cu4jRvHYUvSMKXvKYP+jMs7UdCtmk2kNAAAAAwAAAAEAAQ==");
+
+///
+/// this constant contains the version string.
+///
+const elle::String		Infinit::Version("Infinit[alpha]");
--- Infinit.hh
+++ Infinit.hh
@@ -8,7 +8,7 @@
 // file          /home/mycure/infinit/Infinit.hh
 //
 // created       julien quintard   [tue may  4 22:00:43 2010]
-// updated       julien quintard   [fri mar  4 11:55:04 2011]
+// updated       julien quintard   [thu may 26 15:05:20 2011]
 //
 
 #ifndef INFINIT_HH
@@ -32,11 +32,7 @@ public:
   //
   static const elle::String		Authority;
 
-  //
-  // types
-  //
-  typedef elle::Natural64		User;
-  typedef elle::Natural64		Network;
+  static const elle::String		Version;
 };
 
 #endif
