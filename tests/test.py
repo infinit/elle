ff40dbcb312bf5cb901edf118fba18c71cd57722
mefyl <mefyl@gruntech.org>
2011-07-08 10:33:29 +0200

    Handle the test process entierly in the repository.

--- tests/test.py
+++ tests/test.py
@@ -9,6 +9,7 @@ dir = os.path.dirname(__file__)
 if dir:
   myself += '/%s' % dir
 sys.path = [myself + '/../src'] + sys.path
+os.environ['PYTHONPATH'] = ':'.join(sys.path)
 
 import doctest
 import drake
@@ -20,3 +21,7 @@ doctest.testmod(drake)
 doctest.testmod(drake.git)
 doctest.testmod(drake.python)
 doctest.testmod(drake.utils)
+
+assert os.system('src/drake/sched.py') == 0
+os.chdir('_build')
+assert os.system('./drake-build.py //check') == 0
