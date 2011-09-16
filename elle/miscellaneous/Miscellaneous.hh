67be04eaab9796082c1ff0fd3ee87f747614554e
Julien Quintard <julien.quintard@gmail.com>
2011-09-16 10:27:08 +0200

    Added the bpt.h header.

---
+++ elle/miscellaneous/Miscellaneous.hh
@@ -0,0 +1,30 @@
+//
+// ---------- header ----------------------------------------------------------
+//
+// project       elle
+//
+// license       infinit
+//
+// author        julien quintard   [thu sep 15 15:15:52 2011]
+//
+
+#ifndef ELLE_MISCELLANEOUS_MISCELLANEOUS_HH
+#define ELLE_MISCELLANEOUS_MISCELLANEOUS_HH
+
+namespace elle
+{
+  namespace miscellaneous
+  {
+
+    // nothing to do
+
+  }
+}
+
+//
+// ---------- includes --------------------------------------------------------
+//
+
+#include <elle/miscellaneous/bpt.h>
+
+#endif
