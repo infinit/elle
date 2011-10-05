d58632ae9fa946c984bcd351671d82136fb55c71
Alexandre Bique <bique.alexandre@gmail.com>
2011-10-05 13:11:46 +0200

    Added a test for Way()

---
+++ etoile/tests/way-slice.cc
@@ -0,0 +1,19 @@
+//
+// ---------- header ----------------------------------------------------------
+//
+// project       etoile
+//
+// license       infinit
+//
+// author        Alexandre Bique   [wed oct  5 13:03:06 2011]
+//
+
+#include <etoile/Etoile.hh>
+
+int main(int argc, char ** argv)
+{
+  etoile::path::Slab		name;
+  etoile::path::Way		way(etoile::path::Way("/"), name);
+
+  return 0;
+}
