a417ec69596bef36d0367fa646ac9c612feb919a
Julien Quintard <julien.quintard@gmail.com>
2011-11-24 18:38:25 +0100

    Added a prototype-specific resource.

---
+++ oracle/disciples/meta/resources/prototype.py
@@ -0,0 +1,40 @@
+#
+# ---------- imports ----------------------------------------------------------
+#
+
+import web
+import json
+
+import database
+
+#
+# ---------- classes ----------------------------------------------------------
+#
+
+class Prototype:
+  def GET(self, name):
+    devices = []
+
+    for device in database.devices.find({'name': name}):
+      devices += [ device['locus'] ]
+
+    return json.dumps(devices)
+
+  def POST(self, name):
+    return json.dumps(None)
+
+  def PUT(self, name):
+    data = json.loads(web.data())
+
+    for locus in data["loci"]:
+      if database.devices.find_one({'name': name,
+				    'locus': locus}):
+	continue
+
+      database.devices.insert({'name': name,
+			       'locus': locus})
+
+    return json.dumps(None)
+
+  def DELETE(self, name):
+    return json.dumps(None)
