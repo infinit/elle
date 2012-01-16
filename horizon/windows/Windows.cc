bf5984c78e5f014095be1e9e281e3d430dce34a8
Julien Quintard <julien.quintard@gmail.com>
2012-01-16 11:02:15 +0100

    Added a windows skeleton for it to compile.

---
+++ horizon/windows/Windows.cc
@@ -0,0 +1,74 @@
+//
+// ---------- header ----------------------------------------------------------
+//
+// project       facade
+//
+// license       infinit
+//
+// author        julien quintard   [tue may 31 10:31:35 2011]
+//
+
+//
+// ---------- includes --------------------------------------------------------
+//
+
+#include <facade/windows/Windows.hh>
+#include <facade/Facade.hh>
+
+#include <elle/Elle.hh>
+#include <agent/Agent.hh>
+#include <Infinit.hh>
+
+namespace facade
+{
+  namespace windows
+  {
+
+//
+// ---------- definitions -----------------------------------------------------
+//
+
+    // XXX
+
+//
+// ---------- methods ---------------------------------------------------------
+//
+
+    ///
+    /// this method initializes windows.
+    ///
+    elle::Status	Windows::Initialize()
+    {
+      enter();
+
+      // XXX
+
+      leave();
+    }
+
+    ///
+    /// this method sets up the windows implementation.
+    ///
+    elle::Status	Windows::Setup()
+    {
+      enter();
+
+      // XXX
+
+      leave();
+    }
+
+    ///
+    /// this method cleans windows.
+    ///
+    elle::Status	Windows::Clean()
+    {
+      enter();
+
+      // XXX
+
+      leave();
+    }
+
+  }
+}
---
+++ horizon/windows/Windows.hh
@@ -0,0 +1,65 @@
+//
+// ---------- header ----------------------------------------------------------
+//
+// project       facade
+//
+// license       infinit
+//
+// author        julien quintard   [tue may 31 10:32:37 2011]
+//
+
+#ifndef FACADE_WINDOWS_WINDOWS_HH
+#define FACADE_WINDOWS_WINDOWS_HH
+
+//
+// ---------- includes --------------------------------------------------------
+//
+
+#include <Infinit.hh>
+#include <elle/Elle.hh>
+#include <nucleus/Nucleus.hh>
+#include <lune/Lune.hh>
+
+namespace facade
+{
+  ///
+  /// this namespace contains all the elements related to the windows facade
+  /// i.e interface between the kernel and Infinit.
+  ///
+  namespace windows
+  {
+
+//
+// ---------- classes ---------------------------------------------------------
+//
+
+    ///
+    /// this class contains the general-purpose information related to the
+    /// windows facade.
+    ///
+    class Windows
+    {
+    public:
+      //
+      // static methods
+      //
+      static elle::Status	Initialize();
+      static elle::Status	Setup();
+      static elle::Status	Clean();
+
+      //
+      // static attributes
+      //
+      // XXX
+    };
+
+  }
+}
+
+//
+// ---------- includes --------------------------------------------------------
+//
+
+// XXX
+
+#endif
