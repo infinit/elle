2a14822d3d0b72f50079fa23bdf6431e9e523091
Raphaël Londeix <raphael.londeix@gmail.com>
2012-01-26 17:37:42 +0100

    des espaces

--- comet/RAND.cc
+++ comet/RAND.cc
@@ -35,17 +35,17 @@ namespace comet
 #define MD_Init(a)              EVP_DigestInit_ex(a,EVP_sha1(), NULL)
 #define MD(a,b,c)               EVP_Digest(a,b,c,NULL,EVP_sha1(), NULL)
 
-#define ENTROPY_NEEDED		32  /* require 256 bits = 32 bytes of randomness */
+#define ENTROPY_NEEDED          32  /* require 256 bits = 32 bytes of randomness */
 
-#define STATE_SIZE		1023
+#define STATE_SIZE              1023
 
 //
 // ---------- definitions -----------------------------------------------------
 //
 
   static unsigned int crypto_lock_rand = 0; /* may be set only when a thread
-					     * holds CRYPTO_LOCK_RAND
-					     * (to prevent double locking) */
+                                             * holds CRYPTO_LOCK_RAND
+                                             * (to prevent double locking) */
   /* access to lockin_thread is synchronized by CRYPTO_LOCK_RAND2 */
   static CRYPTO_THREADID locking_threadid; /* valid iff crypto_lock_rand is set */
 
@@ -83,17 +83,17 @@ namespace comet
 
       int n = STATE_SIZE; /* so that the complete pool gets accessed */
       while (n > 0)
-	{
+        {
 #if MD_DIGEST_LENGTH > 20
 # error "Please adjust DUMMY_SEED."
 #endif
 #define DUMMY_SEED "...................." /* at least MD_DIGEST_LENGTH */
-	  /* Note that the seed does not matter, it's just that
-	   * ssleay_rand_add expects to have something to hash. */
-	  /// the following line is a fix replacing ssleay_rand_add()
-	  RAND_add(DUMMY_SEED, MD_DIGEST_LENGTH, 0.0);
-	  n -= MD_DIGEST_LENGTH;
-	}
+          /* Note that the seed does not matter, it's just that
+           * ssleay_rand_add expects to have something to hash. */
+          /// the following line is a fix replacing ssleay_rand_add()
+          RAND_add(DUMMY_SEED, MD_DIGEST_LENGTH, 0.0);
+          n -= MD_DIGEST_LENGTH;
+        }
     }
   }
 
@@ -123,11 +123,11 @@ namespace comet
     /* check if we already have the lock */
     if (crypto_lock_rand)
       {
-	CRYPTO_THREADID cur;
-	CRYPTO_THREADID_current(&cur);
-	CRYPTO_r_lock(CRYPTO_LOCK_RAND2);
-	do_not_lock = !CRYPTO_THREADID_cmp(&locking_threadid, &cur);
-	CRYPTO_r_unlock(CRYPTO_LOCK_RAND2);
+        CRYPTO_THREADID cur;
+        CRYPTO_THREADID_current(&cur);
+        CRYPTO_r_lock(CRYPTO_LOCK_RAND2);
+        do_not_lock = !CRYPTO_THREADID_cmp(&locking_threadid, &cur);
+        CRYPTO_r_unlock(CRYPTO_LOCK_RAND2);
       }
     else
       do_not_lock = 0;
@@ -148,13 +148,13 @@ namespace comet
     state_index += num;
     if (state_index >= STATE_SIZE)
       {
-	state_index%=STATE_SIZE;
-	state_num=STATE_SIZE;
+        state_index%=STATE_SIZE;
+        state_num=STATE_SIZE;
       }
     else if (state_num < STATE_SIZE)
       {
-	if (state_index > state_num)
-	  state_num=state_index;
+        if (state_index > state_num)
+          state_num=state_index;
       }
     /* state_index <= state_num <= STATE_SIZE */
 
@@ -169,49 +169,49 @@ namespace comet
     EVP_MD_CTX_init(&m);
     for (i=0; i<num; i+=MD_DIGEST_LENGTH)
       {
-	j=(num-i);
-	j=(j > MD_DIGEST_LENGTH)?MD_DIGEST_LENGTH:j;
+        j=(num-i);
+        j=(j > MD_DIGEST_LENGTH)?MD_DIGEST_LENGTH:j;
 
-	MD_Init(&m);
-	MD_Update(&m,local_md,MD_DIGEST_LENGTH);
-	k=(st_idx+j)-STATE_SIZE;
-	if (k > 0)
-	  {
-	    MD_Update(&m,&(state[st_idx]),j-k);
-	    MD_Update(&m,&(state[0]),k);
-	  }
-	else
-	  MD_Update(&m,&(state[st_idx]),j);
+        MD_Init(&m);
+        MD_Update(&m,local_md,MD_DIGEST_LENGTH);
+        k=(st_idx+j)-STATE_SIZE;
+        if (k > 0)
+          {
+            MD_Update(&m,&(state[st_idx]),j-k);
+            MD_Update(&m,&(state[0]),k);
+          }
+        else
+          MD_Update(&m,&(state[st_idx]),j);
 
-	/* DO NOT REMOVE THE FOLLOWING CALL TO MD_Update()! */
-	MD_Update(&m,buf,j);
-	/* We know that line may cause programs such as
-	   purify and valgrind to complain about use of
-	   uninitialized data.  The problem is not, it's
-	   with the caller.  Removing that line will make
-	   sure you get really bad randomness and thereby
-	   other problems such as very insecure keys. */
+        /* DO NOT REMOVE THE FOLLOWING CALL TO MD_Update()! */
+        MD_Update(&m,buf,j);
+        /* We know that line may cause programs such as
+           purify and valgrind to complain about use of
+           uninitialized data.  The problem is not, it's
+           with the caller.  Removing that line will make
+           sure you get really bad randomness and thereby
+           other problems such as very insecure keys. */
 
-	MD_Update(&m,(unsigned char *)&(md_c[0]),sizeof(md_c));
-	MD_Final(&m,local_md);
-	md_c[1]++;
+        MD_Update(&m,(unsigned char *)&(md_c[0]),sizeof(md_c));
+        MD_Final(&m,local_md);
+        md_c[1]++;
 
-	buf=(const char *)buf + j;
+        buf=(const char *)buf + j;
 
-	for (k=0; k<j; k++)
-	  {
-	    /* Parallel threads may interfere with this,
-	     * but always each byte of the new state is
-	     * the XOR of some previous value of its
-	     * and local_md (itermediate values may be lost).
-	     * Alway using locking could hurt performance more
-	     * than necessary given that conflicts occur only
-	     * when the total seeding is longer than the random
-	     * state. */
-	    state[st_idx++]^=local_md[k];
-	    if (st_idx >= STATE_SIZE)
-	      st_idx=0;
-	  }
+        for (k=0; k<j; k++)
+          {
+            /* Parallel threads may interfere with this,
+             * but always each byte of the new state is
+             * the XOR of some previous value of its
+             * and local_md (itermediate values may be lost).
+             * Alway using locking could hurt performance more
+             * than necessary given that conflicts occur only
+             * when the total seeding is longer than the random
+             * state. */
+            state[st_idx++]^=local_md[k];
+            if (st_idx >= STATE_SIZE)
+              st_idx=0;
+          }
       }
     EVP_MD_CTX_cleanup(&m);
 
@@ -222,7 +222,7 @@ namespace comet
      * much entropy as fits into md. */
     for (k = 0; k < (int)sizeof(md); k++)
       {
-	md[k] ^= local_md[k];
+        md[k] ^= local_md[k];
       }
     if (entropy < ENTROPY_NEEDED) /* stop counting when we have enough */
       entropy += add;
@@ -283,29 +283,29 @@ namespace comet
 
     if (!initialized)
       {
-	/// the call to RAND_poll() has been removed here because
-	/// injecting unwanted entropy.
-	initialized = 1;
+        /// the call to RAND_poll() has been removed here because
+        /// injecting unwanted entropy.
+        initialized = 1;
       }
 
     ok = (entropy >= ENTROPY_NEEDED);
     if (!ok)
       {
-	/* If the PRNG state is not yet unpredictable, then seeing
-	 * the PRNG output may help attackers to determine the new
-	 * state; thus we have to decrease the entropy estimate.
-	 * Once we've had enough initial seeding we don't bother to
-	 * adjust the entropy count, though, because we're not ambitious
-	 * to provide *information-theoretic* randomness.
-	 *
-	 * NOTE: This approach fails if the program forks before
-	 * we have enough entropy. Entropy should be collected
-	 * in a separate input pool and be transferred to the
-	 * output pool only when the entropy limit has been reached.
-	 */
-	entropy -= num;
-	if (entropy < 0)
-	  entropy = 0;
+        /* If the PRNG state is not yet unpredictable, then seeing
+         * the PRNG output may help attackers to determine the new
+         * state; thus we have to decrease the entropy estimate.
+         * Once we've had enough initial seeding we don't bother to
+         * adjust the entropy count, though, because we're not ambitious
+         * to provide *information-theoretic* randomness.
+         *
+         * NOTE: This approach fails if the program forks before
+         * we have enough entropy. Entropy should be collected
+         * in a separate input pool and be transferred to the
+         * output pool only when the entropy limit has been reached.
+         */
+        entropy -= num;
+        if (entropy < 0)
+          entropy = 0;
       }
 
     /// here the section filling the 'md' has been moved to RAND_cleanup().
@@ -331,42 +331,42 @@ namespace comet
 
     while (num > 0)
       {
-	/* num_ceil -= MD_DIGEST_LENGTH/2 */
-	j=(num >= MD_DIGEST_LENGTH/2)?MD_DIGEST_LENGTH/2:num;
-	num-=j;
-	MD_Init(&m);
-	MD_Update(&m,local_md,MD_DIGEST_LENGTH);
-	MD_Update(&m,(unsigned char *)&(md_c[0]),sizeof(md_c));
+        /* num_ceil -= MD_DIGEST_LENGTH/2 */
+        j=(num >= MD_DIGEST_LENGTH/2)?MD_DIGEST_LENGTH/2:num;
+        num-=j;
+        MD_Init(&m);
+        MD_Update(&m,local_md,MD_DIGEST_LENGTH);
+        MD_Update(&m,(unsigned char *)&(md_c[0]),sizeof(md_c));
 
-	/* The following line uses the supplied buffer as a small
-	 * source of entropy: since this buffer is often uninitialised
-	 * it may cause programs such as purify or valgrind to
-	 * complain. So for those builds it is not used: the removal
-	 * of such a small source of entropy has negligible impact on
-	 * security.
-	 */
-	/// the following line adds undeterministic entropy and has therefore
-	/// been removed
-	// MD_Update(&m,buf,j);
+        /* The following line uses the supplied buffer as a small
+         * source of entropy: since this buffer is often uninitialised
+         * it may cause programs such as purify or valgrind to
+         * complain. So for those builds it is not used: the removal
+         * of such a small source of entropy has negligible impact on
+         * security.
+         */
+        /// the following line adds undeterministic entropy and has therefore
+        /// been removed
+        // MD_Update(&m,buf,j);
 
-	k=(st_idx+MD_DIGEST_LENGTH/2)-st_num;
-	if (k > 0)
-	  {
-	    MD_Update(&m,&(state[st_idx]),MD_DIGEST_LENGTH/2-k);
-	    MD_Update(&m,&(state[0]),k);
-	  }
-	else
-	  MD_Update(&m,&(state[st_idx]),MD_DIGEST_LENGTH/2);
-	MD_Final(&m,local_md);
+        k=(st_idx+MD_DIGEST_LENGTH/2)-st_num;
+        if (k > 0)
+          {
+            MD_Update(&m,&(state[st_idx]),MD_DIGEST_LENGTH/2-k);
+            MD_Update(&m,&(state[0]),k);
+          }
+        else
+          MD_Update(&m,&(state[st_idx]),MD_DIGEST_LENGTH/2);
+        MD_Final(&m,local_md);
 
-	for (i=0; i<MD_DIGEST_LENGTH/2; i++)
-	  {
-	    state[st_idx++]^=local_md[i]; /* may compete with other threads */
-	    if (st_idx >= st_num)
-	      st_idx=0;
-	    if (i < j)
-	      *(buf++)=local_md[i+MD_DIGEST_LENGTH/2];
-	  }
+        for (i=0; i<MD_DIGEST_LENGTH/2; i++)
+          {
+            state[st_idx++]^=local_md[i]; /* may compete with other threads */
+            if (st_idx >= st_num)
+              st_idx=0;
+            if (i < j)
+              *(buf++)=local_md[i+MD_DIGEST_LENGTH/2];
+          }
       }
 
     MD_Init(&m);
@@ -382,10 +382,10 @@ namespace comet
       return(1);
     else
       {
-	RANDerr(RAND_F_SSLEAY_RAND_BYTES,RAND_R_PRNG_NOT_SEEDED);
-	ERR_add_error_data(1, "You need to read the OpenSSL FAQ, "
-			   "http://www.openssl.org/support/faq.html");
-	return(0);
+        RANDerr(RAND_F_SSLEAY_RAND_BYTES,RAND_R_PRNG_NOT_SEEDED);
+        ERR_add_error_data(1, "You need to read the OpenSSL FAQ, "
+                           "http://www.openssl.org/support/faq.html");
+        return(0);
       }
   }
 
@@ -397,10 +397,10 @@ namespace comet
     ret = RAND_bytes(buf, num);
     if (ret == 0)
       {
-	err = ERR_peek_error();
-	if (ERR_GET_LIB(err) == ERR_LIB_RAND &&
-	    ERR_GET_REASON(err) == RAND_R_PRNG_NOT_SEEDED)
-	  ERR_clear_error();
+        err = ERR_peek_error();
+        if (ERR_GET_LIB(err) == ERR_LIB_RAND &&
+            ERR_GET_REASON(err) == RAND_R_PRNG_NOT_SEEDED)
+          ERR_clear_error();
       }
 
     return (ret);
@@ -417,39 +417,39 @@ namespace comet
      * (could happen if a RAND_poll() implementation calls RAND_status()) */
     if (crypto_lock_rand)
       {
-	CRYPTO_r_lock(CRYPTO_LOCK_RAND2);
-	do_not_lock = !CRYPTO_THREADID_cmp(&locking_threadid, &cur);
-	CRYPTO_r_unlock(CRYPTO_LOCK_RAND2);
+        CRYPTO_r_lock(CRYPTO_LOCK_RAND2);
+        do_not_lock = !CRYPTO_THREADID_cmp(&locking_threadid, &cur);
+        CRYPTO_r_unlock(CRYPTO_LOCK_RAND2);
       }
     else
       do_not_lock = 0;
 
     if (!do_not_lock)
       {
-	CRYPTO_w_lock(CRYPTO_LOCK_RAND);
+        CRYPTO_w_lock(CRYPTO_LOCK_RAND);
 
-	/* prevent ssleay_rand_bytes() from trying to obtain the lock again */
-	CRYPTO_w_lock(CRYPTO_LOCK_RAND2);
-	CRYPTO_THREADID_cpy(&locking_threadid, &cur);
-	CRYPTO_w_unlock(CRYPTO_LOCK_RAND2);
-	crypto_lock_rand = 1;
+        /* prevent ssleay_rand_bytes() from trying to obtain the lock again */
+        CRYPTO_w_lock(CRYPTO_LOCK_RAND2);
+        CRYPTO_THREADID_cpy(&locking_threadid, &cur);
+        CRYPTO_w_unlock(CRYPTO_LOCK_RAND2);
+        crypto_lock_rand = 1;
       }
 
     if (!initialized)
       {
-	/// the call to RAND_poll() has been removed here because
-	/// injecting additional entropy.
-	initialized = 1;
+        /// the call to RAND_poll() has been removed here because
+        /// injecting additional entropy.
+        initialized = 1;
       }
 
     ret = entropy >= ENTROPY_NEEDED;
 
     if (!do_not_lock)
       {
-	/* before unlocking, we must clear 'crypto_lock_rand' */
-	crypto_lock_rand = 0;
+        /* before unlocking, we must clear 'crypto_lock_rand' */
+        crypto_lock_rand = 0;
 
-	CRYPTO_w_unlock(CRYPTO_LOCK_RAND);
+        CRYPTO_w_unlock(CRYPTO_LOCK_RAND);
       }
 
     return ret;
