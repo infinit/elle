5ff694d7a4c0484ac3eae5b93ad3850d612edf5b
Raphaël Londeix <raphael.londeix@gmail.com>
2012-01-26 17:38:21 +0100

    des espaces

---
+++ plasma/installer/resources.hh
@@ -0,0 +1,14 @@
+#ifndef RESOURCES_HPP
+# define RESOURCES_HPP
+
+
+# define INFINIT_UPDATER_URL "http://hotgloupi.fr/CV.pdf"
+# define INFINIT_UPDATER_FILENAME "updater.exe"
+
+# ifdef WIN32
+#  define INFINIT_HOME "Infinit"
+# else
+#  define INFINIT_HOME ".infinit"
+# endif
+
+#endif // RESOURCES_HPP
