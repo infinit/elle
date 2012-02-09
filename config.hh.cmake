abfc0341df2a18356b4442dd971eb77de2186009
Julien Quintard <julien.quintard@infinit.io>
2012-02-09 16:08:18 +0100

    Added the config.hh.cmake which is used to generate the config.h.

---
+++ config.hh.cmake
@@ -0,0 +1,20 @@
+//
+// ---------- header ----------------------------------------------------------
+//
+// project       config.hh.cmake
+//
+// license       infinit
+//
+// author        julien quintard   [mon feb  6 11:16:48 2012]
+//
+
+#ifndef CONFIG_HH
+#define CONFIG_HH
+
+#cmakedefine INFINIT_LINUX
+#cmakedefine INFINIT_MACOSX
+#cmakedefine INFINIT_WINDOWS
+
+// XXX[MacOS X has some problems] #cmakedefine HAVE_SETXATTR
+
+#endif
