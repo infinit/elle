62f585a4e1df9fe120320b5727a3a3ab2a447d8b
Raphaël Londeix <raphael.londeix@gmail.com>
2012-02-13 12:38:29 +0100

    fcgi spawner

---
+++ oracle/disciples/spawn-fcgi.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+DIR="`dirname \"$0\"`"
+PIDFILE=/var/run/infinit-creosus.pid
+[ ! -e $PIDFILE ] && sudo touch $PIDFILE && sudo chown www-data $PIDFILE
+PID="`cat $PIDFILE`"
+[ -n "$PID" ] && echo "Killing $PID" && sudo kill "$PID"
+sleep 1
+sudo -u www-data spawn-fcgi -a 127.0.0.1 -p 7880 -P $PIDFILE -F 1 "$DIR"/creosus-server 127.0.0.1:8089
