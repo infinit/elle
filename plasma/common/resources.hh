82b6be7ba808346715d310304a2d71002a8428ca
Raphaël Londeix <raphael.londeix@gmail.com>
2012-02-14 12:42:54 +0100

    fix: urls for the demo

--- plasma/common/resources.hh
+++ plasma/common/resources.hh
@@ -4,8 +4,8 @@
 # include "elle/system/Platform.hh"
 
 # define _INFINIT_HOST_PROTOCOL "http"
-# define _INFINIT_HOST "hotgloupi.fr"
-# define _INFINIT_DOWNLOAD_DIR "downloads"
+# define _INFINIT_HOST "download.infinit.im"
+# define _INFINIT_DOWNLOAD_DIR ""
 
 # ifdef WIN32
 #  define _INFINIT_FILENAME_SUFFIX ".exe"
@@ -20,7 +20,7 @@
 # endif
 
 # ifdef INFINIT_WINDOWS
-#  define _INFINIT_PLATFORM "windows" _INFINIT_ARCH
+#  define _INFINIT_PLATFORM "win" _INFINIT_ARCH
 # elif defined(INFINIT_LINUX)
 #  define _INFINIT_PLATFORM "linux" _INFINIT_ARCH
 # elif defined(INFINIT_MACOSX)
@@ -35,7 +35,7 @@
   _INFINIT_HOST_PROTOCOL ":/" "/" _INFINIT_HOST "/" _INFINIT_DOWNLOAD_DIR "/" _INFINIT_PLATFORM
 
 # define INFINIT_UPDATER_URI \
-  (INFINIT_BASE_URL "/" INFINIT_UPDATER_FILENAME)
+  (INFINIT_BASE_URL "/binaries/" INFINIT_UPDATER_FILENAME)
 
 # define INFINIT_RELEASE_URI \
   (INFINIT_BASE_URL "/" "release.xml")
@@ -43,6 +43,7 @@
 # define INFINIT_META_URL "http://localhost:12345"
 
 # ifdef INFINIT_WINDOWS
+#  error "TODO !"
 #  define INFINIT_HOME_DIRECTORY "Infinit"
 # else
 #  define INFINIT_HOME_DIRECTORY ".config/infinit"
