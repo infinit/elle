a3dbee6b955bb93a1d921c9ef5d28d4910553723
Raphaël Londeix <raphael.londeix@gmail.com>
2012-02-22 15:39:37 +0100

    troll module, startup scripts

--- oracle/disciples/production-meta-server.sh
+++ oracle/disciples/production-meta-server.sh
@@ -1,8 +1,59 @@
 #!/bin/sh
-DIR="`dirname \"$0\"`"
-PIDFILE=/var/run/infinit-creosus.pid
-[ ! -e $PIDFILE ] && sudo touch $PIDFILE && sudo chown www-data $PIDFILE
-PID="`cat $PIDFILE`"
-[ -n "$PID" ] && echo "Killing $PID" && sudo kill "$PID"
-sleep 1
-sudo -u www-data spawn-fcgi -a 127.0.0.1 -p 7880 -P $PIDFILE -F 1 "$DIR"/creosus-server 127.0.0.1:8089
+# @file production-meta-server.sh
+# @author <raphael.londeix@gmail.com> Raphael Londeix
+# @version @FIXME@
+
+SCRIPT=`basename "$0"`
+SCRIPTDIR=`python -c "import os;print os.path.abspath(os.path.dirname('$0'))"`
+
+kill_screen_session()
+{
+	if [ -n "`screen -ls | grep $1`" ]
+	then
+		echo "Killing $1 screen session"
+		screen -S INFINIT_MONGODB_SERVER -X quit
+		while [ -n "`screen -ls | grep $1`" ]
+		do
+			echo "Waiting $1 screen session to be closed"
+			sleep 1
+		done
+	fi
+}
+
+start_screen()
+{
+	screen -S $1 -m -d $2
+}
+
+restart_screen()
+{
+	kill_screen_session $1
+	start_screen $1 "$2"
+}
+
+restart_screen INFINIT_MONGODB_SERVER "mongod --dbpath /tmp"
+
+kill_screen_session INFINIT_META_SERVER
+
+try=0
+max_tries=10
+while true
+do
+	echo "Launching meta server"
+	screen -L -S INFINIT_META_SERVER -m -d "$SCRIPTDIR"/meta-server
+	before=`screen -ls | grep INFINIT_META_SERVER`
+	sleep 1
+	after=`screen -ls | grep INFINIT_META_SERVER`
+	echo "'$before' '$after'"
+	[ -z "$after" ] && echo BIET
+	[ -n "`screen -ls | grep INFINIT_META_SERVER`" ] && exit 0
+	if [ $try = $max_tries ]
+	then
+		echo "Stop trying ..."
+		exit 1
+	fi
+	try=`expr $try + 1`
+	echo "Retrying to start meta server (try $try)"
+	cat "$SCRIPTDIR"/screenlog.0
+	rm "$SCRIPTDIR"/screenlog.0
+done
