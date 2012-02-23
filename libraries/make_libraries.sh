f31a05a01d61cc54afd4949b4814c88bbce2432a
Raphaël Londeix <raphael.londeix@gmail.com>
2012-02-23 16:02:38 +0100

    make all libs

--- libraries/make_libraries.sh
+++ libraries/make_libraries.sh
@@ -53,24 +53,96 @@ echo "**** OpenSSL up-to-date"
 #############################################################################
 # msgpack
 
-if [ ! -d msgpack ]
+msgpack_version=0.5.7
+msgpack_dir=msgpack-$msgpack_version
+msgpack_tarball=$msgpack_dir.tar.gz
+msgpack_url="http://msgpack.org/releases/cpp/$msgpack_tarball"
+
+if [ ! -d $msgpack_dir ]
 then
 	echo "**** Download msgpack"
-	git clone git://github.com/msgpack/msgpack.git
-else
-	echo "**** Update msgpack"
-	(
-		cd msgpack
-		git pull
-		cd -
-	) 2>&1 > /dev/null
+	wget "$msgpack_url" -O $msgpack_tarball
+	tar xzf $msgpack_tarball
+	rm $msgpack_tarball
 fi
 
 [ ! -e build/lib/libmsgpack.a -o ! -e build/lib/libmsgpack.so ] && (
 	echo "**** Build msgpack"
-	cd msgpack/cpp
-	./bootstrap
-	./configure --prefix="$BUILDDIR"
+	cd $msgpack_dir
+	./configure --prefix="$BUILDDIR" CFLAGS="${CFLAGS} -fPIC"
 	make install
 	cd -
 )
+
+echo "**** msgpack up-to-date"
+
+#############################################################################
+# jsoncpp
+
+[ ! -d jsoncpp ] && (
+	echo "**** Download jsoncpp"
+	svn co https://jsoncpp.svn.sourceforge.net/svnroot/jsoncpp/trunk/jsoncpp jsoncpp
+)
+
+[ ! -e build/lib/libjson.a -o ! -e build/lib/libjson.so ] && (
+	echo "**** Build jsoncpp"
+	cd jsoncpp
+	scons platform=linux-gcc
+	cd -
+	cp jsoncpp/buildscons/*/src/lib_json/*.a build/lib/libjson.a
+	cp jsoncpp/buildscons/*/src/lib_json/*.so build/lib/libjson.so
+	cp -r jsoncpp/include/json build/include
+)
+
+echo "**** jsoncpp up-to-date"
+
+#############################################################################
+# curl
+
+curl_version=7.24.0
+curl_dir=curl-$curl_version
+curl_tarball=$curl_dir.tar.gz
+curl_url="http://curl.haxx.se/download/$curl_tarball"
+
+if [ ! -d $curl_dir ]
+then
+	echo "**** Download curl"
+	wget "$curl_url" -O $curl_tarball
+	tar xzf $curl_tarball
+	rm $curl_tarball
+fi
+
+[ ! -e build/lib/libcurl.a -o ! -e build/lib/libcurl.so ] && (
+	cd $curl_dir
+	./configure --prefix="$BUILDDIR" --with-ssl="$BUILDDIR/openssl"
+	make install
+	cd -
+)
+
+echo "**** curl up-to-date"
+
+
+#############################################################################
+# qjson
+
+[ ! -d qjson ] && (
+	echo "**** Download qjson"
+	git clone git://gitorious.org/qjson/qjson.git
+)
+
+[ ! -e build/lib/libqjson.so ] && (
+	echo "**** build qjson"
+	cd qjson
+	mkdir build
+	cd build
+	cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX="$BUILDDIR" ..
+	make install
+	cd ../..
+	mkdir -p build/include/QJson
+	cd build/include/QJson
+	echo '#include "../qjson/serializer.h"' > Serializer
+	echo '#include "../qjson/parser.h"' > Parser
+	echo '#include "../qjson/qobjecthelper.h"' > QObjectHelper
+)
+
+echo "**** qjson up-to-date"
