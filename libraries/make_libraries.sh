41758aca4fb19f590b78261d1cb90d84fdb4dd66
Raphaël Londeix <raphael.londeix@gmail.com>
2012-02-23 21:58:18 +0100

    improve: more checks in make_library.sh

--- libraries/make_libraries.sh
+++ libraries/make_libraries.sh
@@ -17,11 +17,15 @@ die()
 }
 
 [ -z $ARCH ] && die "You have to specify an architecture"
+[ -f "$SCRIPTDIR/$SCRIPT" ] || die "Wrong value for SCRIPTDIR: '$SCRIPTDIR'"
 
 BUILDDIR="$SCRIPTDIR/$ARCH/build"
 
-mkdir -p "$BUILDDIR"
-cd "$SCRIPTDIR/$ARCH"
+[ ! -d "$BUILDDIR" ] && (
+	mkdir -p "$BUILDDIR" || die "Cannot create $BUILDDIR directory"
+)
+
+cd "$SCRIPTDIR/$ARCH" || die "Cannot cd to $SCRIPTDIR/$ARCH" directory"
 
 #############################################################################
 # OpenSSL
@@ -31,25 +35,23 @@ openssl_dir=openssl-$openssl_version
 openssl_tarball=$openssl_dir.tar.gz
 openssl_url="http://www.openssl.org/source/$openssl_tarball"
 
-if [ ! -d $openssl_dir ]
-then
+[ ! -d $openssl_dir ] && (
 	echo "**** Download OpenSSL"
-	wget "$openssl_url" -O $openssl_tarball
-	#cp ~/Downloads/$openssl_tarball .
-	tar xzf $openssl_tarball
+	wget "$openssl_url" -O $openssl_tarball && \
+	tar xzf $openssl_tarball && \
 	rm $openssl_tarball
-fi
+) || die "Couldn't checkout openssl"
 
 [ ! -e build/lib/libssl.a -o ! -e build/lib/libcrypto.a ] && (
 	echo "**** Build OpenSSL"
-	cd $openssl_dir
+	cd $openssl_dir && \
 	./config --prefix="$BUILDDIR"                       \
 	         --openssldir="$BUILDDIR/openssl"           \
 	         no-shared no-asm no-threads zlib           \
-	         -fPIC
-	make install
+	         -fPIC && \
+	make install && \
 	cd -
-)
+) || die "Couldn't build openssl"
 
 echo "**** OpenSSL up-to-date"
 
@@ -61,21 +63,20 @@ msgpack_dir=msgpack-$msgpack_version
 msgpack_tarball=$msgpack_dir.tar.gz
 msgpack_url="http://msgpack.org/releases/cpp/$msgpack_tarball"
 
-if [ ! -d $msgpack_dir ]
-then
+[ ! -d $msgpack_dir ] && (
 	echo "**** Download msgpack"
-	wget "$msgpack_url" -O $msgpack_tarball
-	tar xzf $msgpack_tarball
+	wget "$msgpack_url" -O $msgpack_tarball && \
+	tar xzf $msgpack_tarball && \
 	rm $msgpack_tarball
-fi
+) || die "Couldn't checkout msgpack"
 
 [ ! -e build/lib/libmsgpack.a ] && (
 	echo "**** Build msgpack"
-	cd $msgpack_dir
-	./configure --prefix="$BUILDDIR" CFLAGS="${CFLAGS} -fPIC"
-	make install
+	cd $msgpack_dir && \
+	./configure --prefix="$BUILDDIR" CFLAGS="${CFLAGS} -fPIC" && \
+	make install && \
 	cd -
-)
+) || die "Couldn't build msgpack"
 
 echo "**** msgpack up-to-date"
 
@@ -85,17 +86,17 @@ echo "**** msgpack up-to-date"
 [ ! -d jsoncpp ] && (
 	echo "**** Download jsoncpp"
 	svn co https://jsoncpp.svn.sourceforge.net/svnroot/jsoncpp/trunk/jsoncpp jsoncpp
-)
+) || die "Couldn't checkout jsoncpp"
 
 [ ! -e build/lib/libjson.a ] && (
 	echo "**** Build jsoncpp"
-	cd jsoncpp
-	scons platform=linux-gcc
-	cd -
-	cp jsoncpp/buildscons/*/src/lib_json/*.a build/lib/libjson.a
-	cp jsoncpp/buildscons/*/src/lib_json/*.so build/lib/libjson.so
+	cd jsoncpp && \
+	scons platform=linux-gcc && \
+	cd - && \
+	cp jsoncpp/buildscons/*/src/lib_json/*.a build/lib/libjson.a && \
+	cp jsoncpp/buildscons/*/src/lib_json/*.so build/lib/libjson.so && \
 	cp -r jsoncpp/include/json build/include/jsoncpp
-)
+) || die "Could'nt build jsoncpp"
 
 echo "**** jsoncpp up-to-date"
 
@@ -107,20 +108,20 @@ curl_dir=curl-$curl_version
 curl_tarball=$curl_dir.tar.gz
 curl_url="http://curl.haxx.se/download/$curl_tarball"
 
-if [ ! -d $curl_dir ]
-then
+[ ! -d $curl_dir ] && (
 	echo "**** Download curl"
-	wget "$curl_url" -O $curl_tarball
-	tar xzf $curl_tarball
+	wget "$curl_url" -O $curl_tarball && \
+	tar xzf $curl_tarball && \
 	rm $curl_tarball
-fi
+) || die "Couldn't checkout curl"
+
 
 [ ! -e build/lib/libcurl.a ] && (
 	cd $curl_dir
 	./configure --prefix="$BUILDDIR" --with-ssl="$BUILDDIR/openssl"
 	make install
 	cd -
-)
+) || die "Couldn't build curl"
 
 echo "**** curl up-to-date"
 
@@ -131,7 +132,7 @@ echo "**** curl up-to-date"
 [ ! -d qjson ] && (
 	echo "**** Download qjson"
 	git clone git://gitorious.org/qjson/qjson.git
-)
+) || die "Couldn't checkout qjson"
 
 [ -z "`ls build/lib/libqjson*`" ] && (
 	echo "**** build qjson"
@@ -146,6 +147,6 @@ echo "**** curl up-to-date"
 	echo '#include "../qjson/serializer.h"' > Serializer
 	echo '#include "../qjson/parser.h"' > Parser
 	echo '#include "../qjson/qobjecthelper.h"' > QObjectHelper
-)
+) || die "Couldn't build qjson"
 
 echo "**** qjson up-to-date"
