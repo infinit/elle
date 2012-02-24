950a81230566d0d3d0c063fb05f14e420f8c13ca
Raphaël Londeix <raphael.londeix@gmail.com>
2012-02-24 15:10:50 +0100

    improve: enable fastcgi through the script

--- oracle/disciples/production-creosus-server.sh
+++ oracle/disciples/production-creosus-server.sh
@@ -5,4 +5,5 @@ PIDFILE=/var/run/infinit-creosus.pid
 PID="`cat $PIDFILE`"
 [ -n "$PID" ] && echo "Killing $PID" && sudo kill "$PID"
 sleep 1
+export ENABLE_FASTCGI=1
 sudo -u www-data spawn-fcgi -a 127.0.0.1 -p 7880 -P $PIDFILE -F 1 "$DIR"/creosus-server 127.0.0.1:8089
