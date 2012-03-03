613b22d62ef1f5e248a81b114cde2271beb914ea
Julien Quintard <julien.quintard@infinit.io>
2012-03-03 14:06:24 +0100

    Nothing but the new LICENCE files.

--- etoile/Etoile.cc
+++ etoile/Etoile.cc
@@ -17,103 +17,94 @@
 #include <hole/Hole.hh>
 #include <Infinit.hh>
 
-namespace etoile
-{
+using namespace etoile;
 
 //
 // ---------- definitions -----------------------------------------------------
 //
 
-  ///
-  /// this value defines the component's name.
-  ///
-  const elle::Character         Component[] = "etoile";
+///
+/// this value defines the component's name.
+///
+const elle::Character         Component[] = "etoile";
 
-  ///
-  /// this variable contains the phrase associated with the Infinit's
-  /// current instance.
-  ///
-  lune::Phrase                  Etoile::Phrase;
+///
+/// this variable contains the phrase associated with the Infinit's
+/// current instance.
+///
+lune::Phrase Etoile::Phrase;
 
 //
 // ---------- methods ---------------------------------------------------------
 //
 
-  ///
-  /// this method initializes Etoile.
-  ///
-  elle::Status          Etoile::Initialize()
+///
+/// this method initializes Etoile.
+///
+elle::Status          Etoile::Initialize()
+{
+  //
+  // initialize the components.
+  //
+
+  if (path::Path::Initialize() == elle::StatusError)
+    escape("unable to initialize the path");
+
+  if (depot::Depot::Initialize() == elle::StatusError)
+    escape("unable to initialize the depot");
+
+  if (gear::Gear::Initialize() == elle::StatusError)
+    escape("unable to initialize the gear");
+
+  if (shrub::Shrub::Initialize() == elle::StatusError)
+    escape("unable to initialize the shrub");
+
+  if (portal::Portal::Initialize() == elle::StatusError)
+    escape("unable to initialize the portal");
+
+  //
+  // generate a phrase randomly which will be used by applications to
+  // connect to Etoile and trigger specific actions.
+  //
+  // generate a random string, create a phrase with it along with
+  // the socket used by portal so that applications have everything
+  // to connect to and authenticate to portal.
   {
-    // initialize the path.
-    if (path::Path::Initialize() == elle::StatusError)
-      escape("unable to initialize the path");
+    elle::String string;
 
-    // initialize the depot.
-    if (depot::Depot::Initialize() == elle::StatusError)
-      escape("unable to initialize the depot");
+    if (elle::Random::Generate(string) == elle::StatusError)
+      escape("unable to generate a random string");
 
-    // initialize the gear.
-    if (gear::Gear::Initialize() == elle::StatusError)
-      escape("unable to initialize the gear");
+    if (Etoile::Phrase.Create(string,
+                              portal::Portal::Line) == elle::StatusError)
+      escape("unable to create the phrase");
 
-    // initialize the shrub.
-    if (shrub::Shrub::Initialize() == elle::StatusError)
-      escape("unable to initialize the shrub");
-
-    // initialize the portal.
-    if (portal::Portal::Initialize() == elle::StatusError)
-      escape("unable to initialize the portal");
-
-    //
-    // generate a phrase randomly which will be used by applications to
-    // connect to Etoile and trigger specific actions.
-    //
-    {
-      elle::String              string;
-
-      // generate a random string.
-      if (elle::Random::Generate(string) == elle::StatusError)
-        escape("unable to generate a random string");
-
-      // create the phrase.
-      if (Etoile::Phrase.Create(string,
-                                portal::Portal::Line) == elle::StatusError)
-        escape("unable to create the phrase");
-
-      // store the phrase.
-      if (Etoile::Phrase.Store(Infinit::Network) == elle::StatusError)
-        escape("unable to store the phrase");
-    }
-
-    return elle::StatusOk;
+    if (Etoile::Phrase.Store(Infinit::Network) == elle::StatusError)
+      escape("unable to store the phrase");
   }
 
-  ///
-  /// this method cleans Etoile.
-  ///
-  elle::Status          Etoile::Clean()
-  {
-    // clean the portal.
-    if (portal::Portal::Clean() == elle::StatusError)
-      escape("unable to clean the portal");
+  return elle::StatusOk;
+}
 
-    // clean the shrub.
-    if (shrub::Shrub::Clean() == elle::StatusError)
-      escape("unable to clean the shrub");
+///
+/// this method cleans Etoile.
+///
+elle::Status          Etoile::Clean()
+{
+  if (portal::Portal::Clean() == elle::StatusError)
+    escape("unable to clean the portal");
 
-    // clean the gear.
-    if (gear::Gear::Clean() == elle::StatusError)
-      escape("unable to clean the gear");
+  if (shrub::Shrub::Clean() == elle::StatusError)
+    escape("unable to clean the shrub");
 
-    // clean the depot.
-    if (depot::Depot::Clean() == elle::StatusError)
-      escape("unable to clean the depot");
+  if (gear::Gear::Clean() == elle::StatusError)
+    escape("unable to clean the gear");
 
-    // clean the path.
-    if (path::Path::Clean() == elle::StatusError)
-      escape("unable to clean the path");
+  if (depot::Depot::Clean() == elle::StatusError)
+    escape("unable to clean the depot");
 
-    return elle::StatusOk;
-  }
+  if (path::Path::Clean() == elle::StatusError)
+    escape("unable to clean the path");
 
+  return elle::StatusOk;
 }
