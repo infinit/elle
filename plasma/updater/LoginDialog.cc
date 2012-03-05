4f989842e2c957b3698559d9fd86d2eeab5d08d3
Raphaël Londeix <raphael.londeix@infinit.io>
2012-03-05 11:36:41 +0100

    better hooks

--- plasma/updater/LoginDialog.cc
+++ plasma/updater/LoginDialog.cc
@@ -1,4 +1,6 @@
 
+#include <iostream>
+
 #include "LoginDialog.hh"
 #include "ui_LoginDialog.h"
 
@@ -7,6 +9,7 @@ LoginDialog::LoginDialog(QWidget *parent) :
     ui(new Ui::LoginDialog)
 {
     ui->setupUi(this);
+    this->connect(this, SIGNAL(accepted), this, SLOT(_OnAccepted));
 }
 
 LoginDialog::~LoginDialog()
@@ -14,16 +17,20 @@ LoginDialog::~LoginDialog()
     delete ui;
 }
 
-void LoginDialog::GetLoginPassword(std::string& login, std::string& password)
-{
-  if (this->ui && this->ui->email && this->ui->password)
-    {
-      login = this->ui->email->text().toStdString();
-      password = this->ui->password->text().toStdString();
-    }
-}
-
 void LoginDialog::SetErrorMessage(std::string const& error)
 {
   this->ui->error->setText(error.c_str());
+}
+
+void LoginDialog::_OnAccepted()
+{
+  if (this->ui && this->ui->email && this->ui->password)
+    {
+      emit doLogin(this->ui->email->text().toStdString(),
+                   this->ui->password->text().toStdString());
+    }
+  else
+    {
+      std::cout << "Bah goul\n";
+    }
 }
