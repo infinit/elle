dea3b3b7b814d5a6e33ee04fc6a8041b59fd1f5a
Julien Quintard <julien.quintard@infinit.io>
2012-03-05 12:47:55 +0100

    Nothing really.

--- _infinit.cc
+++ _infinit.cc
@@ -111,6 +111,8 @@ elle::Status            Main(elle::Natural32                    argc,
 
     // assign the username.
     Infinit::User.assign(pw->pw_name);
+    // XXX[this will take the current user and therefore displays:
+           `enter your passphrase ...`]
   }
 #elif defined(INFINIT_WINDOWS)
   {
@@ -123,6 +125,8 @@ elle::Status            Main(elle::Natural32                    argc,
 
     // assign the username.
     Infinit::User.assign(username, length);
+    // XXX[this will take the current user and therefore displays:
+           `enter your passphrase ...`]
   }
 #else
 # error "unsupported platform"
