9c02b80b59e1e986528c09e516aedeae31b3bc3d
Charles Guillot <charles.guillot@infinit.io>
2012-04-01 23:03:34 +0200

    File & Folder hierarchy - Polished

--- surface/macosx/medecine/dopant/helper/FISwizzle.h
+++ surface/macosx/medecine/dopant/helper/FISwizzle.h
@@ -1,7 +1,9 @@
 #import <Foundation/Foundation.h>
 
 @interface NSObject (FISwizzle)
+
 + (BOOL)fi_swizzleMethod:(SEL)origSel_ withMethod:(SEL)altSel_ error:(NSError**)error_;
 + (BOOL)fi_swizzleClassMethod:(SEL)origSel_ withClassMethod:(SEL)altSel_ error:(NSError**)error_;
+
 @end
 
