03a01664c30c2e835fba4dbc41a7f041cb9a7300
Raphaël Londeix <raphael.londeix@infinit.io>
2012-04-07 12:13:26 +0200

    some doc, some test

--- serialize/BinaryArchive.hh
+++ serialize/BinaryArchive.hh
@@ -7,6 +7,17 @@
 
 namespace elle { namespace serialize {
 
+    ///
+    /// Binary archives are intended for high performace packing. It uses
+    /// exactly the size of serialized objects, with a few extra data when
+    /// serializing classes. The serialization itself is portable accross
+    /// architectures, but DOES NOT protect you from using non-portable
+    /// types, like 'int'.
+    ///
+    /// « You should always serialize fixed size types like 'int32_t'. »
+    ///
+    /// @see elle::serialize::BaseArchive for general informations about archives
+    ///
     template<ArchiveMode mode>
       class BinaryArchive :
         public BaseArchive<mode, BinaryArchive<mode>>
