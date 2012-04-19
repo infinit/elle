a5d45946821bc157fd5b9e903052fab350666f95
Charles Guillot <charles.guillot@infinit.io>
2012-04-19 16:32:31 +0200

    Doc Icon Overlayer Swizzler Saison 2

--- surface/macosx/medecine/dopant/iconOverlay/OOZombieDictKey.h
+++ surface/macosx/medecine/dopant/iconOverlay/OOZombieDictKey.h
@@ -9,17 +9,31 @@
 #import <Cocoa/Cocoa.h>
 #import "OOCellDictKey-Protocol.h"
 
-
+/**
+ This class is instanciated by cell dictionary keys
+ in order to simulate an override of the cell's parrent
+ dealloc method
+ */
 @interface OOZombieDictKey : NSObject
 {
+    /**
+     The siblings cells
+     */
     NSMutableArray  *linkedDictKeys;
+    
+    /**
+     The parent cell
+     */
     id              parent;
 }
 
 @property(retain) NSMutableArray *linkedDictKeys;
 @property(assign) id parent;
 
+/**
+ Add a cell dictionary key
+ @param id a cell dictionary key
+ */
 - (void) addDictKey:(id)arg1;
-- (id) init;
 
 @end
