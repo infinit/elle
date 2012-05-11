23d341070c91dbe3f46308c1e0db0fe31eb9dd4f
Raphaël Londeix <raphael.londeix@infinit.io>
2012-05-11 17:45:13 +0200

    fix: print test passes

--- tests/elle/print.cc
+++ tests/elle/print.cc
@@ -8,5 +8,6 @@ int main()
   auto s = elle::stringify("Salut", elle::iomanip::tabsep, "les", elle::iomanip::spacesep, 21, "couillons");
   elle::print(s);
   assert(s == "Salut\tles 21 couillons");
+  elle::print("tests", "done.");
   return 0;
 }
