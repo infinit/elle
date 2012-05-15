a9a4b7c91c59959725695eaa1d2e18d5e7d7ca0a
Raphaël Londeix <raphael.londeix@infinit.io>
2012-05-15 13:52:13 +0200

    no more password from the register form

--- oracle/disciples/creosus/pages/register.py
+++ oracle/disciples/creosus/pages/register.py
@@ -13,15 +13,17 @@ class Register(creosus.Page):
     _form = form.Form('register_form', [
         form.Text('email', validator=form.validators.Email),
         form.Text('fullname', validator=form.validators.NotNull),
-        form.Password('password', validator=form.validators.Regexp(r'^.{6,}$', 'Password too short')),
-        form.Password('password_confirmation'),
+        # XXX
+        #form.Password('password', validator=form.validators.Regexp(r'^.{6,}$', 'Password too short')),
+        #form.Password('password_confirmation'),
         form.Text('activation_code'),
         form.Submit('register', label="Register", validator=form.validators.NotNull),
     ], validators=[
-        form.validators.Validator(
-            lambda f: f['password'] == f['password_confirmation'],
-            "Passwords didn't match"
-        ),
+        # XXX
+        #form.validators.Validator(
+        #    lambda f: f['password'] == f['password_confirmation'],
+        #    "Passwords didn't match"
+        #),
         form.validators.Validator(
             lambda f: f['activation_code'] == 'bite',
             "Invalid activation code"
@@ -35,7 +37,7 @@ class Register(creosus.Page):
             res = self.api_admin.post('/register',{
                 'email': f['email'].value,
                 'fullname': f['fullname'].value,
-                'password': f['password'].value,
+                'password': '', # f['password'].value, #XXX
             })
             if res['success']:
                 raise web.seeother('/')
