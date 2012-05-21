c9d5b549ce373e98ac1dc30ef4fb169fb5729a60
Charles Guillot <charles.guillot@infinit.io>
2012-05-21 17:55:56 +0200

    Surface : Fuse kext installer

---
+++ surface/macosx/medecine/nurse/OONurseManager.m
@@ -0,0 +1,22 @@
+//
+//  OONurseManager.m
+//  medecine
+//
+//  Created by Charles Guillot on 21/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "OONurseManager.h"
+
+@implementation OONurseManager
+
+- (BOOL)manage:(NSString *)arg1
+{
+    OONurseInjector *nurseInj = [[OONurseInjector alloc] init];
+    OONurseFuser *nurseFuse = [[OONurseFuser alloc] init];
+    
+    return [nurseInj injectWithAppPath:arg1] && [nurseFuse installFuseWithAppPath:arg1];
+}
+
+
+@end
