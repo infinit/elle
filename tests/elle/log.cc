f2a8e6af9b2543c778940b6e09dbee359520229f
Raphaël Londeix <raphael.londeix@infinit.io>
2012-05-28 12:21:51 +0200

    draft logger

--- tests/elle/log.cc
+++ tests/elle/log.cc
@@ -1,6 +1,21 @@
 
 #include <elle/log.hh>
 
+ELLE_LOG_TRACE_COMPONENT("test_log");
+
+void g()
+{
+  ELLE_LOG_TRACE("Me too!");
+}
+
+void f()
+{
+  ELLE_LOG_TRACE("This is usefull!")
+  {
+    g();
+  }
+}
+
 int main()
 {
   elle::log::debug("BIET");
@@ -11,5 +26,6 @@ int main()
   elle::log::info("Salut", "les", 12, "copains");
   elle::log::default_logger.name("LOGGER_NAME_IS_STUPID");
   elle::log::info("tests done.");
+  f();
   return 0;
 }
