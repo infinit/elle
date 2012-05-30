8df8157f9483071543f8ffbff1a8cde23d52abb5
mefyl <quentin.hocquet@infinit.io>
2012-05-30 15:44:09 +0200

    Horizon: fix bug: do not call statfs on linux.
    Also factor FUSE operations list.

--- horizon/macosx/FUker.cc
+++ horizon/macosx/FUker.cc
@@ -1,16 +1,3 @@
-//
-// ---------- header ----------------------------------------------------------
-//
-// project       horizon
-//
-// license       infinit
-//
-// author        julien quintard   [tue jul 26 15:33:54 2011]
-//
-
-//
-// ---------- includes --------------------------------------------------------
-//
 
 #include <horizon/macosx/FUSE.hh>
 
@@ -32,52 +19,13 @@
 # include <reactor/thread.hh>
 #include <elle/idiom/Open.hh>
 
+#include <horizon/operations.hh>
+
 namespace horizon
 {
   namespace macosx
   {
     typedef struct ::timespec timespec2[2];
-
-#if defined(HAVE_SETXATTR)
-# define INFINIT_FUSE_COMMANDS_XATTR                                    \
-    ((setxattr, (const char*)(const char*)(const char*)                 \
-      (size_t)(int)(uint32_t)))                                         \
-    ((getxattr, (const char*)(const char*)(char*)(size_t)(uint32_t)))   \
-    ((listxattr, (const char*)(char*)(size_t)))                         \
-    ((removexattr, (const char*)(const char*)))
-#else
-# define INFINIT_FUSE_COMMANDS_XATTR
-#endif
-
-#define INFINIT_FUSE_COMMANDS                                           \
-    ((statfs, (const char*)(struct ::statvfs*)))                        \
-    ((getattr, (const char*)(struct ::stat*)))                          \
-    ((fgetattr, (const char*)(struct ::stat*)                           \
-      (struct ::fuse_file_info*)))                                      \
-    ((utimens, (const char*)(const timespec2)))                         \
-    ((opendir, (const char*)(struct ::fuse_file_info*)))                \
-    ((readdir, (const char*)(void*) (::fuse_fill_dir_t)(off_t)          \
-      (struct ::fuse_file_info*)))                                      \
-    ((releasedir, (const char*)(struct ::fuse_file_info*)))             \
-    ((mkdir, (const char*)(mode_t)))                                    \
-    ((rmdir, (const char*)))                                            \
-    ((access, (const char*)(int)))                                      \
-    ((chmod, (const char*)(mode_t)))                                    \
-    ((chown, (const char*)(uid_t)(gid_t)))                              \
-    INFINIT_FUSE_COMMANDS_XATTR                                         \
-    ((symlink, (const char*)(const char*)))                             \
-    ((readlink, (const char*)(char*)(size_t)))                          \
-    ((create, (const char*)(mode_t)(struct ::fuse_file_info*)))         \
-    ((open, (const char*)(struct ::fuse_file_info*)))                   \
-    ((write, (const char*)(const char*)(size_t)(off_t)                  \
-      (struct ::fuse_file_info*)))                                      \
-    ((read, (const char*)(char*)(size_t)(off_t)                         \
-      (struct ::fuse_file_info*)))                                      \
-    ((truncate, (const char*)(off_t)))                                  \
-    ((ftruncate, (const char*)(off_t)(struct ::fuse_file_info*)))       \
-    ((release, (const char*)(struct ::fuse_file_info*)))                \
-    ((rename, (const char*)(const char*)))                              \
-    ((unlink, (const char*)))                                           \
 
     ///
     /// this is the identifier of the thread which is spawn so as to start
