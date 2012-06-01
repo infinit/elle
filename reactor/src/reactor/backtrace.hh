6cc5df26a8004b82ca645bc7bd34397ff7b9646e
mefyl <quentin.hocquet@infinit.io>
2012-06-01 17:47:22 +0200

    Reactor: store the backtrace in exceptions.

---
+++ reactor/src/reactor/backtrace.hh
@@ -0,0 +1,32 @@
+#ifndef REACTOR_BACKTRACE_HH
+# define REACTOR_BACKTRACE_HH
+
+# include <vector>
+
+namespace reactor
+{
+  struct StackFrame
+  {
+    std::string symbol;
+    std::string symbol_mangled;
+    std::string symbol_demangled;
+    int address;
+    int offset;
+    operator std::string() const;
+  };
+
+  std::string	demangle(const std::string& sym);
+
+  class Backtrace: public std::vector<StackFrame>
+  {
+  public:
+    Backtrace();
+    typedef std::vector<StackFrame> SuperType;
+    typedef StackFrame Frame;
+  };
+
+  std::ostream& operator<< (std::ostream& output, const StackFrame& frame);
+  std::ostream& operator<< (std::ostream& output, const Backtrace& bt);
+}
+
+#endif
