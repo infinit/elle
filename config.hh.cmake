12e9805e73189373bb9f6555f55892b73497d19a
Julien Quintard <julien.quintard@infinit.io>
2012-06-06 14:15:39 +0200

    Added some logging in Program.
    Moved the default configuration in default/home/.config/

--- config.hh.cmake
+++ config.hh.cmake
@@ -1,13 +1,3 @@
-//
-// ---------- header ----------------------------------------------------------
-//
-// project       config.hh.cmake
-//
-// license       infinit
-//
-// author        julien quintard   [mon feb  6 11:16:48 2012]
-//
-
 #ifndef CONFIG_HH
 #define CONFIG_HH
 
