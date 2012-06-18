b081f81db3ddafe3c8f8de3379043ed9f37b44e2
Raphaël Londeix <raphael.londeix@infinit.io>
2012-06-18 17:58:03 +0200

    rebuilding default infinit conf

--- surface/gap/State.cc
+++ surface/gap/State.cc
@@ -1,4 +1,18 @@
+
+#include <cstdlib>
+#include <fstream>
+#include <pwd.h>
 #include <stdexcept>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <boost/filesystem.hpp>
+
+#include <QString>
+#include <QByteArray>
+#include <QLocalSocket>
+
+#include <plasma/common/resources.hh>
 
 #include "State.hh"
 
@@ -12,6 +26,20 @@ namespace surface
       std::string name;
     };
 
+    State::State()
+      : _infinit_home(::getenv("INFINIT_HOME"))
+      , _networks()
+      , _networks_dirty(true)
+    {
+      if (this->_infinit_home.empty())
+        {
+          struct passwd *pw = ::getpwuid(::getuid());
+          boost::filesystem::path homedir(pw->pw_dir);
+          homedir /= ".config/infinit";
+          this->_infinit_home = homedir.string();
+        }
+    }
+
     State::~State()
     {
       for (auto& it: this->_networks)
@@ -21,12 +49,54 @@ namespace surface
 
     void State::refresh_networks()
     {
+      this->_send_watchdog_cmd("refresh_networks");
+      this->_networks_dirty = true;
     }
 
-    std::string const& State::path_to_network(std::string const& path) const
+    std::string const& State::path_to_network(std::string const& path_string)
     {
-      throw std::runtime_error("Cannot find any network for path '" + path + "'");
-    };
+      if (this->_networks_dirty)
+        this->_reload_networks();
+      throw std::runtime_error("Cannot find any network for path '" + path_string + "'");
+    }
+
+    void State::_reload_networks()
+    {
+    }
+
+    std::string State::_watchdog_id() const // XXX should be cached
+    {
+      boost::filesystem::path wtg_id_path(this->_infinit_home);
+      wtg_id_path /= "infinit.wtg";
+      std::ifstream file(wtg_id_path.string());
+      if (!file.good())
+        throw std::runtime_error("Cannot open '" + wtg_id_path.string() + "'");
+
+      char wtg_id[4096];
+      file.read(wtg_id, 4096);
+      if (!file.eof())
+        throw std::runtime_error("Watchdog id is too long!");
+      return std::string(wtg_id, file.gcount());
+    }
+
+    void State::_send_watchdog_cmd(std::string const& cmd)
+    {
+      QString id = this->_watchdog_id().c_str();
+      QLocalSocket conn;
+      conn.connectToServer(WATCHDOG_SERVER_NAME);
+      if (conn.waitForConnected(2000))
+        {
+          QByteArray json_cmd = QString("{"
+                "\"command\":\"" + QString(cmd.c_str()) + "\","
+                "\"_id\": \"" + id + "\""
+          "}\n").toAscii();
+          conn.write(json_cmd);
+          if (!conn.waitForBytesWritten(2000))
+              throw std::runtime_error("Couldn't send the command '" + cmd + "'");
+        }
+      else
+        throw std::runtime_error("Couldn't connect to the watchdog");
+    }
 
   }
 }
--- surface/gap/State.hh
+++ surface/gap/State.hh
@@ -14,11 +14,18 @@ namespace surface
     class State
     {
     private:
+      std::string                     _infinit_home;
       std::map<std::string, Network*> _networks;
+      bool                            _networks_dirty;
     public:
+      State();
       ~State();
       void refresh_networks();
-      std::string const& path_to_network(std::string const& path) const;
+      std::string const& path_to_network(std::string const& path);
+    private:
+      std::string _watchdog_id() const;
+      void _send_watchdog_cmd(std::string const& cmd);
+      void _reload_networks();
     };
 
   }
