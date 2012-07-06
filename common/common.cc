e266774cbab035ecc8fe796fc221f692b86df475
Raphaël Londeix <raphael.londeix@infinit.io>
2012-07-06 17:52:05 +0200

    add: common variables accessors

---
+++ common/common.cc
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+
+#include <elle/utility/getenv.hh>
+
+#include <common/common.hh>
+
+namespace
+{
+
+  std::string _home_directory()
+  {
+    struct passwd* pw = ::getpwuid(getuid());
+    if (pw != nullptr && pw->pw_dir != nullptr)
+      return std::string{pw->pw_dir};
+    else
+      return elle::utility::getenv("HOME", "/tmp");
+  }
+
+  std::string _infinit_home()
+  {
+    return elle::utility::getenv(
+        "INFINIT_HOME",
+        common::home_directory() + "/" + COMMON_DEFAULT_INFINIT_HOME
+    );
+  }
+
+}
+
+namespace common
+{
+
+  std::string const& home_directory()
+  {
+    static std::string home_dir = _home_directory();
+    return home_dir;
+  }
+
+  std::string const& infinit_home()
+  {
+    static std::string infinit_dir = _infinit_home();
+    return infinit_dir;
+  }
+
+}
+
