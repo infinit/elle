961b76d1f9ff6aa2dc287b21464a53583a5b50ba
Raphaël Londeix <raphael.londeix@infinit.io>
2012-07-25 15:15:08 +0200

    normalized sources, explicit dtor

--- format/json/Null.cc
+++ format/json/Null.cc
@@ -4,13 +4,42 @@
 
 #include "Null.hh"
 
-using namespace elle::format::json;
-
-Null const elle::format::json::null{};
-
-void Null::Save(elle::serialize::OutputJSONArchive& ar) const
+namespace elle
 {
-  ar.stream() << "null";
+  namespace format
+  {
+    namespace json
+    {
+
+      Null::~Null()
+      {}
+
+      void
+      Null::Save(elle::serialize::OutputJSONArchive& ar) const
+      {
+        ar.stream() << "null";
+      }
+
+      std::unique_ptr<Object>
+      Null::Clone() const
+      {
+        return std::unique_ptr<Object>(new Null);
+      }
+
+      bool
+      Null::operator ==(Object const& other) const
+      {
+        return other == *this;
+      }
+
+      bool
+      Null::operator ==(Null const&) const
+      {
+        return true;
+      }
+
+      Null const null;
+
+    }
+  }
 }
-
-
