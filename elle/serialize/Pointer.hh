530b7296f9d3e7caee4b6144881b5aefd0c869c3
Raphaël Londeix <raphael.londeix@infinit.io>
2012-07-31 17:10:47 +0200

    clean: Pointer serializer doc and norm

--- elle/serialize/Pointer.hh
+++ elle/serialize/Pointer.hh
@@ -1,4 +1,4 @@
-#ifndef ELLE_SERIALIZE_POINTER_HH
+#ifndef  ELLE_SERIALIZE_POINTER_HH
 # define ELLE_SERIALIZE_POINTER_HH
 
 namespace elle
@@ -6,59 +6,74 @@ namespace elle
   namespace serialize
   {
 
-    template<typename T> struct ArchiveSerializer;
-
     ///
     /// Handy class to serialize pointed values. This class does NOT provide a
     /// way to store memory address. It will store a boolean and the pointed
     /// value when the pointer is not null.
     ///
+    /// -----------------------------------------------------------------------
+    /// struct A { int* i; };
+    /// ELLE_SERIALIZE_SIMPLE(A, archive, value, version)
+    /// {
+    ///   archive & Pointer<int>(value.i); // i may or may not be null.
+    /// }
+    /// -----------------------------------------------------------------------
+    ///
+    ///
     /// @see elle::serialize::pointer(T*) to infer this type.
     ///
     template<typename T>
-      class Pointer
-      {
-        friend class ArchiveSerializer<Pointer<T>>;
-      private:
-        T*& _ptr;
-      public:
-        Pointer(T*& ptr) : _ptr(ptr) {}
-        Pointer(Pointer&& other) : _ptr(other._ptr) {}
-      };
+    class Pointer;
 
     ///
     /// Alive pointer acts the same as Pointer<T> class, except it will throw
     /// an error if the pointer is null.
     ///
+    /// -----------------------------------------------------------------------
+    /// struct A { int* i; };
+    /// ELLE_SERIALIZE_SIMPLE(A, archive, value, version)
+    /// {
+    ///   archive & AlivePointer<int>(value.i); // i should not be null.
+    /// }
+    /// -----------------------------------------------------------------------
+    ///
     template<typename T>
-      class AlivePointer
-      {
-        friend class ArchiveSerializer<AlivePointer<T>>;
-      private:
-        T*& _ptr;
-      public:
-        AlivePointer(T*& ptr) : _ptr(ptr) {}
-        AlivePointer(AlivePointer&& other) : _ptr(other._ptr) {}
-        AlivePointer(AlivePointer const& other) : _ptr(other._ptr) {}
-      };
+    class AlivePointer;
 
+    ///
     /// Infer the Pointer<T> type.
-    template<typename T>
-      Pointer<T> pointer(T*& ptr)
-      {
-        return Pointer<T>(ptr);
-      }
+    ///
+    /// -----------------------------------------------------------------------
+    /// struct A { int* i; };
+    /// ELLE_SERIALIZE_SIMPLE(A, archive, value, version)
+    /// {
+    ///   archive & pointer(value.i); // i may or may not be null.
+    /// }
+    /// -----------------------------------------------------------------------
+    ///
+    template<typename T> inline
+    Pointer<T>
+    pointer(T*& ptr);
 
+    ///
     /// Infer the AlivePointer<T> type.
-    template<typename T>
-      AlivePointer<T> alive_pointer(T*& ptr)
-      {
-        return AlivePointer<T>(ptr);
-      }
+    ///
+    /// -----------------------------------------------------------------------
+    /// struct A { int* i; };
+    /// ELLE_SERIALIZE_SIMPLE(A, archive, value, version)
+    /// {
+    ///   archive & alive_pointer(value.i); // i should not be null.
+    /// }
+    /// -----------------------------------------------------------------------
+    ///
+    template<typename T> inline
+    AlivePointer<T>
+    alive_pointer(T*& ptr);
+
   }
 }
 
-# include <elle/serialize/Pointer.hxx>
+# include "Pointer.hxx"
 
 #endif /* ! POINTER_HH */
 
