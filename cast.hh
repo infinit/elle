6d7e1ac375e9479fb38653dd327311aad698cb90
mefyl <quentin.hocquet@infinit.io>
2012-08-10 20:30:59 +0200

    Elle: add cast helper.

---
+++ cast.hh
@@ -0,0 +1,59 @@
+#ifndef ELLE_CAST_HH
+# define ELLE_CAST_HH
+
+namespace elle
+{
+  template <typename T>
+  struct cast
+  {
+    template <typename U>
+    static std::unique_ptr<T>
+    runtime(std::unique_ptr<U>& p)
+    {
+      T* res = dynamic_cast<T*>(p.get());
+      if (!res)
+        throw std::runtime_error(elle::sprintf("%s of type %s cannot be cast to type %s",
+                                               *p, typeid(*p).name(), typeid(T).name()));
+      p.release();
+      return std::unique_ptr<T>(static_cast<T*>(res));
+    }
+
+    template <typename U>
+    static std::unique_ptr<T>
+    runtime(std::unique_ptr<U>&& p)
+    {
+      T* res = dynamic_cast<T*>(p.get());
+      if (!res)
+        throw std::runtime_error(elle::sprintf("%s of type %s cannot be cast to type %s",
+                                               *p, typeid(*p).name(), typeid(T).name()));
+      p.release();
+      return std::unique_ptr<T>(static_cast<T*>(res));
+    }
+
+    // template <typename U>
+    // static T*
+    // static_(U* p)
+    // {
+    //   assert(dynamic_cast<T*>(u));
+    //   return static_cast<T*>(p);
+    // }
+
+    // template <typename U>
+    // static std::unique_ptr<T>
+    // static_(std::unique_ptr<U>&& p)
+    // {
+    //   assert(dynamic_cast<T*>(p.get()));
+    //   return std::unique_ptr<T>(static_cast<T*>(p.release()));
+    // }
+
+    // template <typename U>
+    // static T*
+    // static_(const std::unique_ptr<U>& p)
+    // {
+    //   assert(dynamic_cast<T*>(p.get()));
+    //   static_cast<T*>(p.get());
+    // }
+  };
+}
+
+#endif
