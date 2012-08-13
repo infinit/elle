3d65d4ada26034fb66d64d3c07f3f0e0a4c0f7d9
Raphaël Londeix <raphael.londeix@infinit.io>
2012-08-13 23:04:03 +0200

    improve: move json factory in its own header

--- format/json/Factory.hh
+++ format/json/Factory.hh
@@ -4,15 +4,16 @@
 # include <boost/call_traits.hpp>
 # include <type_traits>
 
-# include "fwd.hh"
+# include "Object.hh"
 
 // Helper to enable Factory::construct() method when passing argument by value.
 # define _ELLE_FORMAT_JSON_FACTORY_ENABLE_BY_VALUE(T)                         \
   typename std::enable_if<                                                    \
-    std::is_same<                                                             \
-          T const                                                             \
-        , typename boost::call_traits<T>::param_type                          \
-      >::value                                                                \
+        !std::is_pointer<T>::value                                            \
+    &&  std::is_same<                                                         \
+            T const                                                           \
+          , typename boost::call_traits<T>::param_type                        \
+        >::value                                                              \
     , std::unique_ptr<Object>                                                 \
   >::type                                                                     \
   /**/
@@ -20,10 +21,11 @@
 // Helper to enable Factory::construct() method when passing argument by ref.
 # define _ELLE_FORMAT_JSON_FACTORY_ENABLE_BY_REF(T)                           \
   typename std::enable_if<                                                    \
-    std::is_same<                                                             \
-          T const&                                                            \
-        , typename boost::call_traits<T>::param_type                          \
-      >::value                                                                \
+         std::is_array<T>::value                                              \
+    ||  std::is_same<                                                         \
+            T const&                                                          \
+          , typename boost::call_traits<T>::param_type                        \
+        >::value                                                              \
     , std::unique_ptr<Object>                                                 \
   >::type                                                                     \
   /**/
@@ -36,24 +38,29 @@ namespace elle
     {
 
       ///
-      /// Build a json object from an arbitrary value.
+      /// Build json objects from an arbitrary value.
       ///
       struct Factory
       {
+      public:
         template <typename T>
-        static
+        static inline
         _ELLE_FORMAT_JSON_FACTORY_ENABLE_BY_VALUE(T)
-        construct(T const value);
+        construct(T value);
 
         template <typename T>
-        static
+        static inline
         _ELLE_FORMAT_JSON_FACTORY_ENABLE_BY_REF(T)
         construct(T const& value);
+
+      protected:
+        struct Impl;
       };
 
     }
   }
 }
 
+# include "Factory.hxx"
 #endif
 
