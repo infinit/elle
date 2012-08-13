dfa3e38531a26e7a0e551807fe3b1ff8981e2175
Raphaël Londeix <raphael.londeix@infinit.io>
2012-08-13 18:00:50 +0200

    improve: start moving (and cleaning) json object factory.

--- format/json/Array.hxx
+++ format/json/Array.hxx
@@ -1,46 +1,52 @@
 #ifndef ELLE_FORMAT_JSON_ARRAY_HXX
 # define ELLE_FORMAT_JSON_ARRAY_HXX
 
-# include <boost/foreach.hpp>
-
-# include "Object.hxx"
-
-namespace elle { namespace format { namespace json {
-
-    template<typename Container>
-    Array::Array(Container const& container)
+namespace elle
+{
+  namespace format
+  {
+    namespace json
     {
-      static_assert(
-          detail::IsArray<Container>::value,
-          "The constructor argument is not an iterable"
-      );
-      BOOST_FOREACH(auto val, container)
-      {
-        this->push_back(val);
-      }
-    }
 
-    template<typename T> inline void Array::push_back(T const& value)
+      template <typename Container>
+      Array::Array(Container const& container)
+      {
+        static_assert(
+            detail::IsArray<Container>::value,
+            "The constructor argument is not an iterable"
+        );
+        for (auto& val : container)
+          this->push_back(val);
+      }
+
+      template <typename T>
+      inline
+      void
+      Array::push_back(T const& value)
       {
         _value.push_back(Object::Factory::Construct(value).release());
       }
 
-    // XXX should be "sequence" convertible, not just list
-    template<typename T>
-    Array::operator std::list<T>() const
-    {
-      std::list<T> result;
-      for (Object* obj : _value)
+      // XXX should be "sequence" convertible, not just list
+      template <typename T>
+      Array::operator std::list<T>() const
       {
-        result.push_back(obj->as<T>());
+        std::list<T> result;
+        for (Object* obj : _value)
+        {
+          result.push_back(obj->as<T>());
+        }
+        return result;
       }
-      return result;
-    }
 
-    Object& Array::operator[] (size_t index) const
-    {
-      return *_value.at(index);
+      Object&
+      Array::operator [](size_t index) const
+      {
+        return *_value.at(index);
+      }
+
     }
-}}}
+  }
+}
 
 #endif
