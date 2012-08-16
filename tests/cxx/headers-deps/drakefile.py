31a6470ce125bc6731715b00797ae9cfe13e05f9
mefyl <mefyl@gruntech.org>
2012-08-16 12:50:22 +0200

    Tests: Cxx: restore dependencies test.

--- tests/cxx/headers-deps/drakefile.py
+++
@@ -1,8 +0,0 @@
-import drake, drake.cxx
-
-def configure(cxx_toolkit):
-
-  cfg = drake.cxx.Config()
-  cfg.add_local_include_path('./.') # Remove when drake.cxx search in the dir of source files as expected
-  sources = drake.nodes('main.cc', 'test.cc')
-  executable = drake.cxx.Executable('test', sources, cxx_toolkit, cfg)
