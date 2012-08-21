99c63ad957ff79e04b37ee8a6b76ba8a9e973591
Raphaël Londeix <raphael.londeix@infinit.io>
2012-08-21 13:01:36 +0200

    typo

--- packages/libpkg/farmbuild.py
+++ packages/libpkg/farmbuild.py
@@ -67,7 +67,7 @@ class FarmBuild(Build):
 
         def _downloadTarball(self, dl, target,
                              print_status=True,
-                             chunck_size=4096 * 4):
+                             chunk_size=4096 * 4):
             size = 0
             with open(target, 'wb') as f:
                 while True:
