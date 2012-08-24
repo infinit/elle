640a6c636126539d2e509755acf8222187c7d094
mefyl <quentin.hocquet@infinit.io>
2012-08-24 12:42:09 +0200

    Elle: Printable: comment.

--- elle/Printable.hh
+++ elle/Printable.hh
@@ -5,20 +5,28 @@
 
 namespace elle
 {
+  /// Objects with a pretty string representation.
+  ///
+  /// Subclasses shall override print to define the pretty
+  /// representation and will be printable through the << operator.
   class Printable
   {
   public:
+    /// Destroy a Printable.
     virtual
     ~Printable();
 
+    /// Print pretty representation to \a stream.
     virtual
     void
     print(std::ostream& stream) const = 0;
 
+    /// The pretty representation.
     std::string
     stringify() const;
   };
 
+  /// Print pretty representation of \a o to \a stream.
   std::ostream& operator << (std::ostream& stream,
                              Printable const& o);
 }
