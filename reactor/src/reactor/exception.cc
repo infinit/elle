06473618a13c88ee527307b5e74186626f370ac5
mefyl <quentin.hocquet@infinit.io>
2012-08-24 11:08:53 +0200

    Reactor: exception: support nested exceptions, show backtrace upon printing.

--- reactor/src/reactor/exception.cc
+++ reactor/src/reactor/exception.cc
@@ -9,6 +9,18 @@ namespace reactor
     : std::runtime_error(message)
     , _scheduler(scheduler)
     , _backtrace(Backtrace::current())
+    , _inner(0)
+  {
+    if (Thread* t = scheduler.current())
+      _backtrace.strip_base(t->_backtrace_root);
+  }
+
+  Exception::Exception(Scheduler& scheduler, const std::string& message,
+                       Backtrace const& bt)
+    : std::runtime_error(message)
+    , _scheduler(scheduler)
+    , _backtrace(bt)
+    , _inner(0)
   {
     if (Thread* t = scheduler.current())
       _backtrace.strip_base(t->_backtrace_root);
@@ -17,10 +29,25 @@ namespace reactor
   Exception::~Exception() throw ()
   {}
 
+  Exception const*
+  Exception::inner_exception() const
+  {
+    return this->_inner;
+  }
+
+  void
+  Exception::inner_exception(Exception* e)
+  {
+    this->_inner = e;
+  }
+
   std::ostream&
   operator << (std::ostream& s, Exception const& e)
   {
-    s << e.what();
+    s << e.what() << std::endl;
+    s << e.backtrace();
+    if (Exception const* inner = e.inner_exception())
+      s << std::endl << "Exception was triggerd by:" << *inner;
     return s;
   }
 
