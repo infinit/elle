e3c4314fff20f51f04bc85b28acbb74a1f5f2af4
Raphaël Londeix <raphael.londeix@infinit.io>
2012-09-19 16:25:24 +0200

    add: base exception using elle::scheduler

---
+++ elle/Exception.hh
@@ -0,0 +1,27 @@
+#ifndef  ELLE_EXCEPTION_HH
+# define ELLE_EXCEPTION_HH
+
+# include <reactor/exception.hh>
+
+# include <elle/concurrency/Scheduler.hh>
+# include <elle/printf.hh>
+
+namespace elle
+{
+
+  class Exception:
+    public reactor::Exception
+  {
+  public:
+    template<typename... Args>
+    Exception(char const* fmt, Args&... args)
+      : reactor::Exception(elle::concurrency::scheduler(),
+                           elle::sprintf(fmt, args...))
+    {}
+
+  };
+
+}
+
+#endif
+
