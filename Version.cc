29ee0ccb895212c68a81277b4925745b67199f21
mefyl <quentin.hocquet@infinit.io>
2012-09-19 11:43:33 +0200

    Move Version in elle.

---
+++ Version.cc
@@ -0,0 +1,88 @@
+#include <elle/Version.hh>
+
+//
+// ---------- construction ----------------------------------------------------
+//
+
+Version::Version():
+  _major(0),
+  _minor(0)
+{
+}
+
+Version::Version(elle::Natural8 major,
+                 elle::Natural8 minor):
+  _major(major),
+  _minor(minor)
+{
+}
+
+//
+// ---------- methods ---------------------------------------------------------
+//
+
+elle::Natural8
+Version::major() const
+{
+  return (this->_major);
+}
+
+elle::Natural8
+Version::minor() const
+{
+  return (this->_minor);
+}
+
+//
+// ---------- printable -------------------------------------------------------
+//
+
+void
+Version::print(std::ostream& stream) const
+{
+  stream << static_cast<elle::Natural32>(this->_major)
+         << ":"
+         << static_cast<elle::Natural32>(this->_minor);
+}
+
+//
+// ---------- operators -------------------------------------------------------
+//
+
+elle::Boolean
+Version::operator ==(Version const& other) const
+{
+  return ((this->_major == other._major) && (this->_minor == other._minor));
+}
+
+elle::Boolean
+Version::operator !=(Version const& other) const
+{
+  return (!this->operator ==(other));
+}
+
+elle::Boolean
+Version::operator <(Version const& other) const
+{
+  return ((this->_major < other._major) ||
+          ((this->_major == other._major) && (this->_minor < other._minor)));
+}
+
+elle::Boolean
+Version::operator >(Version const& other) const
+{
+  return ((this->_major > other._major) ||
+          ((this->_major == other._major) && (this->_minor > other._minor)));
+}
+
+elle::Boolean
+Version::operator <=(Version const& other) const
+{
+  return (!this->operator >(other));
+}
+
+elle::Boolean
+Version::operator >=(Version const& other) const
+{
+  return (!this->operator <(other));
+}
