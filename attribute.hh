2e1d3f52f905728537e39dedf3c38d10206dce55
Julien Quintard <julien.quintard@infinit.io>
2012-09-21 12:22:48 +0200

    Updated the nucleus library with more accessors and throwable methods.

--- attribute.hh
+++ attribute.hh
@@ -5,37 +5,65 @@
 
 # include <boost/preprocessor/seq/cat.hpp>
 
-# define ELLE_ATTRIBUTE(Type, Name)             \
-  private:                                      \
-  Type BOOST_PP_CAT(_, Name);                   \
+# define ELLE_ATTRIBUTE(Type, Name)                                     \
+  private:                                                              \
+  Type BOOST_PP_CAT(_, Name);
 
-# define ELLE_ATTRIBUTE_R(Type, Name)                   \
-  ELLE_ATTRIBUTE(Type, Name)                            \
-  ELLE_ATTRIBUTE_R_(Type, Name)                         \
+# define ELLE_ATTRIBUTE_R_ACCESSOR(Type, Name)                          \
+  public:                                                               \
+  Type const&                                                           \
+  Name() const                                                          \
+  {                                                                     \
+    return (this->BOOST_PP_CAT(_, Name));                               \
+  }
 
-# define ELLE_ATTRIBUTE_X(Type, Name)                   \
-  ELLE_ATTRIBUTE(Type, Name)                            \
-  ELLE_ATTRIBUTE_X_(Type, Name)                         \
+# define ELLE_ATTRIBUTE_W_ACCESSOR(Type, Name)                          \
+  public:                                                               \
+  void                                                                  \
+  Name(Type const& name)                                                \
+  {                                                                     \
+    this->BOOST_PP_CAT(_, Name) = name;                                 \
+  }
 
-# define ELLE_ATTRIBUTE_RX(Type, Name)                   \
-  ELLE_ATTRIBUTE(Type, Name)                             \
-  ELLE_ATTRIBUTE_R_(Type, Name)                          \
-  ELLE_ATTRIBUTE_X_(Type, Name)                          \
+# define ELLE_ATTRIBUTE_X_ACCESSOR(Type, Name)                          \
+  public:                                                               \
+  Type&                                                                 \
+  Name()                                                                \
+  {                                                                     \
+    return (this->BOOST_PP_CAT(_, Name));                               \
+  }
 
-# define ELLE_ATTRIBUTE_R_(Type, Name)                  \
-  public:                                               \
-  typename std::remove_reference<Type>::type const&     \
-  Name() const                                          \
-  {                                                     \
-    return BOOST_PP_CAT(_, Name);                       \
-  }                                                     \
+# define ELLE_ATTRIBUTE_R(Type, Name)                                   \
+  ELLE_ATTRIBUTE(Type, Name)                                            \
+  ELLE_ATTRIBUTE_R_ACCESSOR(Type, Name)
 
-# define ELLE_ATTRIBUTE_X_(Type, Name)                  \
-  public:                                               \
-  typename std::remove_reference<Type>::type&           \
-  Name()                                                \
-  {                                                     \
-    return BOOST_PP_CAT(_, Name);                       \
-  }                                                     \
+# define ELLE_ATTRIBUTE_W(Type, Name)                                   \
+  ELLE_ATTRIBUTE(Type, Name)                                            \
+  ELLE_ATTRIBUTE_W_ACCESSOR(Type, Name)
+
+# define ELLE_ATTRIBUTE_X(Type, Name)                                   \
+  ELLE_ATTRIBUTE(Type, Name)                                            \
+  ELLE_ATTRIBUTE_X_ACCESSOR(Type, Name)
+
+# define ELLE_ATTRIBUTE_RW(Type, Name)                                  \
+  ELLE_ATTRIBUTE(Type, Name)                                            \
+  ELLE_ATTRIBUTE_R_ACCESSOR(Type, Name)                                 \
+  ELLE_ATTRIBUTE_W_ACCESSOR(Type, Name)
+
+# define ELLE_ATTRIBUTE_RX(Type, Name)                                  \
+  ELLE_ATTRIBUTE(Type, Name)                                            \
+  ELLE_ATTRIBUTE_R_ACCESSOR(Type, Name)                                 \
+  ELLE_ATTRIBUTE_X_ACCESSOR(Type, Name)
+
+# define ELLE_ATTRIBUTE_WX(Type, Name)                                  \
+  ELLE_ATTRIBUTE(Type, Name)                                            \
+  ELLE_ATTRIBUTE_W_ACCESSOR(Type, Name)                                 \
+  ELLE_ATTRIBUTE_X_ACCESSOR(Type, Name)
+
+# define ELLE_ATTRIBUTE_RWX(Type, Name)                                 \
+  ELLE_ATTRIBUTE(Type, Name)                                            \
+  ELLE_ATTRIBUTE_R_ACCESSOR(Type, Name)                                 \
+  ELLE_ATTRIBUTE_W_ACCESSOR(Type, Name)                                 \
+  ELLE_ATTRIBUTE_X_ACCESSOR(Type, Name)
 
 #endif
