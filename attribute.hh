42d2d07e8f4910ec50fc7a442c8601dc6e7923e4
mefyl <quentin.hocquet@infinit.io>
2012-09-24 18:58:26 +0200

    Elle: attribute: add X accessor.

--- attribute.hh
+++ attribute.hh
@@ -1,19 +1,41 @@
 #ifndef ELLE_ATTRIBUTE_HH
 # define ELLE_ATTRIBUTE_HH
 
+# include <type_traits>
+
 # include <boost/preprocessor/seq/cat.hpp>
 
 # define ELLE_ATTRIBUTE(Type, Name)             \
   private:                                      \
   Type BOOST_PP_CAT(_, Name);                   \
 
-# define ELLE_ATTRIBUTE_R(Type, Name)           \
-  public:                                       \
-  Type const&                                   \
-  Name() const                                  \
-  {                                             \
-    return BOOST_PP_CAT(_, Name);               \
-  }                                             \
-  ELLE_ATTRIBUTE(Type, Name)                    \
+# define ELLE_ATTRIBUTE_R(Type, Name)                   \
+  ELLE_ATTRIBUTE(Type, Name)                            \
+  ELLE_ATTRIBUTE_R_(Type, Name)                         \
+
+# define ELLE_ATTRIBUTE_X(Type, Name)                   \
+  ELLE_ATTRIBUTE(Type, Name)                            \
+  ELLE_ATTRIBUTE_X_(Type, Name)                         \
+
+# define ELLE_ATTRIBUTE_RX(Type, Name)                   \
+  ELLE_ATTRIBUTE(Type, Name)                             \
+  ELLE_ATTRIBUTE_R_(Type, Name)                          \
+  ELLE_ATTRIBUTE_X_(Type, Name)                          \
+
+# define ELLE_ATTRIBUTE_R_(Type, Name)                  \
+  public:                                               \
+  typename std::remove_reference<Type>::type const&     \
+  Name() const                                          \
+  {                                                     \
+    return BOOST_PP_CAT(_, Name);                       \
+  }                                                     \
+
+# define ELLE_ATTRIBUTE_X_(Type, Name)                  \
+  public:                                               \
+  typename std::remove_reference<Type>::type&           \
+  Name()                                                \
+  {                                                     \
+    return BOOST_PP_CAT(_, Name);                       \
+  }                                                     \
 
 #endif
