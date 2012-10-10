843921beb4bf0c7015781b41e1272ca3cc7fbf41
PICHOT Fabien <fabien.pichot@infinit.io>
2012-10-10 11:26:04 +0200

    func scripts: refactor check_convergenre, and update PYTHONPATH in test

--- tests/functional/scripts/write_serial.2.py
+++ tests/functional/scripts/write_serial.2.py
@@ -2,19 +2,13 @@
 
 import os
 import sys
+import infinutils
 
 mnt1 = os.environ['MNT1']
 mnt2 = os.environ['MNT2']
 
 path1 = os.path.join(mnt1, 'pif')
 path2 = os.path.join(mnt2, 'pif')
-
-def check_convergence(path1, path2):
-    with open(path1, 'r') as f1:
-        with open(path2, 'r')  as f2:
-            data1, data2 = f1.read(), f2.read()
-            if data1 != data2:
-                raise Exception("'%s' != '%s'" % (data1, data2))
 
 def write_serial(path1, path2):
     import time
@@ -28,7 +22,7 @@ def write_serial(path1, path2):
 
 try:
     write_serial(path1, path2)
-    check_convergence(path1, path2)
+    infinutils.check_convergence(path1, path2)
     print(write_serial.__name__, "passes")
 except Exception as err:
     sys.exit("test '{0}' failed: {1}".format(write_serial.__name__, str(err)))
