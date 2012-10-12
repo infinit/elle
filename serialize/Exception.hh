b61a376fbc3a2d1c4d62bfe97e5c88d6a83865e9
Raphaël Londeix <raphael.londeix@infinit.io>
2012-10-12 19:29:42 +0200

    add: exception class for elle::serialize module

---
+++ serialize/Exception.hh
@@ -0,0 +1,16 @@
+#ifndef  ELLE_SERIALIZE_EXCEPTION_HH
+# define ELLE_SERIALIZE_EXCEPTION_HH
+
+# include <elle/Exception.hh>
+
+namespace elle
+{
+  namespace serialize
+  {
+
+    typedef elle::Exception Exception;
+
+  }
+}
+
+#endif
