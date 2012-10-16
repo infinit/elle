9b2a56f999e5a3e97b8734ea8fa85d90bc2517a0
Julien Quintard <julien.quintard@infinit.io>
2012-10-16 17:59:46 +0200

    Added a all.hh file for including all the cryptographic module.

---
+++ cryptography/all.hh
@@ -0,0 +1,18 @@
+#ifndef ELLE_CRYPTOGRAPHY_ALL_HH
+# define ELLE_CRYPTOGRAPHY_ALL_HH
+
+# include <elle/cryptography/Cipher.hh>
+# include <elle/cryptography/Clear.hh>
+# include <elle/cryptography/Code.hh>
+# include <elle/cryptography/Digest.hh>
+# include <elle/cryptography/KeyPair.hh>
+# include <elle/cryptography/OneWay.hh>
+# include <elle/cryptography/Plain.hh>
+# include <elle/cryptography/PrivateKey.hh>
+# include <elle/cryptography/PublicKey.hh>
+# include <elle/cryptography/Random.hh>
+# include <elle/cryptography/SecretKey.hh>
+# include <elle/cryptography/Seed.hh>
+# include <elle/cryptography/Signature.hh>
+
+#endif
