84ddc2d7def1996e9163e85c3c11ff449585a7dd
Glyn Matthews <glyn.matthews@gmail.com>
2012-10-20 13:46:19 +0200

    Added build scripts for new URI project, ensured that all tests are built and running.

---
+++ include/network/uri.hpp
@@ -0,0 +1,14 @@
+// Copyright (c) Glyn Matthews 2012.
+// Copyright 2012 Dean Michael Berris <dberris@google.com>
+// Copyright 2012 Google, Inc.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef NETWORK_URI_HPP
+#define NETWORK_URI_HPP
+
+#include <network/uri/uri.hpp>
+#include <network/uri/uri_io.hpp>
+
+#endif // NETWORK_URI_HPP
