2fc751c10ab309bbb3fca243c7fb7648378f0edc
Julien Quintard <julien.quintard@infinit.io>
2012-10-28 21:02:49 +0100

    Introduced a ELLE_ASSERT() which throws.

--- assert.hh
+++ assert.hh
@@ -4,14 +4,23 @@
 # include <string>
 
 # include <elle/compiler.hh>
+# include <elle/types.hh>
+
+/// Throw if the condition is unmet.
+# define ELLE_ASSERT(_condition_)                                       \
+  do                                                                    \
+    {                                                                   \
+      if (!(_condition_))                                               \
+        throw elle::Exception("assertion failed (%s) at %s:%s",         \
+                              #_condition_, __FILE__, __LINE__);        \
+    } while (0)
 
 namespace elle
 {
   /// Print the message and abort program execution.
   ELLE_COMPILER_ATTRIBUTE_NORETURN
   void
-  abort(const std::string& msg);
-
+  abort(std::string const& msg);
   /// Abort the program. Flags unreachable code.
   ELLE_COMPILER_ATTRIBUTE_NORETURN
   void
