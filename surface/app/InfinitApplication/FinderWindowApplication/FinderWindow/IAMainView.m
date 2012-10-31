850b32d950613cda17704a3bb9387caaeb0013ca
Raphaël Londeix <raphael.londeix@infinit.io>
2012-10-31 15:53:46 +0100

    add: timer works with operation queue

---
+++ surface/app/InfinitApplication/FinderWindowApplication/FinderWindow/IAMainView.m
@@ -0,0 +1,28 @@
+//
+//  IAMainView.m
+//  FinderWindowApplication
+//
+//  Created by infinit on 10/31/12.
+//  Copyright (c) 2012 infinit. All rights reserved.
+//
+
+#import "IAMainView.h"
+
+@implementation IAMainView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
