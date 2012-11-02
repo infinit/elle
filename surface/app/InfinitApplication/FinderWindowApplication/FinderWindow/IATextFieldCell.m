dbdb59395ac77fdfab6ce76e2a2fd518d6e3f54e
Raphaël Londeix <raphael.londeix@infinit.io>
2012-11-02 16:14:04 +0100

    improve: Use placeholders strings instead of titles

---
+++ surface/app/InfinitApplication/FinderWindowApplication/FinderWindow/IATextFieldCell.m
@@ -0,0 +1,13 @@
+//
+//  IATextFieldCell.m
+//  FinderWindowApplication
+//
+//  Created by infinit on 11/2/12.
+//  Copyright (c) 2012 infinit. All rights reserved.
+//
+
+#import "IATextFieldCell.h"
+
+@implementation IATextFieldCell
+
+@end
