2c17439129a60ec4686718d3a19372b43ea175ed
Julien Quintard <julien.quintard@infinit.io>
2012-11-06 12:11:33 +0100

    Made use of ELLE_ASSERT instead of a hand-made macro-function or even the assert().

--- tests/unit/elle/cryptography/ec_oneway.cc
+++ tests/unit/elle/cryptography/ec_oneway.cc
@@ -1,16 +1,12 @@
-#include <iostream>
-
 #include <elle/cryptography/oneway.hh>
 #include <elle/cryptography/Plain.hh>
 #include <elle/cryptography/Digest.hh>
 
 #include <elle/format/hexadecimal.hh>
 
-#include <stdexcept>
+#include <elle/assert.hh>
 
-#define ASSERT(_condition_)                                             \
-  if (!(_condition_))                                                   \
-    throw std::runtime_error(#_condition_);
+#include <iostream>
 
 int main()
 {
@@ -23,7 +19,7 @@ int main()
       elle::cryptography::oneway::Algorithm::md5)};
   elle::Buffer md5_result{
     elle::format::hexadecimal::decode("a81cf1c99a9833e027c9d5a33e9b85cd")};
-  ASSERT(md5_digest.buffer() == md5_result);
+  ELLE_ASSERT(md5_digest.buffer() == md5_result);
 
   elle::cryptography::Digest sha_digest{
     elle::cryptography::oneway::hash(
@@ -32,7 +28,7 @@ int main()
   elle::Buffer sha_result{
     elle::format::hexadecimal::decode("51f973db2070983d562bd5d744eea4e5"
                                       "47de09fe")};
-  ASSERT(sha_digest.buffer() == sha_result);
+  ELLE_ASSERT(sha_digest.buffer() == sha_result);
 
   elle::cryptography::Digest sha1_digest{
     elle::cryptography::oneway::hash(
@@ -41,7 +37,7 @@ int main()
   elle::Buffer sha1_result{
     elle::format::hexadecimal::decode("1e408a2e1dace99ab9b3035f371417e6"
                                       "84c088fc")};
-  ASSERT(sha1_digest.buffer() == sha1_result);
+  ELLE_ASSERT(sha1_digest.buffer() == sha1_result);
 
   elle::cryptography::Digest sha224_digest{
     elle::cryptography::oneway::hash(
@@ -50,7 +46,7 @@ int main()
   elle::Buffer sha224_result{
     elle::format::hexadecimal::decode("909300b8bb726fbe15199967a2648855"
                                       "fb9bedf608c0446b366935b0")};
-  ASSERT(sha224_digest.buffer() == sha224_result);
+  ELLE_ASSERT(sha224_digest.buffer() == sha224_result);
 
   elle::cryptography::Digest sha256_digest{
     elle::cryptography::oneway::hash(
@@ -59,7 +55,7 @@ int main()
   elle::Buffer sha256_result{
     elle::format::hexadecimal::decode("b8bb6458a32b31734314985cfe300cec"
                                       "4d4a592a45c9859695bf2a2bdc9c0446")};
-  ASSERT(sha256_digest.buffer() == sha256_result);
+  ELLE_ASSERT(sha256_digest.buffer() == sha256_result);
 
   elle::cryptography::Digest sha384_digest{
     elle::cryptography::oneway::hash(
@@ -69,7 +65,7 @@ int main()
     elle::format::hexadecimal::decode("a1ded70921fbe3afa87c856596de58a1"
                                       "a4e0cb43369c5df1ffe9a70744360f15"
                                       "74380c302077a5c92113a8f3d2b3cb68")};
-  ASSERT(sha384_digest.buffer() == sha384_result);
+  ELLE_ASSERT(sha384_digest.buffer() == sha384_result);
 
   elle::cryptography::Digest sha512_digest{
     elle::cryptography::oneway::hash(
@@ -80,7 +76,7 @@ int main()
                                       "77b2621da77e9496edb667741c437c7a"
                                       "61f9f5436989d11a93830adc530235bd"
                                       "c34efb258fd05ca32688f5c809ea199f")};
-  ASSERT(sha384_digest.buffer() == sha384_result);
+  ELLE_ASSERT(sha384_digest.buffer() == sha384_result);
 
   std::cout << "tests done.\n";
 
