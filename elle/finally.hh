c5f5e83f1831a04750932984c530a7060b686350
mefyl <quentin.hocquet@infinit.io>
2012-11-08 12:09:13 +0100

    Elle: Finally: support release().

--- elle/finally.hh
+++ elle/finally.hh
@@ -1,28 +1,30 @@
 #ifndef ELLE_FINALLY_HH
 # define ELLE_FINALLY_HH
 
-# include <boost/preprocessor/cat.hpp>
+namespace elle
+{
+  class Finally
+  {
+  public:
+    Finally(std::function<void()> const& action)
+      : _action(action)
+    {}
 
-# define ELLE_FINALLY(Lambda)                                   \
-  auto BOOST_PP_CAT(_Finally_action_, __LINE__) = Lambda;       \
-  class BOOST_PP_CAT(_Finally_, __LINE__)                       \
-  {                                                             \
-    public:                                                     \
-    inline                                                      \
-      BOOST_PP_CAT(_Finally_, __LINE__)                         \
-      (decltype(BOOST_PP_CAT(_Finally_action_, __LINE__))       \
-       const& action)                                           \
-      : _action(action)                                         \
-    {}                                                          \
-    inline                                                      \
-      ~BOOST_PP_CAT(_Finally_, __LINE__)()                      \
-    {                                                           \
-      this->_action();                                          \
-    }                                                           \
-    private:                                                    \
-    decltype(BOOST_PP_CAT(_Finally_action_, __LINE__))          \
-      const& _action;                                           \
-  } BOOST_PP_CAT(_finally_, __LINE__)                           \
-    (BOOST_PP_CAT(_Finally_action_, __LINE__));                 \
+    ~Finally()
+    {
+      if (_action)
+        _action();
+    }
+
+    void
+    release()
+    {
+      _action = std::function<void()>();
+    }
+
+  private:
+    std::function<void()> _action;
+  };
+}
 
 #endif
