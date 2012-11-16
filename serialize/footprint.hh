f201c786936048a1da480add2eefb1e9ffd0364f
Julien Quintard <julien.quintard@infinit.io>
2012-11-16 10:41:13 +0100

    Made Porcupine recompile. The main test does not work yet!

--- serialize/footprint.hh
+++ serialize/footprint.hh
@@ -1,4 +1,4 @@
-#ifndef  ELLE_SERIALIZE_FOOTPRINT_HH
+#ifndef ELLE_SERIALIZE_FOOTPRINT_HH
 # define ELLE_SERIALIZE_FOOTPRINT_HH
 
 # include "BinaryArchive.hh"
@@ -7,15 +7,22 @@ namespace elle
 {
   namespace serialize
   {
+    /// The size of the footprint.
+    typedef elle::Natural32 FootprintSize;
 
     /// Compute the size of an object T serialized with a Archive.
     template <typename Archive = OutputBinaryArchive, typename T>
-    size_t
+    FootprintSize
     footprint(T&& value);
 
+    /// Compute the size of a type T once serialized. Note that
+    /// an Object is instantiated through its default constructor.
+    template <typename T>
+    FootprintSize
+    footprint();
   }
 }
 
-# include "footprint.hxx"
+# include <elle/serialize/footprint.hxx>
 
 #endif
