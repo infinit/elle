04fcbd1eedc2f8a0cd617565f721a4e82dc2f93a
PICHOT Fabien <fabien.pichot@infinit.io>
2012-11-19 14:55:35 +0100

    elle unit tests: remove the dependecies to all infinit libraries.

--- tests/unit/elle/nat/nat_hole_ctor.cc
+++ tests/unit/elle/nat/nat_hole_ctor.cc
@@ -1,10 +1,11 @@
+#include <elle/concurrency/Scheduler.hh>
 #include <elle/nat/Nat.hh>
 
 
 int
 main(int , const char *[])
 {
-    elle::nat::NAT a;
+    elle::nat::NAT a(elle::concurrency::scheduler());
 
     (void)a;
     return 0;
