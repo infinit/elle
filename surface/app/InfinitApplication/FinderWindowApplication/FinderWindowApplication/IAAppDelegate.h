f6878c832af7c5245baf8b516d603bffb87b3e43
Raphaël Londeix <raphael.londeix@infinit.io>
2012-11-20 09:35:16 +0100

    add: Delegate for main application controller.

---
+++ surface/app/InfinitApplication/FinderWindowApplication/FinderWindowApplication/IAAppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  IAAppDelegate.h
+//  FinderWindowApplication
+//
+//  Created by infinit on 10/25/12.
+//  Copyright (c) 2012 infinit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IAAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (retain) NSWindowController *window_controller;
+@end
