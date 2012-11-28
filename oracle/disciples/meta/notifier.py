11b5b702ff217b603ec452fdecfdc5cbdcefd840
antony <antony.mechin@infinit.io>
2012-11-28 10:54:21 +0100

    meta: add timestamp to notification, fix indent (diff unreadable).

--- oracle/disciples/meta/notifier.py
+++ oracle/disciples/meta/notifier.py
@@ -7,6 +7,7 @@ import meta.page
 import database
 
 from twisted.python import log
+import time
 
 FILE_TRANSFER = 7
 FILE_TRANSFER_STATUS = 11
@@ -15,75 +16,77 @@ MESSAGE = 217
 LOGGED_IN = 666
 
 class Notifier(object):
-
-        def open(self):
-                raise Exception('Not implemented')
-	def notify_one(self, notif_id, to, message):
-                raise Exception('Not implemented')
-	def notify_some(self, notif_id, to, message):
-                raise Exception('Not implemented')
-        def send_notification(self, message):
-                raise Exception('Not implemented')
-	def close(self):
-                raise Exception('Not implemented')
+    def open(self):
+        raise Exception('Not implemented')
+    def notify_one(self, notif_id, to, message):
+        raise Exception('Not implemented')
+    def notify_some(self, notif_id, to, message):
+        raise Exception('Not implemented')
+    def send_notification(self, message):
+        raise Exception('Not implemented')
+    def close(self):
+        raise Exception('Not implemented')
 
 class TrophoniusNotify(Notifier):
-	def __init__(self):
-		self.conn = socket.socket()
+    def __init__(self):
+        self.conn = socket.socket()
 
-	def open(self):
-		self.conn.connect(("localhost", 23457))
+    def open(self):
+        self.conn.connect(("localhost", 23457))
 
         def send_notification(self, message):
-                if isinstance(message, dict):
-			msg = json.dumps(message, default = str)
-		# elif isinstance(message, str):
-		# 	msg = message + json.dumps(_dict, default = str)
-                else:
-                        log.err('Notification was bad formed.')
+            if isinstance(message, dict):
+                msg = json.dumps(message, default = str)
+            # elif isinstance(message, str):
+            #     msg = message + json.dumps(_dict, default = str)
+            else:
+                log.err('Notification was bad formed.')
 
-                print("{}\n".format(msg))
-		self.conn.send("{}\n".format(msg))
+            # XXX: Log to remove.
+            print("{}\n".format(msg))
+            self.conn.send("{}\n".format(msg))
 
         def _add_notif_to_db(self, recipient_id, notif):
-                user_ = database.users().find_one(
-                            database.ObjectId(recipient_id));
+            user_ = database.users().find_one(
+                database.ObjectId(recipient_id));
 
-                if not user_:
-                        return None
+            if not user_:
+                return None
 
-                user_['notifications'].append(notif)
-                database.users().save(user_)
+            #Timestamp in ms.
+            notif['timestamp'] = int(time.time() * 1000)
 
-                return user_
+            user_['notifications'].append(notif)
+            database.users().save(user_)
 
-	def notify_one(self, notification_id, recipient_id, message):
+            return user_
 
-                message['notification_id'] = notification_id;
+        def notify_one(self, notification_id, recipient_id, message):
+            message['notification_id'] = notification_id;
 
-                user_ = self._add_notif_to_db(recipient_id, message)
+            user_ = self._add_notif_to_db(recipient_id, message)
 
-                if not user_:
-                        log.err("Unknown user.")
-                        return
+            if not user_:
+                log.err("Unknown user.")
+                return
 
-                if user_['connected']:
-                        message.update({'to': str(recipient_id)})
-                        self.send_notification(message)
+            if user_['connected']:
+                message.update({'to': str(recipient_id)})
+                self.send_notification(message)
 
         def notify_some(self, notification_id, recipients_id, message):
-                if not isinstance(recipients_id, list):
-                        return self.notify_one(notification_id, recipients_id, message)
+            if not isinstance(recipients_id, list):
+                return self.notify_one(notification_id, recipients_id, message)
 
-                # Recipients empty.
-                if not recipients_id:
-                        return
+            # Recipients empty.
+            if not recipients_id:
+                return
 
-                message.update({'notification_id' : notification_id})
+            message.update({'notification_id' : notification_id})
 
-                for _id in recipients_id:
-                        self._add_notif_to_db(_id, message)
+            for _id in recipients_id:
+                self._add_notif_to_db(_id, message)
 
-                message.update({'to': recipients_id})
+            message.update({'to': recipients_id})
 
-                self.send_notification(message)
+            self.send_notification(message)
