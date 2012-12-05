8df2a7a8d4093cf13a1dc41976b51edbb4d0443b
Glyn Matthews <glyn.matthews@gmail.com>
2012-12-05 22:11:07 +0100

    Added a file for URI resolve tests.

---
+++ test/uri_resolve_test.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) Glyn Matthews 2012.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <gtest/gtest.h>
+#include <network/uri.hpp>
+
+const network::uri base_uri("http://a/b/c/d;p?q");
