001c90acfc650c111a7c7ba9aa12c542426b3013
Antony MECHIN <antony.mechin@infinit.io>
2012-12-12 16:34:30 +0100

    meta: make user search REALLY case insensitive.

--- oracle/disciples/meta/resources/user.py
+++ oracle/disciples/meta/resources/user.py
@@ -38,7 +38,7 @@ class Search(Page):
             )
         else:
             users = database.users().find(
-                {'email' : {'$regex' : '^%s' %text}},
+                {'email' : {'$regex' : '^%s' %text, '$options': 'i'}},
                 fields=["_id"],
                 limit=count + offset
         )
