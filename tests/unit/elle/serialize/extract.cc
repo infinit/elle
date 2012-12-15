2b3d3c1557172a978278ad46ca6455f995d1edbc
Julien Quintard <julien.quintard@infinit.io>
2012-12-15 18:10:31 +0100

    Fixed the extract test.

--- tests/unit/elle/serialize/extract.cc
+++ tests/unit/elle/serialize/extract.cc
@@ -1,6 +1,8 @@
 #include <elle/serialize/extract.hh>
 
-#include <lune/Descriptor.hh>
+#include <cryptography/KeyPair.hh>
+// XXX[temporary: for cryptography]
+using namespace infinit;
 
 int main()
 {
@@ -10,15 +12,16 @@ int main()
 
   // This string has been generated as follows:
   //
-  //   elle::io::Unique unique;
-  //   if (descriptor.Save(unique) == elle::Status::Error)
-  //     escape("XXX");
-  //   std::cout << unique << std::endl;
-  std::string descriptor_str("AAAFAAAAbG9jYWwAAAAAgAC+qKTe1Ib9HXaGWlEu03vIH/dgIHafJtYZwS4nVjH0uzX2ov+AaVoIYaTtwQ+WKRbzUaEUqEvUYOlw0xRjytXcvuEjGvJZqjPjJJQbK4xMQgaeWsPGc9pAw15PeyBhNJURij5jFuMkFjIRrZVnfRTwr3D0kmBA4DwACy+QUqoz1wAAAwABAAEAAAEAAAACAAAAAAAFAAAAbG9jYWwDAAEAAAAAACAAAAAAAAAAz7SW68DZiIChmRkO5q035WhXuNYwvGxIAtgamz9eU6IAAAIAAAAAAAUAAABsb2NhbAMABgAAAAAAIAAAAAAAAAAdNugLAMidIDjXbAsGr01ufXGI2OQp/x+8N0UYhXN/qQAAIAAAAAAAAAACAAAAAAAAXB9h3cUoDKCZFJQqqlSz9l2NKLMrRUc43FU3slmLESDOFZzMg2TJ4aJmZFIAuZZOwMRC7zi4UebSmvq1wF62e5w7zYDx13ttWiEPjgggfdTFnPWKjkBy+wP6bXH+qu/8ovFVAw6y0aXb4VwIhZuTl+05WfT+KNslZdusbRHqvds962RmsTjnLzOXKkFoNJf4a3wjq8TfUU2JkGJeXtQTuqYKZ4MlXSLDKxy9jJIQO/FIsYOM16a3MVVZ/UmD6DG11iomIosPAXvuIEuX0t2/16cpZ4QsL0RzgVXKoPau2HsUOtPliyKCcoLzIgTL/Jyhur5kHNqhR2HOX99wPa9u5WWJT/wsZlSbNqLPyh+YFOoLUDX4pdOhBm+4/xqcAzzc8ZmD4/64navPQ0+j98c2Ca8mdOaKTjqqAdve8u3LkI0SGZ3ghXBum6NVIzvo9D7l1zaiUygumhfZbbmE4JA2Fn15GJrmhkxnGlIK4S7Pl5n5W8tO/2S4jSMlIlq1q5EvzlE9TSqXKXnG10+dw94IPOD2u+sUqC9lVhZnEOnKHAe6/xnS2pxFVV3VZI9xKEU1l4IpqTlusKXOfgcdfKZpPdxPhIgN+HbYfTtJjS1FbJBB0LAWg9DFk+GhBx2sQPLZ3iwCXRY2qhJqVAUjrJ/mxG7h+RagY3w+6XKlL7FAVIsFAAAAbG9jYWwDAAEAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAAAAAAAAqXOVscW+qosSevFvYwd3ppGG9QkEq/kWs5ZI6sWpyBgu2/HIqynRGADr0bqb58BIr3QMHbSnnptsfU0lFpul9OA7Yga3X1tsBAOwCEB+Llikmh32KaPa2jUJ8LlQb3CpXprRuGUYJMJbUld41JYzDm7YcLmenQOBZV+2zOqmW0Y=");
+  //     elle::io::Unique unique;
+  //     cryptography::KeyPair kp = cryptography::KeyPair::generate(1024);
+  //     if (kp.Save(unique) == elle::Status::Error)
+  //       escape("XXX");
+  //     std::cout << unique << std::endl;
+  std::string string("AAAAAAAAgADEfggzknOW/UsxKwhRszX+Ie3IWsFKQwQt9S6vJn98fUi13CXGub6OwOAezIhab004FB3tsC+LcPd7xz6ZNxUcGDwhWgmezS0DeopgwFvMEPASrNW2IcPebGKBC6BdX4x+Artx1O8JUBLd7vGfMkxVlS7VWgaYi9fa21klif6/JwAAAwABAAEAAAAAgADEfggzknOW/UsxKwhRszX+Ie3IWsFKQwQt9S6vJn98fUi13CXGub6OwOAezIhab004FB3tsC+LcPd7xz6ZNxUcGDwhWgmezS0DeopgwFvMEPASrNW2IcPebGKBC6BdX4x+Artx1O8JUBLd7vGfMkxVlS7VWgaYi9fa21klif6/JwAAAwABAAEAAIAAvl0wCu7W57BsGa2MOML8C0LJwPMUEqNMuE2yteQBXRVvzSkEvDwV/u2j9JLJg3wTfsIWoMGxvlFqo1PMNNolqsiNMvixi7RYw1g4Mj6vh7lDavdzja9q6jJx/7LYK04StXs1DSrNQe343G95TRQ5FNVyL02X+uCrAePSMONwE4EAAEAA56zqzeRE/tULy0Secx/F8+VwVmhk/yU0g7BL254qAIySaWisJz1lC1eVzxJpm9ZbpIKDBHWl+FyCfxq90NuWQQAAQADZH3IRQBGhdUsGYmmAse2i6NJEDB5PB3i1tBDfWqxFmjl2zsoEd7Y615F0weYfx6/54MMVlhMEvU86ilsjEItnAABAAA08M63PfKGSSjoykVrCVey5DtHg5OQum/FqAOtq/btKTJL5B9y2BS/AsS5DPwVIGbQsQI/gqLqnjkz0miN0aAEAAEAABc0QSCO1FOJEYeRJO1YFql+cFpNYT/Y8JGauOLl8dtObC3CU9y+Xn9TYnwz7MAguzHdT3wTGd07zifXeX352qwAAQAARXLikv1SNErdzah7icGFyf6RhegmU2wJ6bJ8Snx5Fu9Y1upSd68L9oXhb1jW+AbzSPBBvJ1ZmRx8ONW315m1i");
 
-  auto extractor = from_string<InputBase64Archive>(descriptor_str);
+  auto extractor = from_string<InputBase64Archive>(string);
 
-  lune::Descriptor descriptor{extractor};
+  cryptography::KeyPair pair{extractor};
 
   std::cout << "tests done." << std::endl;
   return 0;
