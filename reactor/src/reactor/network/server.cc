83882c8eb2dea7635a30ff0c9fd14f2d863d4775
mefyl <quentin.hocquet@infinit.io>
2012-12-17 16:18:58 +0100

    Reactor: add protocol factories for socket and servers.

--- reactor/src/reactor/network/server.cc
+++ reactor/src/reactor/network/server.cc
@@ -1,4 +1,6 @@
 #include <reactor/network/server.hh>
+#include <reactor/network/tcp-server.hh>
+#include <reactor/network/udt-server.hh>
 
 namespace reactor
 {
@@ -15,6 +17,20 @@ namespace reactor
     Server::scheduler()
     {
       return _sched;
+    }
+
+    std::unique_ptr<Server>
+    Server::create(Protocol protocol, Scheduler& sched)
+    {
+      switch (protocol)
+        {
+          case Protocol::tcp:
+            return std::unique_ptr<Server>(new TCPServer(sched));
+          case Protocol::udt:
+            return std::unique_ptr<Server>(new UDTServer(sched));
+          default:
+            elle::unreachable();
+        }
     }
   }
 }
