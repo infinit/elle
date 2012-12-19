5cb489603b4a3db528dcbb7fa3d4925fa1637d72
PICHOT Fabien <fabien.pichot@infinit.io>
2012-12-19 12:07:33 +0100

    nat: allow to retrieve a nat::Hole instance
    The punch member function now returns a nat::Hole instance.
    
    Fix the unit test according to the changes.

--- nat/Nat.cc
+++ nat/Nat.cc
@@ -8,93 +8,107 @@
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/classification.hpp>
 
-namespace elle {
-namespace nat {
-
-namespace {
-
-namespace ec = elle::concurrency;
-namespace rnet = reactor::network;
-
-}
-
-// Class Hole {{{
-
-Hole::Hole(reactor::Scheduler &sched,
-           std::string const &hostname,
-           std::string const &port)
-    : _handle(new rnet::UDPSocket(sched, hostname, port))
-    , _public_endpoint{"", 0}
+namespace elle
 {
-}
+  namespace nat
+  {
 
-Hole::Hole(reactor::Scheduler &sched,
-           std::string const &hostname,
-           int port)
-    : _handle(new rnet::UDPSocket(sched, hostname, port))
-    , _public_endpoint{"", 0}
-{
-}
+  namespace {
 
-Hole::~Hole()
-{
-}
+    namespace ec = elle::concurrency;
+    namespace rnet = reactor::network;
 
-void
-Hole::sayHello(void)
-{
-    char msg[] = "hello\n";
+  }
+
+  // Class Hole {{{
+
+  Hole::Hole(reactor::Scheduler &sched,
+             std::string const &hostname,
+             std::string const &port)
+      : _handle{new rnet::UDPSocket{sched, hostname, port}}
+      , _public_endpoint{"", 0}
+  {
+  }
+
+  Hole::Hole(reactor::Scheduler &sched,
+             std::string const &hostname,
+             int port)
+      : _handle{new rnet::UDPSocket{sched, hostname, port}}
+      , _public_endpoint{"", 0}
+  {
+  }
+
+  Hole::Hole(Hole&& other):
+    _handle{std::move(other._handle)}
+  {}
+
+  Hole::~Hole()
+  {
+    this->say("quit");
+  }
+
+  void
+  Hole::sayHello(void)
+  {
+    this->say("hello");
+  }
+
+  void
+  Hole::say(std::string const msg)
+  {
+    if (this->_handle)
+      {
+        this->_handle->write(rnet::Buffer(msg + "\n"));
+      }
+  }
+
+  std::string const
+  Hole::get()
+  {
     std::string resp;
 
     resp.resize(512);
-    this->_handle->write(rnet::Buffer(msg, sizeof(msg)));
-}
+    this->_handle->read_some(rnet::Buffer(resp));
+    return resp;
+  }
 
-void
-Hole::exchangeIP(void)
-{
+  void
+  Hole::exchangeIP(void)
+  {
     std::string resp;
     std::stringstream ss;
     std::vector<std::string> splited;
 
-    ss << "local " << this->_handle->local_endpoint() << std::endl;
-    resp.resize(512);
-    this->_handle->write(rnet::Buffer(ss.str()));
-    this->_handle->read_some(rnet::Buffer(resp));
+    ss << "local " << this->_handle->local_endpoint();
+    this->say(ss.str());
+    resp = this->get();
 
     boost::split(splited, resp, boost::is_any_of(" :"));
-    // The format of the msg returned by the server is:
-    // local hostname:port
-    // so,after the split we have:
-    // ["local", "hostname", "port"]
     this->_public_endpoint = std::make_pair(splited[1], std::stoi(splited[2]));
-}
+  }
 
-void
-Hole::drill(void)
-{
+  void
+  Hole::drill(void)
+  {
     this->sayHello();
     this->exchangeIP();
-}
+  }
 
-// class Hole }}}
+  // class Hole }}}
 
-// class KeepAlive {{{
+  // class KeepAlive {{{
 
-KeepAlive::KeepAlive(reactor::Scheduler& sched,
-                     u_ptr<rnet::UDPSocket>&& s)
-    : sched(sched)
-    , handle(std::move(s))
-{
-}
+  KeepAlive::KeepAlive(reactor::Scheduler& sched,
+                       u_ptr<rnet::UDPSocket>&& s):
+      sched(sched) , handle(std::move(s))
+  {}
 
-KeepAlive::~KeepAlive()
-{
-}
+  KeepAlive::~KeepAlive()
+  {}
 
-void
-KeepAlive::run(void)
-{
+  void
+  KeepAlive::run(void)
+  {
     std::string  buf;
 
     buf.resize(128);
@@ -106,42 +120,41 @@ KeepAlive::run(void)
         handle->read_some(rnet::Buffer(buf));
         s.run();
     }
-}
+  }
 
-void
-KeepAlive::stop(void)
-{
+  void
+  KeepAlive::stop(void)
+  {
     this->running = false;
-}
+  }
 
-// class KeepAlive }}}
+  // class KeepAlive }}}
 
-// class NAT {{{
+  // class NAT {{{
 
-NAT::NAT(reactor::Scheduler &s)
-    : sched(s)
-    , alive()
-{
-}
+  NAT::NAT(reactor::Scheduler &s):
+      sched(s),
+      alive()
+  {}
 
-NAT::~NAT()
-{
+  NAT::~NAT()
+  {
     if (this->alive)
-        this->alive->terminate();
-}
+      this->alive->terminate();
+  }
 
-std::pair<std::string, uint16_t>
-NAT::punch(std::string const &hostname,
-      int port)
-{
+  Hole
+  NAT::punch(std::string const &hostname,
+             int port)
+  {
     Hole h(sched, hostname, port);
 
     h.drill();
-    return h.public_endpoint();
-}
+    return h;
+  }
 
 
-// class NAT }}}
+  // class NAT }}}
 
-} /* nat */
+  } /* nat */
 } /* elle */
