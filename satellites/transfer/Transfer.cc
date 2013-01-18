51785d65b72914c55f0a2b2da3387cf34f015817
mefyl <quentin.hocquet@infinit.io>
2013-01-18 17:25:15 +0100

    Sattelites: adapt to new RPC handshake.

--- satellites/transfer/Transfer.cc
+++ satellites/transfer/Transfer.cc
@@ -97,7 +97,7 @@ namespace satellite
                                         *socket);
     Transfer::channels =
       new infinit::protocol::ChanneledStream(elle::concurrency::scheduler(),
-                                             *serializer, true);
+                                             *serializer);
     Transfer::rpcs = new etoile::portal::RPC(*channels);
 
     if (!Transfer::rpcs->authenticate(phrase.pass))
