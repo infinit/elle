f5091c39de770ab934b58351970c55c9c0369982
PICHOT Fabien <fabien.pichot@infinit.io>
2013-01-28 16:51:51 +0100

    fix unit test sufix

--- tests/unit/elle/network/interface_autoip.cc
+++ tests/unit/elle/network/interface_autoip.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int
-main(int argc, const char *argv[])
+main()
 {
   auto interfaces = elle::network::Interface::get_map(
       elle::network::Interface::Filter::only_up
@@ -14,5 +14,6 @@ main(int argc, const char *argv[])
       std::cout << "IP: " << pair.second.ipv4_address << std::endl
                 << "MAC: " << pair.second.mac_address << std::endl;
   }
+  std::cout << "tests done." << std::endl;
   return (0);
 }
