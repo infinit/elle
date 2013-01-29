f8f62029489cdc4df5ede76be0193b66c4c09653
Julien Quintard <julien.quintard@infinit.io>
2013-01-29 16:06:26 +0100

    cryptography: Improved the initialization/cleaning so as to avoid memory leaks while minimizing the number of operations.

--- sources/cryptography/rsa/keypair.cc
+++ sources/cryptography/rsa/keypair.cc
@@ -1,6 +1,7 @@
 #include <cryptography/rsa/keypair.hh>
 #include <cryptography/finally.hh>
 
+#include <elle/attribute.hh>
 #include <elle/assert.hh>
 #include <elle/log.hh>
 
@@ -18,6 +19,57 @@ namespace infinit
     {
       namespace keypair
       {
+        /*--------.
+        | Classes |
+        `--------*/
+
+        // The key pair initializer.
+        class Initializer
+        {
+        public:
+          Initializer():
+            _context(nullptr)
+          {
+            // Create the context for the RSA algorithm.
+            if ((this->_context =
+                 ::EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, nullptr)) == nullptr)
+              throw elle::Exception("%s",
+                                    ::ERR_error_string(ERR_get_error(),
+                                                       nullptr));
+
+            // Initialise the context for key generation.
+            if (::EVP_PKEY_keygen_init(this->_context) <= 0)
+              throw elle::Exception("%s",
+                                    ::ERR_error_string(ERR_get_error(),
+                                                       nullptr));
+          }
+
+          ~Initializer()
+          {
+            // Release the generation context.
+            ::EVP_PKEY_CTX_free(this->_context);
+          }
+
+        private:
+          ELLE_ATTRIBUTE_R(::EVP_PKEY_CTX*, context);
+        };
+
+        /*-----------------.
+        | Static Functions |
+        `-----------------*/
+
+        /// Construct the key pair generation context.
+        static
+        ::EVP_PKEY_CTX*
+        _context()
+        {
+          ELLE_DEBUG_FUNCTION("");
+
+          static Initializer initialized;
+
+          return (initialized.context());
+        }
+
         /*----------.
         | Functions |
         `----------*/
@@ -30,22 +82,8 @@ namespace infinit
           // Make sure the cryptographic system is set up.
           cryptography::require();
 
-          ::EVP_PKEY_CTX* context = nullptr;
-
-          // Create the context for the RSA algorithm.
-          if ((context =
-               ::EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, nullptr)) == nullptr)
-            throw elle::Exception("%s",
-                                  ::ERR_error_string(ERR_get_error(), nullptr));
-
-          INFINIT_CRYPTOGRAPHY_FINALLY_ACTION_FREE_EVP_PKEY_CONTEXT(context);
-
-          ELLE_ASSERT(context != nullptr);
-
-          // Initialise the context for key generation.
-          if (::EVP_PKEY_keygen_init(context) <= 0)
-            throw elle::Exception("%s",
-                                  ::ERR_error_string(ERR_get_error(), nullptr));
+          // Retrieve the key pair generation context.
+          static ::EVP_PKEY_CTX* context = _context();
 
           // Set the key length in the keypair generation context.
           if (::EVP_PKEY_CTX_set_rsa_keygen_bits(context, length) <= 0)
@@ -58,10 +96,6 @@ namespace infinit
           if (::EVP_PKEY_keygen(context, &key) <= 0)
             throw elle::Exception("%s",
                                   ::ERR_error_string(ERR_get_error(), nullptr));
-
-          // Release the generation context.
-          ::EVP_PKEY_CTX_free(context);
-          INFINIT_CRYPTOGRAPHY_FINALLY_ABORT(context);
 
           INFINIT_CRYPTOGRAPHY_FINALLY_ACTION_FREE_EVP_PKEY(key);
 
