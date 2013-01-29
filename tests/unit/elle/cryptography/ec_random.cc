73a0bf6b82422cdeb46e708e05931d9a2f7ad368
Julien Quintard <julien.quintard@infinit.io>
2013-01-29 12:24:01 +0100

    cryptography: Improved the random test.

--- tests/unit/elle/cryptography/ec_random.cc
+++ tests/unit/elle/cryptography/ec_random.cc
@@ -1,10 +1,15 @@
 #include <cryptography/random.hh>
+
+#include <elle/types.hh>
 #include <elle/Exception.hh>
 
 int
 main(int, const char *[])
 {
   infinit::cryptography::random::setup();
+
+  elle::Natural32 n =
+    infinit::cryptography::random::generate<elle::Natural32>(3, 15);
 
   std::cout << "tests done." << std::endl;
 
