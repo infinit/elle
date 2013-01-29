97eb2953b94b97663f84adab1d17c393a6117e3b
Julien Quintard <julien.quintard@infinit.io>
2013-01-29 12:23:02 +0100

    cryptography: Improved random so as to provide range-based functions.

--- cryptography/sources/cryptography/random.hh
+++ cryptography/sources/cryptography/random.hh
@@ -23,10 +23,45 @@ namespace infinit
       template <typename T>
       T
       generate();
-      /// Generate a random value based on another metric.
-      template <typename T>
+      /// Generate a random value. The following versions are provided:
+      ///
+      ///   generate<elle::Boolean>();
+      ///   generate<elle::Character>();
+      ///   generate<elle::Character>(elle::Character minimum,
+      ///                             elle::Character maximum);
+      ///   generate<elle::Real>();
+      ///   generate<elle::Real>(elle::Real minimum,
+      ///                        elle::Real maximum);
+      ///   generate<elle::Integer8>();
+      ///   generate<elle::Integer8>(elle::Integer8 minimum,
+      ///                            elle::Integer8 maximum);
+      ///   generate<elle::Integer16>();
+      ///   generate<elle::Integer16>(elle::Integer16 minimum,
+      ///                             elle::Integer16 maximum);
+      ///   generate<elle::Integer32>();
+      ///   generate<elle::Integer32>(elle::Integer32 minimum,
+      ///                             elle::Integer32 maximum);
+      ///   generate<elle::Integer64>();
+      ///   generate<elle::Integer64>(elle::Integer64 minimum,
+      ///                             elle::Integer64 maximum);
+      ///   generate<elle::Natural8>();
+      ///   generate<elle::Natural8>(elle::Natural8 minimum,
+      ///                            elle::Natural8 maximum);
+      ///   generate<elle::Natural16>();
+      ///   generate<elle::Natural16>(elle::Natural16 minimum,
+      ///                             elle::Natural16 maximum);
+      ///   generate<elle::Natural32>();
+      ///   generate<elle::Natural32>(elle::Natural32 minimum,
+      ///                             elle::Natural32 maximum);
+      ///   generate<elle::Natural64>();
+      ///   generate<elle::Natural64>(elle::Natural64 minimum,
+      ///                             elle::Natural64 maximum);
+      ///   generate<elle::String>(elle::Natural32 length);
+      ///   generate<elle::Buffer>(elle::Natural32 size);
+      template <typename T,
+                typename... A>
       T
-      generate(elle::Natural32 const metric);
+      generate(A... arguments);
     }
   }
 }
