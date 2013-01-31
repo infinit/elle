06fe679e0fa5ae686104ea2cdf9d2cef66229938
Raphaël Londeix <raphael.londeix@infinit.io>
2013-01-31 17:41:34 +0100

    fixes: urls and import for invitation

--- oracle/disciples/pythia/constants.py
+++ oracle/disciples/pythia/constants.py
@@ -3,4 +3,4 @@ import os
 
 ADMIN_TOKEN = "fdjskfdakl;asdklwqioefwiopfdsjkl;daskl;askl;fsd"
 _META_PORT = 'META_SERVER_PORT' in os.environ and os.environ['META_SERVER_PORT'] or 12345
-DEFAULT_SERVER = 'http://infinit.im:%i' % int(_META_PORT)
+DEFAULT_SERVER = 'http://localhost:%i' % int(_META_PORT)
