30be575c4910b72889a5c80f4c9646d0c141b3ab
mefyl <quentin.hocquet@infinit.io>
2013-02-06 13:54:15 +0100

    Elle: log: move components into the logger.

--- log/Logger.cc
+++ log/Logger.cc
@@ -1,3 +1,7 @@
+#include <fnmatch.h>
+
+#include <boost/algorithm/string.hpp>
+
 #include <elle/log/Logger.hh>
 
 namespace elle
@@ -28,9 +32,49 @@ namespace elle
 #undef ELLE_LOG_LEVEL_MESSAGE
 
     Logger::Logger()
-    {}
+      : _component_patterns()
+      , _component_enabled()
+      , _component_max_size(0)
+    {
+      static char const* components_str = ::getenv("ELLE_LOG_COMPONENTS");
+      if (components_str == nullptr)
+        this->_component_patterns.push_back("*");
+      else
+        {
+          boost::algorithm::split(this->_component_patterns, components_str,
+                                  boost::algorithm::is_any_of(","),
+                                  boost::algorithm::token_compress_on);
+          for (auto& pattern: this->_component_patterns)
+            boost::algorithm::trim(pattern);
+        }
+    }
 
     Logger::~Logger()
     {}
+
+    bool
+    Logger::component_enabled(std::string const& name)
+    {
+      auto elt = this->_component_enabled.find(name);
+      if (elt == this->_component_enabled.end())
+        {
+          bool res = false;
+          for (auto const& pattern: this->_component_patterns)
+            {
+              if (fnmatch(pattern.c_str(), name.c_str(), 0) == 0)
+                {
+                  this->_component_max_size =
+                    std::max(this->_component_max_size,
+                             static_cast<unsigned int>(name.size()));
+                  res = true;
+                  break;
+                }
+            }
+          this->_component_enabled[name] = res;
+          return res;
+        }
+      else
+        return elt->second;
+    }
   }
 }
