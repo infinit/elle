4376734fec2b146471d504eb22a103ac5639e7b1
mefyl <quentin.hocquet@infinit.io>
2013-02-11 14:57:06 +0100

    Reactor: UDT punching: show checked port.

--- reactor/src/reactor/network/udt-server.cc
+++ reactor/src/reactor/network/udt-server.cc
@@ -281,9 +281,10 @@ namespace reactor
                                         this->_udp_socket,
                                         longinus_2);
                 if (res.port() == this->_public_endpoint.port())
-                  ELLE_ERR("CHIE DU CHAMPAGNE CA MARCHE");
+                  ELLE_ERR("CHIE DU CHAMPAGNE CA MARCHE: %s", res.port());
                 else
-                  ELLE_ERR("OSTIE DE MAUDIT NIAISEUX DE FIREWALL");
+                  ELLE_ERR("OSTIE DE MAUDIT NIAISEUX DE FIREWALL: %s",
+                           res.port());
               }
           }
         catch (std::runtime_error const& e)
