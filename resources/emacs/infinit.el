97eb2059f018b5e7cb8475c98a785a98f1f85016
Julien Quintard <julien.quintard@infinit.io>
2013-02-14 16:21:38 +0100

    resources: updated the Infinit emacs configuration file which now contains the default indentation values.

--- resources/emacs/infinit.el
+++ resources/emacs/infinit.el
@@ -9,3 +9,19 @@
 (setq rebox-default-style 223)
 (global-set-key [(control c)(b)] 'rebox-comment)
 (load-file "~/.emacs.d/rebox.el")
+
+;; indentation
+(require 'cc-mode)
+(add-to-list 'c-style-alist
+  '("infinit"
+    (c-basic-offset . 2)
+    (c-comment-only-line-offset . 0)
+    (c-hanging-braces-alist . ((substatement-open before after)))
+    (c-offsets-alist . ((inline-open . 0)
+                        (inline-close . 0)
+                        (topmost-intro . 0)
+                        (comment-intro . 0)
+                        (substatement . +)
+                        (substatement-open . 0)
+                        (case-label . +)
+                        (access-label . -)))))
