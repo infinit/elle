bbf735b6fc5d4d2b3da7765ea3a09495e8747df2
Julien Quintard <julien.quintard@infinit.io>
2013-02-15 12:45:51 +0100

    cryptography: A new test for the BIGNUM type, especially its serialization.

---
+++ cryptography/tests/cryptography/bn.cc
@@ -0,0 +1,91 @@
+#include "cryptography.hh"
+
+#include <cryptography/bn.hh>
+#include <cryptography/Exception.hh>
+
+#include <elle/serialize/Base64Archive.hh>
+#include <elle/serialize/insert.hh>
+#include <elle/serialize/extract.hh>
+
+#include <openssl/err.h>
+
+/*----------.
+| Represent |
+`----------*/
+
+void
+test_represent()
+{
+  // These generate base64-based representations which can be used in
+  // other tests.
+
+  // 1)
+  {
+    ::BIGNUM bn;
+
+    ::BN_init(&bn);
+
+    if (::BN_pseudo_rand(&bn, 1024, -1, 0) == 0)
+      throw infinit::cryptography::Exception("unable to randomly generate a "
+                                             "big number: %s",
+                                             ::ERR_error_string(ERR_get_error(),
+                                                                nullptr));
+
+    elle::String archive;
+    elle::serialize::to_string<
+      elle::serialize::OutputBase64Archive>(archive) << bn;
+    elle::printf("[representation 1] %s\n", archive);
+  }
+}
+
+/*----------.
+| Serialize |
+`----------*/
+
+void
+test_serialize()
+{
+  // Deserialize from the hard-coded [representation 1]: useful for detecting
+  // changes in formats.
+  {
+    elle::String archive1("AACAAAAA8DGAteEiGfhKAnwaPaIUxWWrkOxUQs7RMq2emyWnc2aPpjWY+PwcSFP1nsz4FU7hFAK3l/dh9p1uw50vp6mzhXZdXAAAqkiaHRqrJ2ckfj7mPF9ikKk88dmSmqZGsnFtVhuD2/bOGbdgGPzSRnJ8vNj77zzxRAMF88vwjliHR2s=");
+
+    ::BIGNUM bn;
+    ::BN_init(&bn);
+
+    elle::serialize::from_string<
+      elle::serialize::InputBase64Archive>(archive1) >> bn;
+
+    elle::String archive2;
+    elle::serialize::to_string<
+      elle::serialize::OutputBase64Archive>(archive2) << bn;
+
+    BOOST_CHECK_EQUAL(archive1, archive2);
+  }
+}
+
+/*-----.
+| Main |
+`-----*/
+
+bool
+test()
+{
+  boost::unit_test::test_suite* suite = BOOST_TEST_SUITE("bn");
+
+  // To uncomment if one wants to update the representations.
+  suite->add(BOOST_TEST_CASE(test_represent));
+
+  suite->add(BOOST_TEST_CASE(test_serialize));
+
+  boost::unit_test::framework::master_test_suite().add(suite);
+
+  return (true);
+}
+
+int
+main(int argc,
+     char** argv)
+{
+  return boost::unit_test::unit_test_main(test, argc, argv);
+}
