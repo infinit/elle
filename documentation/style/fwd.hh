717f7a56d01f5488dc2c8c7ada406c656753d7a2
Julien Quintard <julien.quintard@infinit.io>
2013-02-19 04:30:21 +0100

    documentation: Completed the style guidelines.

--- documentation/style/fwd.hh
+++ documentation/style/fwd.hh
@@ -1,8 +1,7 @@
 /*
  * In this file, every class in the module gets forward declared so as
- * to speed up the compilation process.
- * Pointers and references must be forward declared in order to speed
- * up the compilation process.
+ * to speed up the compilation process. Pointers and references must be
+ * forward declared in order to speed up the compilation process.
  *
  * Note that all files which do not describe a class must be named in
  * lowercase: fwd.hh, all.hh, log.hh etc.
@@ -11,20 +10,18 @@
 #ifndef PACKAGE_MODULE_FWD_HH
 # define PACKAGE_MODULE_FWD_HH
 
-/*
- * First files defining type aliases (i.e typedefs) are included.
- */
-# include <package/module/SimpleType.hh>
-
 namespace package
 {
   namespace module
   {
-
     class CodingStandard;
     class SomeOtherClass;
-
   }
 }
+
+/*
+ * Then, files defining type aliases (i.e typedefs) are included.
+ */
+# include <package/module/SimpleType.hh>
 
 #endif
