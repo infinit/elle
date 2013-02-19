b3de7597bea3bcd63f4c24469cd9f467e113cfc3
PICHOT Fabien <fabien.pichot@infinit.io>
2013-02-19 15:36:48 +0100

    oracles: add a bit of history.

--- oracle/disciples/longinus-server.py
+++ oracle/disciples/longinus-server.py
@@ -3,6 +3,21 @@
 
 """
 longinus-server is an helper for hole punching
+
+Longinus is the name given in medieval and some modern Christian traditions to
+the Roman soldier who pierced Jesus in his side with a lance, the "Holy Lance"
+(lancea, in the Latin Vulgate) while he was on the Cross.
+
+The figure is unnamed in the gospels.
+
+The Longinus legend further identifies this soldier as the centurion present at
+the Crucifixion, who testified, "In truth this man was son of God."
+
+Longinus's legend grew over the years to the point that he was said to have
+converted to Christianity after the Crucifixion, and he is traditionally
+venerated as a saint in the Catholic Church, Eastern Orthodox Church, and
+several other Christian communions.
+
 """
 
 import sys
