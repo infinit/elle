24c4af307d24f7ad2c49f40581bf5416144ee32f
Fabien `chipot' PICHOT <pichot.fabien@infinit.io>
2013-03-27 10:57:01 +0100

    strsignal: print signal name.

---
+++ elle/src/elle/system/signal.cc
@@ -0,0 +1,24 @@
+#include <elle/system/signal.hh>
+
+#include <signal.h>
+
+#include <map>
+
+namespace elle {
+  namespace system {
+    // We don't use strsignal from libc because it's not portable.
+    std::string
+    strsignal(int signum)
+    {
+      static std::map<int, std::string> sigmap{
+          {SIGABRT, "Aborted"},
+          {SIGILL, "Illegal instruction"},
+          {SIGFPE, "Floating point exception"},
+          {SIGINT, "Interrupt"},
+          {SIGSEGV, "Segmentation fault"},
+          {SIGTERM, "Terminated"}
+      };
+      return sigmap[signum];
+    }
+  } /* system */
+} /* elle */
---
+++ elle/src/elle/system/signal.hh
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef SIGNAL_90V2GN6V
+#define SIGNAL_90V2GN6V
+
+#include <string>
+
+namespace elle {
+  namespace system {
+    std::string
+    strsignal(int signum);
+  } /* system */
+} /* elle */
+
+#endif /* end of include guard: SIGNAL_90V2GN6V */
