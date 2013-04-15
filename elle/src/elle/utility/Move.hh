478a070c43075c3b0dee668d4678f8154ff1be4b
Fabien `chipot' PICHOT <pichot.fabien@infinit.io>
2013-04-15 18:23:05 +0200

    norm

--- elle/src/elle/utility/Move.hh
+++ elle/src/elle/utility/Move.hh
@@ -1,35 +1,37 @@
 #ifndef ELLE_UTILITY_MOVE_HH
 # define ELLE_UTILITY_MOVE_HH
 
-namespace elle {
-  namespace utility {
+namespace elle
+{
+  namespace utility
+  {
 
     template<typename T>
     struct move_on_copy_wrapper
     {
       mutable T value;
 
-      move_on_copy_wrapper(T&& t)
-          : value(std::move(t))
+      move_on_copy_wrapper(T&& t):
+          value(std::move(t))
       {}
 
-      move_on_copy_wrapper(move_on_copy_wrapper const& other)
-          : value(std::move(other.value))
+      move_on_copy_wrapper(move_on_copy_wrapper const& other):
+          value(std::move(other.value))
       {}
 
-      move_on_copy_wrapper(move_on_copy_wrapper&& other)
-          : value(std::move(other.value))
+      move_on_copy_wrapper(move_on_copy_wrapper&& other):
+          value(std::move(other.value))
       {}
 
-      move_on_copy_wrapper& operator=(move_on_copy_wrapper const& other)
+      move_on_copy_wrapper& operator = (move_on_copy_wrapper const& other)
       {
-        value=std::move(other.value);
+        value = std::move(other.value);
         return *this;
       }
 
-      move_on_copy_wrapper& operator=(move_on_copy_wrapper&& other)
+      move_on_copy_wrapper& operator = (move_on_copy_wrapper&& other)
       {
-        value=std::move(other.value);
+        value = std::move(other.value);
         return *this;
       }
     };
