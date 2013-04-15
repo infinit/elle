da2fec2b0322c1be66cfe31bbfcc6a79ac7b880f
Fabien `chipot' PICHOT <pichot.fabien@infinit.io>
2013-04-15 18:03:26 +0200

    utility: move.hh, gives you move_on_copy sematics, usefull with lambda.

---
+++ elle/src/elle/utility/Move.hh
@@ -0,0 +1,39 @@
+#ifndef ELLE_UTILITY_MOVE_HH
+# define ELLE_UTILITY_MOVE_HH
+
+namespace elle {
+  namespace utility {
+
+    template<typename T>
+    struct move_on_copy_wrapper
+    {
+      mutable T value;
+
+      move_on_copy_wrapper(T&& t)
+          : value(std::move(t))
+      {}
+
+      move_on_copy_wrapper(move_on_copy_wrapper const& other)
+          : value(std::move(other.value))
+      {}
+
+      move_on_copy_wrapper(move_on_copy_wrapper&& other)
+          : value(std::move(other.value))
+      {}
+
+      move_on_copy_wrapper& operator=(move_on_copy_wrapper const& other)
+      {
+        value=std::move(other.value);
+        return *this;
+      }
+
+      move_on_copy_wrapper& operator=(move_on_copy_wrapper&& other)
+      {
+        value=std::move(other.value);
+        return *this;
+      }
+    };
+  } /* utility */
+} /* elle */
+
+#endif
