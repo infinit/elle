41b610cf3388fb148575edbb364a64589840e0fd
Fabien `chipot' PICHOT <pichot.fabien@infinit.io>
2013-04-16 10:43:03 +0200

    norm

--- elle/src/elle/utility/Move.hh
+++ elle/src/elle/utility/Move.hh
@@ -1,35 +1,38 @@
 #ifndef ELLE_UTILITY_MOVE_HH
 # define ELLE_UTILITY_MOVE_HH
 
+# include <algorithm>
+
 namespace elle
 {
   namespace utility
   {
-
-    template<typename T>
-    struct move_on_copy_wrapper
+    template <typename T>
+    struct move_on_copy
     {
       mutable T value;
 
-      move_on_copy_wrapper(T&& t):
-          value(std::move(t))
+      move_on_copy(T&& t):
+        value{std::move(t)}
       {}
 
-      move_on_copy_wrapper(move_on_copy_wrapper const& other):
-          value(std::move(other.value))
+      move_on_copy(move_on_copy const& other):
+        value{std::move(other.value)}
       {}
 
-      move_on_copy_wrapper(move_on_copy_wrapper&& other):
-          value(std::move(other.value))
+      move_on_copy(move_on_copy&& other):
+        value{std::move(other.value)}
       {}
 
-      move_on_copy_wrapper& operator = (move_on_copy_wrapper const& other)
+      move_on_copy&
+      operator =(move_on_copy const& other)
       {
         value = std::move(other.value);
         return *this;
       }
 
-      move_on_copy_wrapper& operator = (move_on_copy_wrapper&& other)
+      move_on_copy&
+      operator =(move_on_copy&& other)
       {
         value = std::move(other.value);
         return *this;
