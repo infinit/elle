10192d275977d1d27a5d07673c9fc29bfd91babe
Julien Quintard <julien.quintard@infinit.io>
2013-04-17 18:25:56 +0200

    comet: rename rsa for the future.

--- sources/comet/rsa.hh
+++ sources/comet/rsa.hh
@@ -3,10 +3,7 @@
 
 # include <openssl/rsa.h>
 
-namespace comet
-{
-  int RSA_rotate(RSA *rsa, int bits, const unsigned char *seed, size_t length);
-  int RSA_derive(RSA *rsa, BIGNUM* N, const unsigned char *seed, size_t length);
-}
+int dRSA_rotate(RSA *rsa, int bits, const unsigned char *seed, size_t length);
+int dRSA_derive(RSA *rsa, BIGNUM* N, const unsigned char *seed, size_t length);
 
 #endif
