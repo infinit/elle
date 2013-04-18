2b42530aee6989a86ab1c601407c33055877ed70
Fabien `chipot' PICHOT <pichot.fabien@infinit.io>
2013-04-18 11:19:24 +0200

    add unit test to cryptography

---
+++ cryptography/tests/cryptography/insert.cc
@@ -0,0 +1,43 @@
+#include <elle/assert.hh>
+#include <elle/serialize/extract.hh>
+#include <elle/serialize/insert.hh>
+
+#include <cryptography/SecretKey.hh>
+// XXX[temporary: for cryptography]
+using namespace infinit;
+
+int main()
+{
+  using elle::serialize::from_string;
+  using elle::serialize::to_string;
+  using elle::serialize::InputBase64Archive;
+  using elle::serialize::OutputBase64Archive;
+
+  // This string has been generated as follows:
+  //
+//       cryptography::SecretKey _key(cryptography::cipher::Algorithm::aes256,
+//                                    "password");
+//       elle::io::Unique _unique;
+//       to_string<OutputBase64Archive>(_unique) << _key;
+//       std::cout << _unique << std::endl;
+
+  std::string reference{};
+  {
+    cryptography::SecretKey key{
+      cryptography::cipher::Algorithm::aes256,
+      "password"};
+    to_string<OutputBase64Archive>(reference) << key;
+  }
+
+  std::string unique{};
+  {
+    cryptography::SecretKey key{
+      from_string<InputBase64Archive>(reference)};
+    to_string<OutputBase64Archive>(unique) << key;
+  }
+
+  ELLE_ASSERT(unique == reference);
+
+  std::cout << "tests done." << std::endl;
+  return 0;
+}
