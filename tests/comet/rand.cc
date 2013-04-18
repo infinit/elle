2dce7ada9309d352773e09a8ca2dc89320b22b41
Julien Quintard <julien.quintard@infinit.io>
2013-04-18 11:32:15 +0200

    comet: improve the dRAND test.

--- tests/comet/rand.cc
+++ tests/comet/rand.cc
@@ -10,9 +10,12 @@
 | Basic |
 `------*/
 
-void test_basic()
+static
+void
+test_basic()
 {
   BOOST_CHECK(dRAND_init() == 1);
+
   BOOST_CHECK(dRAND_start() == 1);
   {
     RAND_status();
@@ -24,7 +27,7 @@ void test_basic()
   }
   BOOST_CHECK(dRAND_stop() == 1);
 
-  RAND_status();
+  BOOST_CHECK(RAND_status() == 1);
 
   BOOST_CHECK(dRAND_clean() == 1);
 }
@@ -33,6 +36,7 @@ void test_basic()
 | Seed |
 `-----*/
 
+static
 void
 test_seed()
 {
@@ -40,15 +44,16 @@ test_seed()
     "Sir, an equation has no meaning for me "
     "unless it expresses a thought of GOD.";
 
-  dRAND_init();
+  BOOST_CHECK(dRAND_init() == 1);
 
-  dRAND_start();
+  BOOST_CHECK(dRAND_start() == 1);
   {
     // Reset the random implementation and seed the random generator.
     dRAND_reset();
     RAND_seed(seed, ::strlen(seed));
     BOOST_CHECK(RAND_status() == 1);
     char* fingerprint1 = dRAND_fingerprint();
+    BOOST_CHECK(fingerprint1 != nullptr);
 
     // Re-reset the random implementation and finally re-seed it:
     // the result should be the same as the first seeding.
@@ -56,25 +61,27 @@ test_seed()
     RAND_seed(seed, ::strlen(seed));
     BOOST_CHECK(RAND_status() == 1);
     char* fingerprint2 = dRAND_fingerprint();
+    BOOST_CHECK(fingerprint2 != nullptr);
 
     BOOST_CHECK(::strcmp(fingerprint1, fingerprint2) == 0);
 
     ::free(fingerprint1);
     ::free(fingerprint2);
   }
-  dRAND_stop();
+  BOOST_CHECK(dRAND_stop() == 1);
 
-  dRAND_clean();
+  BOOST_CHECK(dRAND_clean() == 1);
 }
 
 /*---------------.
 | Generate Prime |
 `---------------*/
 
+static
 void
 test_generate_prime()
 {
-  dRAND_init();
+  BOOST_CHECK(dRAND_init() == 1);
 
   // Undeterministically randomly generate numbers should should therefore
   // all be different, with high probability.
@@ -105,12 +112,13 @@ test_generate_prime()
       "God exists since mathematics is consistent, "
       "and the Devil exists since we cannot prove it.";
 
-    dRAND_start();
+    BOOST_CHECK(dRAND_start() == 1);
     {
       ::BIGNUM* n1 = BN_new();
       dRAND_reset();
       RAND_seed(seed, ::strlen(seed));
       char* fingerprint1 = dRAND_fingerprint();
+      BOOST_CHECK(fingerprint1 != nullptr);
       BOOST_CHECK(RAND_status() == 1);
       BOOST_CHECK_EQUAL(dBN_generate_prime_ex(n1, 1024, 0,
                                               NULL, NULL, NULL), 1);
@@ -119,6 +127,7 @@ test_generate_prime()
       dRAND_reset();
       RAND_seed(seed, ::strlen(seed));
       char* fingerprint2 = dRAND_fingerprint();
+      BOOST_CHECK(fingerprint2 != nullptr);
       BOOST_CHECK(RAND_status() == 1);
       BOOST_CHECK_EQUAL(dBN_generate_prime_ex(n2, 1024, 0,
                                               NULL, NULL, NULL), 1);
@@ -127,6 +136,7 @@ test_generate_prime()
       dRAND_reset();
       RAND_seed(seed, ::strlen(seed));
       char* fingerprint3 = dRAND_fingerprint();
+      BOOST_CHECK(fingerprint3 != nullptr);
       BOOST_CHECK(RAND_status() == 1);
       BOOST_CHECK_EQUAL(dBN_generate_prime_ex(n3, 1024, 0,
                                               NULL, NULL, NULL), 1);
@@ -139,7 +149,7 @@ test_generate_prime()
       BOOST_CHECK(::BN_cmp(n1, n3) == 0);
       BOOST_CHECK(::BN_cmp(n2, n3) == 0);
     }
-    dRAND_stop();
+    BOOST_CHECK(dRAND_stop() == 1);
   }
 
   // Likewise, generate numbers in a deterministic but safe way.
@@ -149,12 +159,13 @@ test_generate_prime()
       "If his patterns are more permanent than theirs, it is because they "
       "are made with ideas.";
 
-    dRAND_start();
+    BOOST_CHECK(dRAND_start() == 1);
     {
       ::BIGNUM* n1 = BN_new();
       dRAND_reset();
       RAND_seed(seed, ::strlen(seed));
       char* fingerprint1 = dRAND_fingerprint();
+      BOOST_CHECK(fingerprint1 != nullptr);
       BOOST_CHECK(RAND_status() == 1);
       BOOST_CHECK_EQUAL(dBN_generate_prime_ex(n1, 512, 1,
                                               NULL, NULL, NULL), 1);
@@ -163,6 +174,7 @@ test_generate_prime()
       dRAND_reset();
       RAND_seed(seed, ::strlen(seed));
       char* fingerprint2 = dRAND_fingerprint();
+      BOOST_CHECK(fingerprint2 != nullptr);
       BOOST_CHECK(RAND_status() == 1);
       BOOST_CHECK_EQUAL(dBN_generate_prime_ex(n2, 2048, 1,
                                               NULL, NULL, NULL), 1);
@@ -171,6 +183,7 @@ test_generate_prime()
       dRAND_reset();
       RAND_seed(seed, ::strlen(seed));
       char* fingerprint3 = dRAND_fingerprint();
+      BOOST_CHECK(fingerprint3 != nullptr);
       BOOST_CHECK(RAND_status() == 1);
       BOOST_CHECK_EQUAL(dBN_generate_prime_ex(n3, 512, 1,
                                               NULL, NULL, NULL), 1);
@@ -183,16 +196,17 @@ test_generate_prime()
       BOOST_CHECK(::BN_cmp(n1, n3) == 0);
       BOOST_CHECK(::BN_cmp(n2, n3) != 0);
     }
-    dRAND_stop();
+    BOOST_CHECK(dRAND_stop() == 1);
   }
 
-  dRAND_clean();
+  BOOST_CHECK(dRAND_clean() == 1);
 }
 
 /*-----.
 | Main |
 `-----*/
 
+static
 bool
 test()
 {
