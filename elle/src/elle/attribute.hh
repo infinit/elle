c7593623eed64f6ffedb089ecd30353d26e4c01e
mefyl <mefyl@gruntech.org>
2013-04-18 18:20:08 +0200

    Elle: ATTRIBUTE: add prototype-only versions, use boost::call_traits.

--- elle/src/elle/attribute.hh
+++ elle/src/elle/attribute.hh
@@ -3,6 +3,7 @@
 
 # include <type_traits>
 
+# include <boost/call_traits.hpp>
 # include <boost/preprocessor/seq/cat.hpp>
 
 /// Define a private attribute.
@@ -11,41 +12,42 @@
   _type_ BOOST_PP_CAT(_, _name_);
 
 /// Define an accessor returning a const reference on the attribute.
-# define ELLE_ATTRIBUTE_R_ACCESSOR(_type_, _name_)                      \
+# define ELLE_ATTRIBUTE_r_ACCESSOR(_type_, _name_)                      \
   public:                                                               \
-  typename std::remove_cv<                                              \
-    typename std::remove_reference<                                     \
-      _type_                                                            \
-    >::type                                                             \
-  >::type const&                                                        \
+  typename boost::call_traits<_type_>::param_type                       \
   _name_() const                                                        \
+
+/// Define and implement an accessor returning a const reference on the attribute.
+# define ELLE_ATTRIBUTE_R_ACCESSOR(_type_, _name_)                      \
+  ELLE_ATTRIBUTE_r_ACCESSOR(_type_, _name_)                             \
   {                                                                     \
     return (this->BOOST_PP_CAT(_, _name_));                             \
   }
 
 /// Define an accessor enabling one to set the attribute.
-# define ELLE_ATTRIBUTE_W_ACCESSOR(_type_, _name_)                      \
+# define ELLE_ATTRIBUTE_w_ACCESSOR(_type_, _name_)                      \
   public:                                                               \
   void                                                                  \
-  _name_(typename std::remove_cv<                                       \
-         typename std::remove_reference<                                \
-           _type_                                                       \
-         >::type                                                        \
-       >::type const& name)                                             \
+  _name_(typename boost::call_traits<_type_>::param_type name)          \
+
+/// Define and implement an accessor enabling one to set the attribute.
+# define ELLE_ATTRIBUTE_W_ACCESSOR(_type_, _name_)                      \
+  ELLE_ATTRIBUTE_w_ACCESSOR(_type_, _name_)                             \
   {                                                                     \
     this->BOOST_PP_CAT(_, _name_) = name;                               \
   }
 
 /// Define an accessor returning a reference on the attribute i.e
 /// enabling one to potentially modify the attribute outside the class.
-# define ELLE_ATTRIBUTE_X_ACCESSOR(_type_, _name_)                      \
+# define ELLE_ATTRIBUTE_x_ACCESSOR(_type_, _name_)                      \
   public:                                                               \
-  typename std::remove_cv<                                              \
-    typename std::remove_reference<                                     \
-      _type_                                                            \
-    >::type                                                             \
-  >::type&                                                              \
+  typename boost::call_traits<_type_>::reference                        \
   _name_()                                                              \
+
+/// Define and implement an accessor returning a reference on the attribute i.e
+/// enabling one to potentially modify the attribute outside the class.
+# define ELLE_ATTRIBUTE_X_ACCESSOR(_type_, _name_)                      \
+  ELLE_ATTRIBUTE_x_ACCESSOR(_type_, _name_)                             \
   {                                                                     \
     return (this->BOOST_PP_CAT(_, _name_));                             \
   }
@@ -82,6 +84,11 @@
 # define ELLE_ATTRIBUTE_RW(_type_, _name_)                              \
   ELLE_ATTRIBUTE(_type_, _name_)                                        \
   ELLE_ATTRIBUTE_RW_ACCESSORS(_type_, _name_)
+
+# define ELLE_ATTRIBUTE_Rw(_type_, _name_)                              \
+  ELLE_ATTRIBUTE(_type_, _name_)                                        \
+  ELLE_ATTRIBUTE_R_ACCESSOR(_type_, _name_)                             \
+  ELLE_ATTRIBUTE_w_ACCESSOR(_type_, _name_)                             \
 
 # define ELLE_ATTRIBUTE_RX(_type_, _name_)                              \
   ELLE_ATTRIBUTE(_type_, _name_)                                        \
