2b1622df7a0bb378cf256ee254819f5416667491
Julien Quintard <julien.quintard@infinit.io>
2013-04-19 11:40:43 +0200

    comet: improve the rand test with a sequence of deterministic random generations.

--- tests/comet/rand.cc
+++ tests/comet/rand.cc
@@ -120,40 +120,53 @@ test_generate_prime()
     {
       assert(RAND_get_rand_method() == &dRAND_method);
 
-      ::BIGNUM* n1 = ::BN_new();
       ::dRAND_reset();
       ::RAND_seed(seed, ::strlen(seed));
+      ::BIGNUM* n1 = ::BN_new();
       char* fingerprint1 = ::dRAND_fingerprint();
       BOOST_CHECK(fingerprint1 != nullptr);
       BOOST_CHECK(::RAND_status() == 1);
       BOOST_CHECK_EQUAL(::dBN_generate_prime_ex(n1, 1024, 0,
                                                 NULL, NULL, NULL), 1);
-
       ::BIGNUM* n2 = ::BN_new();
-      ::dRAND_reset();
-      ::RAND_seed(seed, ::strlen(seed));
       char* fingerprint2 = ::dRAND_fingerprint();
       BOOST_CHECK(fingerprint2 != nullptr);
       BOOST_CHECK(::RAND_status() == 1);
-      BOOST_CHECK_EQUAL(::dBN_generate_prime_ex(n2, 1024, 0,
+      BOOST_CHECK_EQUAL(::dBN_generate_prime_ex(n2, 2048, 0,
                                                 NULL, NULL, NULL), 1);
 
-      ::BIGNUM* n3 = ::BN_new();
       ::dRAND_reset();
       ::RAND_seed(seed, ::strlen(seed));
+      ::BIGNUM* n3 = ::BN_new();
       char* fingerprint3 = ::dRAND_fingerprint();
       BOOST_CHECK(fingerprint3 != nullptr);
       BOOST_CHECK(::RAND_status() == 1);
       BOOST_CHECK_EQUAL(::dBN_generate_prime_ex(n3, 1024, 0,
                                                 NULL, NULL, NULL), 1);
+      ::BIGNUM* n4 = ::BN_new();
+      char* fingerprint4 = ::dRAND_fingerprint();
+      BOOST_CHECK(fingerprint2 != nullptr);
+      BOOST_CHECK(::RAND_status() == 1);
+      BOOST_CHECK_EQUAL(::dBN_generate_prime_ex(n4, 2048, 0,
+                                                NULL, NULL, NULL), 1);
 
-      BOOST_CHECK(::strcmp(fingerprint1, fingerprint2) == 0);
+      BOOST_CHECK(::strcmp(fingerprint1, fingerprint2) != 0);
+      BOOST_CHECK(::strcmp(fingerprint3, fingerprint4) != 0);
       BOOST_CHECK(::strcmp(fingerprint1, fingerprint3) == 0);
-      BOOST_CHECK(::strcmp(fingerprint2, fingerprint3) == 0);
+      BOOST_CHECK(::strcmp(fingerprint2, fingerprint4) == 0);
 
-      BOOST_CHECK(::BN_cmp(n1, n2) == 0);
+      BOOST_CHECK(::BN_cmp(n1, n2) != 0);
+      BOOST_CHECK(::BN_cmp(n3, n4) != 0);
       BOOST_CHECK(::BN_cmp(n1, n3) == 0);
-      BOOST_CHECK(::BN_cmp(n2, n3) == 0);
+      BOOST_CHECK(::BN_cmp(n2, n4) == 0);
+
+      char* _n1 = ::BN_bn2hex(n1);
+      BOOST_CHECK(_n1 != nullptr);
+      char* _n2 = ::BN_bn2hex(n2);
+      BOOST_CHECK(_n2 != nullptr);
+
+      BOOST_CHECK(::strcmp(_n1, "F25A0F836DC27E7FD3CDD7AFE2A14C84D1B12084DB2C14171E7CD525D1DC8B9F58D1F306296799F20DBF0D0629908A5B8098B32077333844BAD291DFCA7D1683F4B17AE2B8C6CBA1172F013101AB0617941E96E7DF3F97A20935569E3172DB9C0520FA6AB698B86E6135C1BA76599F7768300298FEEEDFEFFA39D4EC0812B569") == 0);
+      BOOST_CHECK(::strcmp(_n2, "ED1B25ABA617994BB998FF236FFF3D60ED5EE4CEC8A7F77CB2161DC395BAD7536077F07FA91DBD78AA5DE6ADD0DF4EFD66324A232362BB9BF0DAA86B93F1D1681D4A56D9E2C0B9F32FA1B65FA16A30A685677AE97DB2C5D8D107484ED4EFB8389BE23CD0ED3BBA79758D9A4D7690F4A910384FB1DBCEA222C8E8497F57E791FCA2A24D27414EF507C5A9CF852D446EDFF46E26868F19B4906092E629BFADC184563E380BECFED675287AFD75B01647654AF57B189A875835205F1F90400EE2408A76EF7A4D743788923C61536C10357508360676D150FD72F8720549786D3504ABEEFFCE70F23DBD7E298127B375B22C3719EFBCF81651249E366A012104C70D") == 0);
     }
     BOOST_CHECK(::dRAND_stop() == 1);
   }
