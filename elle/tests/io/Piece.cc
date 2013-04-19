d8c261e2ed23b384c171f622bea4eeee1fd9951e
Fabien `chipot' PICHOT <pichot.fabien@infinit.io>
2013-04-19 17:43:53 +0200

    build farm: add a missing define for serializer

---
+++ elle/tests/io/Piece.cc
@@ -0,0 +1,28 @@
+
+#define BOOST_TEST_MODULE Piece
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+#include <elle/io/Piece.hh>
+
+namespace elle
+{
+  namespace io
+  {
+    std::ostream&
+    operator << (std::ostream &out, elle::io::Piece const &p)
+    {
+      p.Dump();
+      return out;
+    }
+  }
+}
+
+BOOST_AUTO_TEST_CASE(piece)
+{
+  elle::io::Piece p1("key", "value");
+  elle::io::Piece p2("key", "value");
+
+  BOOST_CHECK_EQUAL(p1, p2);
+}
