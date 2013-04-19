788cba67629c89b621da77e1d7d786d08433c379
mefyl <mefyl@gruntech.org>
2013-04-19 14:12:59 +0200

    Elle: test Backtrace.

---
+++ elle/tests/Backtrace.cc
@@ -0,0 +1,84 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <elle/Backtrace.hh>
+
+using elle::Backtrace;
+
+static
+void
+test_backtrace_empty()
+{
+  Backtrace empty;
+  BOOST_CHECK(empty.empty());
+}
+
+Backtrace
+quux()
+{
+  return Backtrace::current();
+}
+
+Backtrace
+baz()
+{
+  return quux();
+}
+
+Backtrace
+bar()
+{
+  return baz();
+}
+
+Backtrace
+foo()
+{
+  return bar();
+}
+
+static
+void
+test_backtrace()
+{
+  Backtrace bt(foo());
+  BOOST_CHECK_GE(bt.size(), 4);
+  auto sf = bt.begin();
+  BOOST_CHECK_EQUAL(sf->symbol, "quux()");
+  ++sf;
+  BOOST_CHECK_EQUAL(sf->symbol, "baz()");
+  ++sf;
+  BOOST_CHECK_EQUAL(sf->symbol, "bar()");
+  ++sf;
+  BOOST_CHECK_EQUAL(sf->symbol, "foo()");
+  ++sf;
+}
+
+static
+void
+test_strip_base()
+{
+  Backtrace bt(foo());
+  bt.strip_base(Backtrace::current());
+  BOOST_CHECK_LE(bt.size(), 5);
+  BOOST_CHECK_EQUAL(bt.front().symbol, "quux()");
+}
+
+static
+bool
+test_suite()
+{
+  boost::unit_test::test_suite* bt = BOOST_TEST_SUITE("Backtrace");
+  boost::unit_test::framework::master_test_suite().add(bt);
+  bt->add(BOOST_TEST_CASE(test_backtrace_empty));
+  bt->add(BOOST_TEST_CASE(test_backtrace));
+  bt->add(BOOST_TEST_CASE(test_strip_base));
+
+  return true;
+}
+
+int
+main(int argc, char** argv)
+{
+  return ::boost::unit_test::unit_test_main(test_suite, argc, argv);
+}
