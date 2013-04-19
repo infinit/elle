aa4437ee528dbff128e5f21e0f509ece13baac9a
mefyl <mefyl@gruntech.org>
2013-04-19 14:05:29 +0200

    Elle: Compiler: add ATTRIBUTE_ALWAYS_INLINE.

--- elle/src/elle/compiler.hh
+++ elle/src/elle/compiler.hh
@@ -2,6 +2,7 @@
 # define ELLE_COMPILER_HH
 
 # define ELLE_COMPILER_ATTRIBUTE_NORETURN __attribute__((noreturn))
+# define ELLE_COMPILER_ATTRIBUTE_ALWAYS_INLINE __attribute__((always_inline))
 # define ELLE_COMPILER_PRETTY_FUNCTION __PRETTY_FUNCTION__
 
 #endif
