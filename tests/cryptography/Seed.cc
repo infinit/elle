e06f74d4ab0ac7cb31ff603e4e4730c8344a3712
Julien Quintard <julien.quintard@infinit.io>
2013-04-20 16:37:23 +0200

    cryptography: prepare the test suite for Seed.

--- tests/cryptography/Seed.cc
+++ tests/cryptography/Seed.cc
@@ -8,17 +8,15 @@
 #include <elle/serialize/insert.hh>
 #include <elle/serialize/extract.hh>
 
+//#include <comet/Comet.hh> // XXX
+
 /*----------.
 | Represent |
 `----------*/
 
-static
 void
 test_represent()
 {
-  // WARNING: To uncomment only if one wants to update the representations.
-  return;
-
   // These generate base64-based representations which can be used in
   // other tests.
 
@@ -29,7 +27,6 @@ test_represent()
 | Generate |
 `---------*/
 
-static
 void
 test_generate()
 {
@@ -40,7 +37,6 @@ test_generate()
 | Construct |
 `----------*/
 
-static
 void
 test_construct()
 {
@@ -51,7 +47,6 @@ test_construct()
 | Operate |
 `--------*/
 
-static
 void
 test_operate()
 {
@@ -62,7 +57,6 @@ test_operate()
 | Serialize |
 `----------*/
 
-static
 void
 test_serialize()
 {
@@ -73,11 +67,13 @@ test_serialize()
 | Main |
 `-----*/
 
-static
 bool
 test()
 {
   boost::unit_test::test_suite* suite = BOOST_TEST_SUITE("Seed");
+
+  // To uncomment if one wants to update the representations.
+  //suite->add(BOOST_TEST_CASE(test_represent));
 
   // XXX
   {
@@ -87,23 +83,83 @@ test()
 
     infinit::cryptography::Seed seed0 =
       infinit::cryptography::Seed::generate(pair);
-    seed0.buffer().dump();
+
+    elle::printf("pair: %s\n", pair);
+    elle::printf("seed0: %s\n", seed0);
+
+    // XXX
+    /* XXX
+    {
+      ::EVP_PKEY* key = nullptr;
+
+      if ((key = ::EVP_PKEY_new()) == nullptr)
+        throw Exception("unable to allocate the EVP_PKEY: %s",
+                        ::ERR_error_string(ERR_get_error(), nullptr));
+
+      ::RSA* rsa;
+
+      if ((rsa = ::RSA_new()) == nullptr)
+        throw Exception("%s", ::ERR_error_string(ERR_get_error(), nullptr));
+
+      comet::RSA_rotate(rsa,
+                        1024,
+                        seed.buffer().contents(), seed.buffer().size());
+
+      if (::EVP_PKEY_assign_RSA(key, rsa) <= 0)
+        throw Exception("%s", ::ERR_error_string(ERR_get_error(), nullptr));
+    }
+    */
+
     /* XXX
     infinit::cryptography::Seed seed1 = pair.k().rotate(seed0);
-    seed1.buffer().dump();
+    elle::printf("seed1: %s\n", seed1);
 
     infinit::cryptography::Seed seed2 = pair.k().rotate(seed1);
-    seed2.buffer().dump();
+    elle::printf("seed2: %s\n", seed2);
 
-    infinit::cryptography::KeyPair pair2(seed2);
+    // XXX infinit::cryptography::KeyPair pair2(seed2);
 
     infinit::cryptography::Seed seed3 = pair.k().rotate(seed2);
-    seed3.buffer().dump();
+    elle::printf("seed3: %s\n", seed3);
+    */
 
-    infinit::cryptography::Seed _seed2 = pair.K().derive(seed3);
-    _seed2.buffer().dump();
+    //infinit::cryptography::Seed _seed2 = pair.K().derive(seed3);
+    //elle::printf("_seed2: %s\n", _seed2);
 
-    infinit::cryptography::KeyPair _pair2(_seed2);
+    // XXX verifier que lorsque l'on derive, l'exposant est toujours different et pas un truc genre 5 ou 17 sinon la derivation n'est pas privee.
+
+    /* XXX
+       Rotate from owner:
+       - il recupere le seed_x courant en le dechiffrant du bloc Group.
+       - on rotate le seed avec la clef privee du owner:
+         k.rotate(seed_x) -> seed_x+1
+       - on cree une nouvelle keypair avec le nouveau seed:
+         KeyPair kp_x+1(seed_x+1) -> (Kpass_x+1, kpass_x+1)
+         cet appel va utiliser comet::RSA_rotate(bits, seed) -> rsa
+       - on chiffre le seed_x+1 pour que le owner puisse y acceder et
+         on le met dans le nouveau Groupe
+       - on inclue egalement kpass_x+1 qui est la partie "publique" du pass.
+       - on chiffre et distribue Kpass_x+1 a tous les membres
+       NOTE: puisque le owner devrait egalement avoir acces a Kpass_x+1 et
+             puisque stocker seed prend de la place dans les metadata du
+             Group, il serait plus judicieux de creer un record pour le owner
+             directement dans Members. notamment avec Porcupine ca ne signifie
+             pas que les donnees seront dans un autre bloc.
+
+       Derive from member:
+       - il dechiffre Kpass_x ainsi que seed_x
+       - il derive seed_x: K.derive(seed_x) -> seed_x-1
+         seed_x-1 contient N_x-1
+       NOTE: pour ce dernier point, Seed doit etre une representation generique
+             avec rsa::Seed la vraie seed. On devrait par ailleurs effectuer des
+             verifications sur le fait que les clefs/seed sont compatibles.
+       - ensuite on construit Kpass_x-1 a partir du seed:
+         PublicKey Kpass_x-1(seed_x-1)
+    */
+
+    /* XXX
+    infinit::cryptography::PublicKey _K2(_seed2);
+    infinit::cryptography::KeyPair _pair2(_seed2); // XXX + k2
 
     BOOST_CHECK_EQUAL(pair2, _pair2);
 
@@ -118,7 +174,6 @@ test()
   }
   // XXX
 
-  suite->add(BOOST_TEST_CASE(test_represent));
   suite->add(BOOST_TEST_CASE(test_generate));
   suite->add(BOOST_TEST_CASE(test_construct));
   suite->add(BOOST_TEST_CASE(test_operate));
