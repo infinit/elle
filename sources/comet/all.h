ef7a0063ac0a1ab7afc8a9f318553e9d97fe2cd1
Julien Quintard <julien.quintard@infinit.io>
2013-04-24 13:59:34 +0200

    drake: reference the new project dOpenSSL instead of the old Comet.

--- sources/comet/all.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMET_ALL_H
-# define COMET_ALL_H
-
-# include <comet/bn.h>
-# include <comet/rand.h>
-# include <comet/rsa.h>
-
-#endif
--- tests/comet/comet.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMET_H
-# define COMET_H
-
-# include <assert.h>
-# define DOPENSSL_CHECK(_condition_)                                    \
-  assert(_condition_);
-
-#endif
