2b3de3780d1e434ed839d16a5f957e5417275849
Julien Quintard <julien.quintard@infinit.io>
2013-04-24 12:01:12 +0200

    comet: renamed the tests in C files and simplified them since they no longer can rely on boost unit test.

---
+++ tests/comet/comet.h
@@ -0,0 +1,8 @@
+#ifndef COMET_H
+# define COMET_H
+
+# include <assert.h>
+# define DOPENSSL_CHECK(_condition_)                                    \
+  assert(_condition_);
+
+#endif
--- tests/comet/comet.hh
+++
@@ -1,7 +0,0 @@
-#ifndef COMET_HH
-# define COMET_HH
-
-# define BOOST_TEST_DYN_LINK
-# include <boost/test/unit_test.hpp>
-
-#endif
