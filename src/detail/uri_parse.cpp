c5d124c9704fb2f4220ee00d1584d23820be4a26
Glyn Matthews <glyn.matthews@gmail.com>
2013-04-25 22:23:02 +0200

    Added copyright attribution for URI parser.

--- src/detail/uri_parse.cpp
+++ src/detail/uri_parse.cpp
@@ -1,4 +1,6 @@
-// Copyright 2013 Glyn Matthews.
+// Copyright 2009-2010 Jeroen Habraken.
+// Copyright 2009-2013 Dean Michael Berris, Glyn Matthews.
+// Copyright 2012 Google, Inc.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
