fed0361d6818bcb1b09c555977cac151fb6ef6d0
Raphaël Londeix <raphael.londeix@infinit.io>
2013-04-30 09:03:35 +0200

    Add a way to assert even in release builds (ELLE_ENFORCE)

--- elle/src/elle/assert.hh
+++ elle/src/elle/assert.hh
@@ -31,42 +31,52 @@ namespace elle
                int line);
 }
 
+/// Enforce a condition is true (always present in the code)
+/// @see ELLE_ASSERT for debug only assertions.
+#  define ELLE_ENFORCE(_condition_)                                           \
+  ::elle::_assert(_condition_, #_condition_, __FILE__, __LINE__)
+#  define ELLE_ENFORCE_EQ(A, B)                                               \
+  ::elle::_assert(A == B,                                                     \
+                  ::elle::sprintf(#A " == " #B " (%s != %s)", A, B),          \
+                  __FILE__, __LINE__)
+#  define ELLE_ENFORCE_NEQ(A, B)                                              \
+  ::elle::_assert(A != B,                                                     \
+                  ::elle::sprintf(#A " != " #B " (%s == %s)", A, B),          \
+                  __FILE__, __LINE__)
+#  define ELLE_ENFORCE_GT(A, B)                                               \
+  ::elle::_assert(A > B,                                                      \
+                  ::elle::sprintf(#A " > " #B " (%s <= %s)", A, B),           \
+                  __FILE__, __LINE__)
+#  define ELLE_ENFORCE_GTE(A, B)                                              \
+  ::elle::_assert(A >= B,                                                     \
+                  ::elle::sprintf(#A " >= " #B " (%s < %s)", A, B),           \
+                  __FILE__, __LINE__)
+#  define ELLE_ENFORCE_LT(A, B)                                               \
+  ::elle::_assert(A < B,                                                      \
+                  ::elle::sprintf(#A " < " #B " (%s >= %s)", A, B),           \
+                  __FILE__, __LINE__)
+#  define ELLE_ENFORCE_LTE(A, B)                                              \
+  ::elle::_assert(A <= B,                                                     \
+                  ::elle::sprintf(#A " <= " #B " (%s > %s)", A, B),           \
+                  __FILE__, __LINE__)
+
 # if defined(DEBUG) || !defined(NDEBUG)
 /// Throw if the condition is unmet.
-#  define ELLE_ASSERT(_condition_)                                      \
-  ::elle::_assert(_condition_, #_condition_, __FILE__, __LINE__)
-#  define ELLE_ASSERT_EQ(A, B)                                          \
-  ::elle::_assert(A == B,                                               \
-                  ::elle::sprintf(#A " == " #B " (%s != %s)", A, B),    \
-                  __FILE__, __LINE__)
-#  define ELLE_ASSERT_NEQ(A, B)                                         \
-  ::elle::_assert(A != B,                                               \
-                  ::elle::sprintf(#A " != " #B " (%s == %s)", A, B),    \
-                  __FILE__, __LINE__)
-#  define ELLE_ASSERT_GT(A, B)                                          \
-  ::elle::_assert(A > B,                                                \
-                  ::elle::sprintf(#A " > " #B " (%s <= %s)", A, B),     \
-                  __FILE__, __LINE__)
-#  define ELLE_ASSERT_GTE(A, B)                                         \
-  ::elle::_assert(A >= B,                                               \
-                  ::elle::sprintf(#A " >= " #B " (%s < %s)", A, B),     \
-                  __FILE__, __LINE__)
-#  define ELLE_ASSERT_LT(A, B)                                          \
-  ::elle::_assert(A < B,                                                \
-                  ::elle::sprintf(#A " < " #B " (%s >= %s)", A, B),     \
-                  __FILE__, __LINE__)
-#  define ELLE_ASSERT_LTE(A, B)                                         \
-  ::elle::_assert(A <= B,                                               \
-                  ::elle::sprintf(#A " <= " #B " (%s > %s)", A, B),     \
-                  __FILE__, __LINE__)
+#  define ELLE_ASSERT(_condition_) ELLE_ENFORCE(_condition_)
+#  define ELLE_ASSERT_EQ(A, B) ELLE_ENFORCE_EQ(A, B)
+#  define ELLE_ASSERT_NEQ(A, B) ELLE_ENFORCE_NEQ(A, B)
+#  define ELLE_ASSERT_GT(A, B) ELLE_ENFORCE_GT(A, B)
+#  define ELLE_ASSERT_GTE(A, B) ELLE_ENFORCE_GTE(A, B)
+#  define ELLE_ASSERT_LT(A, B) ELLE_ENFORCE_LT(A, B)
+#  define ELLE_ASSERT_LTE(A, B) ELLE_ENFORCE_LTE(A, B)
 # else
 #  define ELLE_ASSERT(_condition_) ((void) 0)
-#  define ELLE_ASSERT_EQ(A, B) ELLE_ASSERT()
-#  define ELLE_ASSERT_NEQ(A, B) ELLE_ASSERT()
-#  define ELLE_ASSERT_GT(A, B) ELLE_ASSERT()
-#  define ELLE_ASSERT_GTE(A, B) ELLE_ASSERT()
-#  define ELLE_ASSERT_LT(A, B) ELLE_ASSERT()
-#  define ELLE_ASSERT_LTE(A, B) ELLE_ASSERT()
+#  define ELLE_ASSERT_EQ(A, B) ELLE_ASSERT(true)
+#  define ELLE_ASSERT_NEQ(A, B) ELLE_ASSERT(true)
+#  define ELLE_ASSERT_GT(A, B) ELLE_ASSERT(true)
+#  define ELLE_ASSERT_GTE(A, B) ELLE_ASSERT(true)
+#  define ELLE_ASSERT_LT(A, B) ELLE_ASSERT(true)
+#  define ELLE_ASSERT_LTE(A, B) ELLE_ASSERT(true)
 # endif
 
 /// Provide a way for generating code only if evolving in the DEBUG mode.
