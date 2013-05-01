d1e8cbd887249fe59a5583687d9dd9599dbec4c1
Julien Quintard <julien.quintard@infinit.io>
2013-05-01 20:59:55 +0200

    include a copyright header which references the LICENSE file.

--- tests/dopenssl/rand.c
+++ tests/dopenssl/rand.c
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2013, infinit.io
+ *
+ * This software is provided "as is" without warranty of any kind,
+ * either expressed or implied, including but not limited to the
+ * implied warranties of fitness for a particular purpose.
+ *
+ * See the LICENSE file for more information on the terms and
+ * conditions.
+ */
+
 #include "dopenssl.h"
 
 #include <dopenssl/bn.h>
