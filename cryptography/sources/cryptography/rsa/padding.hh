54345be2e3e4f47fa2c815f9a99ac11a92a81b82
Julien Quintard <julien.quintard@infinit.io>
2013-05-07 07:07:01 +0200

    cryptography: new function for computing the padding size depending on the padding algorithm.

--- cryptography/sources/cryptography/rsa/padding.hh
+++ cryptography/sources/cryptography/rsa/padding.hh
@@ -1,8 +1,11 @@
 #ifndef INFINIT_CRYPTOGRAPHY_RSA_PADDING_HH
 # define INFINIT_CRYPTOGRAPHY_RSA_PADDING_HH
 
-#include <ostream>
-#include <openssl/evp.h>
+# include <elle/types.hh>
+
+# include <openssl/evp.h>
+
+# include <iosfwd>
 
 namespace infinit
 {
@@ -16,6 +19,10 @@ namespace infinit
         void
         print(std::ostream& stream,
               ::EVP_PKEY_CTX* context);
+        /// Return the padding footprint --- i.e the number of bits (not bytes)
+        /// of data reserved for the padding --- for the given context.
+        elle::Natural32
+        footprint(::EVP_PKEY_CTX* context);
       }
     }
   }
