9cd1b3262797b467a867a684c5c4031affe73f79
Fabien `chipot' PICHOT <pichot.fabien@infinit.io>
2013-05-07 12:05:44 +0200

    HttpClient: remove strange token handling and dead code.

--- elle/src/elle/HttpClient.cc
+++ elle/src/elle/HttpClient.cc
@@ -259,7 +259,6 @@ namespace elle
                          uint16_t port,
                          std::string const& user_agent)
     : _impl{new Impl{server, port, user_agent}}
-    , _token{}
   {}
 
   HTTPClient::~HTTPClient()
@@ -328,10 +327,6 @@ namespace elle
                          : std::string("User-Agent: ") + this->_impl->user_agent + std::string(CRLF))
                      << "Connection: close" CRLF
                      << request.headers_string();
-      if (_token.size())
-      {
-        request_stream << "Authorization: " << _token << CRLF;
-      }
       if (body.size())
         request_stream << "Content-Length: " << elle::sprint(body.size()) << CRLF
                        << "Content-Type: " << request.content_type() << CRLF CRLF
@@ -393,139 +388,6 @@ namespace elle
           throw boost::system::system_error(error);
       }
   }
-
-  /// XXX Remove this
-  // void
-  // HTTPClient::_request(std::string const& url,
-  //                      std::string const& method,
-  //                      std::string const& body,
-  //                      std::stringstream& response_)
-  // {
-  //   //struct STACK {~STACK() { assert(false && "TOUT VA BIEN"); }} stack;
-  //   std::string uri = _impl->server + url;
-
-  //   namespace ip = boost::asio::ip;
-
-  //   ip::tcp::socket socket(_impl->io_service);
-
-  //   ip::tcp::resolver resolver(_impl->io_service);
-  //   ip::tcp::resolver::query query(_impl->server, elle::sprint(_impl->port));
-  //   ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-  //   //XXX which way is better ?
-  //   //boost::asio::connect(socket, endpoint_iterator);
-  //   socket.connect(*endpoint_iterator);
-
-  //   {
-  //     boost::asio::streambuf request;
-  //     std::ostream request_stream(&request);
-  //     request_stream << method << ' ' << url << " HTTP/1.0" CRLF
-  //                    << "Host: " << this->_impl->server << CRLF
-  //       // User agent management suxx.
-  //                    << "User-Agent: MetaClient" CRLF
-  //                    << "Connection: close" CRLF;
-  //     if (_token.size())
-  //       {
-  //         request_stream << "Authorization: " << _token << CRLF;
-  //       }
-  //     if (body.size())
-  //       request_stream << "Content-Length: " << elle::sprint(body.size()) << CRLF
-  //                      << "Content-Type: application/json" << CRLF CRLF
-  //                      << body;
-  //     else
-  //       request_stream << CRLF;
-
-  //     // Send the request.
-  //     boost::asio::write(socket, request);
-  //   }
-
-  //   // Read the response status line. The response streambuf will automatically
-  //   // grow to accommodate the entire line. The growth may be limited by passing
-  //   // a maximum size to the streambuf constructor.
-  //   boost::asio::streambuf response;
-  //   boost::asio::read_until(socket, response, "\r\n");
-
-  //   // Check that response is OK.
-  //   std::istream response_stream(&response);
-  //   std::string http_version;
-  //   response_stream >> http_version;
-  //   unsigned int status_code;
-  //   response_stream >> status_code;
-  //   std::string status_message;
-  //   std::getline(response_stream, status_message);
-  //   if (!response_stream || http_version.substr(0, 5) != "HTTP/")
-  //     throw std::runtime_error("Invalid response");
-  //   if (status_code != 200)
-  //     {
-  //       //struct STACK {~STACK() { ::exit(12); }} stack;
-  //       throw std::runtime_error(
-  //         "Cannot " + method + " '" + body + "' " + uri +
-  //         " returned HTTP code " + elle::sprint(status_code) +
-  //         ": " + status_message
-  //         );
-  //     }
-
-  //   // Read the response headers, which are terminated by a blank line.
-  //   boost::asio::read_until(socket, response, "\r\n\r\n");
-
-  //   // Process the response headers.
-  //   std::string header;
-  //   while (std::getline(response_stream, header) && header != "\r")
-  //     {/* consume header lines */}
-
-  //   // Write whatever content we already have to output.
-  //   if (response.size() > 0)
-  //     response_ << &response;
-
-  //   // Read until EOF, writing data to output as we go.
-  //   boost::system::error_code error;
-  //   while (boost::asio::read(socket,
-  //                            response,
-  //                            boost::asio::transfer_at_least(1), error))
-  //     {
-  //       response_ << &response;
-  //       if (error && error != boost::asio::error::eof)
-  //         throw boost::system::system_error(error);
-  //     }
-  // }
-
-  // std::stringstream
-  // HTTPClient::post(std::string const& url, elle::format::json::Object const& req)
-  // {
-  //   ELLE_LOG_COMPONENT("elle.HTTPClient");
-
-  //   std::stringstream res;
-
-  //   // http request
-  //   try
-  //   {
-  //     this->_request(url, "POST", req.repr(), res);
-  //   }
-  //   catch (std::exception const& err)
-  //   {
-  //     ELLE_TRACE("POST %s %s threw an error", url, req.repr());
-  //     throw HTTPException(ResponseCode::internal_server_error, err.what());
-  //   }
-
-  //   return res;
-  // }
-
-  // std::stringstream
-  // HTTPClient::get(std::string const& url)
-  // {
-  //   ELLE_LOG_COMPONENT("elle.HTTPClient");
-  //   std::stringstream res;
-
-  //   // http request
-  //   try
-  //   { this->_request(url, "GET", "", res); }
-  //   catch (std::runtime_error const& err)
-  //   {
-  //     ELLE_TRACE("GET %s threw an error", url);
-  //     throw HTTPException(ResponseCode::internal_server_error, err.what());
-  //   }
-
-  //   return res;
-  // }
 
   bool
   HTTPClient::put(std::string const& url,
