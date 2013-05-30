f78850b095394d6a0c5a139e529e897f7124b0be
mefyl <quentin.hocquet@infinit.io>
2013-05-30 10:52:09 +0200

    elle.compiler: Fill the windows version.

--- elle/src/elle/compiler.hh
+++ elle/src/elle/compiler.hh
@@ -1,9 +1,17 @@
 #ifndef ELLE_COMPILER_HH
 # define ELLE_COMPILER_HH
 
-# define ELLE_COMPILER_ATTRIBUTE_NORETURN __attribute__((noreturn))
-# define ELLE_COMPILER_ATTRIBUTE_ALWAYS_INLINE __attribute__((always_inline))
-# define ELLE_COMPILER_ATTRIBUTE_NO_INLINE __attribute__((noinline))
-# define ELLE_COMPILER_PRETTY_FUNCTION __PRETTY_FUNCTION__
+# ifdef _MSC_VER
+#  define ELLE_COMPILER_ATTRIBUTE_NORETURN __declspec(noreturn)
+#  define ELLE_COMPILER_ATTRIBUTE_ALWAYS_INLINE
+#  define ELLE_COMPILER_ATTRIBUTE_NO_INLINE __declspec((noinline))
+#  define ELLE_COMPILER_PRETTY_FUNCTION __FUNCSIG__
+#  define constexpr
+# else
+#  define ELLE_COMPILER_ATTRIBUTE_NORETURN __attribute__((noreturn))
+#  define ELLE_COMPILER_ATTRIBUTE_ALWAYS_INLINE __attribute__((always_inline))
+#  define ELLE_COMPILER_ATTRIBUTE_NO_INLINE __attribute__((noinline))
+#  define ELLE_COMPILER_PRETTY_FUNCTION __PRETTY_FUNCTION__
+# endif
 
 #endif
