46a929f65a939204c8c4f2acfc688cf682b21f2b
Raphaël Londeix <raphael.londeix@infinit.io>
2013-06-06 16:45:02 +0200

    elle.serialize: Improve sequence serializer to support movable objects.

--- elle/src/elle/serialize/detail/SequenceSerializer.hh
+++ elle/src/elle/serialize/detail/SequenceSerializer.hh
@@ -1,91 +1,96 @@
 #ifndef ELLE_SERIALIZE_SEQUENCECONTAINER_HH
 # define ELLE_SERIALIZE_SEQUENCECONTAINER_HH
 
-# include <boost/foreach.hpp>
-
 # include <elle/serialize/Serializer.hh>
 
-namespace elle { namespace serialize { namespace detail {
+namespace elle
+{
+  namespace serialize
+  {
+    namespace detail
+    {
+      namespace stl_insert
+      {
+        // XXX do emplace back when possible
+        template <typename Container>
+        struct PushBack
+        {
+          typedef typename Container::value_type value_type;
 
-    namespace stl_insert {
-
-        template<typename Container> struct PushBack
+          template <typename Archive>
+          static inline
+          void
+          insert_elem(Archive& ar,
+                      Container& container)
           {
-            typedef typename Container::value_type ValueType;
+            container.push_back(ar.template construct_value<value_type>());
+          }
+        };
 
-            static inline void Save(Container& container,
-                                    ValueType const& value)
-            { container.push_back(value); }
-          };
+        // XXX do emplace back when possible
+        template <typename Container>
+        struct Insert
+        {
+          typedef typename Container::value_type value_type;
 
-        template<typename Container> struct Insert
+          template <typename Archive>
+          static inline
+          void
+          insert_elem(Archive& ar,
+                      Container& container)
           {
-            typedef typename Container::value_type ValueType;
+            container.insert(ar.template construct_value<value_type>());
+          }
+        };
+      } // !ns stl_insert
 
-            static inline void Save(Container& container,
-                                    ValueType const& value)
-            { container.insert(value); }
-          };
-
-    } // !ns stl_insert
-
-    // Base class for stl sequences
-    template<typename Container_, template <typename> class SaveMethod = stl_insert::PushBack>
-      struct SequenceSerializer
-        : public elle::serialize::SplitSerializer<Container_>
+      // Base class for stl sequences
+      template <typename Container_,
+                template <typename> class InsertMethod = stl_insert::PushBack>
+      struct SequenceSerializer:
+        public elle::serialize::SplitSerializer<Container_>
       {
         typedef Container_ Container;
 
-        template<typename Archive>
-          static inline void Save(Archive& ar, Container const& container, unsigned int)
-            {
-              static_assert(
-                  StoreFormat<Container>::value == false,
-                  "oups, should not store class version"
-              );
-              typedef typename Archive::SequenceSizeType SizeType;
-              static_assert(
-                  std::is_unsigned<SizeType>::value,
-                  "SequenceSizeType has to be unsigned"
-              );
-              size_t size = container.size();
-              if (size > static_cast<size_t>(static_cast<SizeType>(-1)))
-                throw std::runtime_error("Sequence too big");
-              ar << static_cast<SizeType>(size);
-              BOOST_FOREACH(auto& el, container)
-                {
-                  ar << el;
-                }
-            }
+        template <typename Archive>
+        static inline
+        void Save(Archive& ar,
+                  Container const& container,
+                  unsigned int /* version */)
+        {
+          static_assert(StoreFormat<Container>::value == false,
+                        "oups, should not store class version");
+          typedef typename Archive::SequenceSizeType size_type;
+          static_assert(std::is_unsigned<size_type>::value,
+                        "SequenceSizeType has to be unsigned");
+          size_t size = container.size();
+          if (size > static_cast<size_t>(static_cast<size_type>(-1)))
+            throw std::runtime_error("Sequence too big");
+          ar << static_cast<size_type>(size);
+          for (auto const& el: container)
+            ar << el;
+        }
 
-        template<typename Archive>
-          static inline void Load(Archive& ar, Container& container, unsigned int)
-            {
-              static_assert(
-                  StoreFormat<Container>::value == false,
-                  "oups, should not store class version"
-              );
-              typedef typename Archive::SequenceSizeType SizeType;
-              static_assert(
-                  std::is_unsigned<SizeType>::value,
-                  "SequenceSizeType has to be unsigned"
-              );
-              SizeType size;
-              ar >> size;
-              for (SizeType i = 0; i < size; ++i)
-                {
-                  /// XXX copy involved
-                  /// XXX value_type ctor not customizable
-                  typename SaveMethod<Container>::ValueType el;
-                  ar >> el;
-                  SaveMethod<Container>::Save(container, el);
-                }
-            }
+        template <typename Archive>
+        static inline
+        void
+        Load(Archive& ar,
+             Container& container,
+             unsigned int /* version */)
+        {
+          static_assert(StoreFormat<Container>::value == false,
+                        "oups, should not store class version");
+          typedef typename Archive::SequenceSizeType size_type;
+          static_assert(std::is_unsigned<size_type>::value,
+                        "SequenceSizeType has to be unsigned");
+          size_type size;
+          ar >> size;
+          for (size_type i = 0; i < size; ++i)
+            InsertMethod<Container>::insert_elem(ar, container);
+        }
       };
-
-}}}
+    }
+  }
+}
 
 #endif /* ! SEQUENCECONTAINER_HH */
-
-
-
