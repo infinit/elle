98b56ed947b7f347c391572a768745cbc3e02b62
Antony MECHIN <antony.mechin@infinit.io>
2013-06-11 12:30:57 +0200

    elle.format: Improve base64 unit test.

--- elle/tests/format/base64.cc
+++ elle/tests/format/base64.cc
@@ -1,5 +1,5 @@
-// #define BOOST_TEST_DYN_LINK
-// #define BOOST_TEST_MODULE JSONSerialization
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Base64Format
 
 #include <elle/Buffer.hh>
 #include <elle/format/base64.hh>
@@ -7,28 +7,19 @@
 #include <boost/test/unit_test.hpp>
 
 #include <string>
-#include <iostream> //XXX
 
-// BOOST_AUTO_TEST_CASE(Format)
-// {
-//   std::string to_encode = "I'm a beaver with a flat tail.";
-//   {
-//     elle::Buffer buff(to_encode.data(), to_encode.length());
-//     std::string encoded = elle::format::base64::encode(buff);
-//     std::cerr << encoded << std::endl;
-//   }
-// }
-
-int main(void)
+BOOST_AUTO_TEST_CASE(encode_decode_base64)
 {
   std::string to_encode = "";
-  for (unsigned int i = 0; i < 10000; ++i)
+  for (unsigned int i = 0; i < 100; ++i)
   {
-    {
-      elle::Buffer buff(to_encode.data(), to_encode.length());
-      std::string encoded = elle::format::base64::encode(buff);
-      std::cerr << encoded << std::endl;
-    }
+    elle::Buffer buffin(to_encode.data(), to_encode.length());
+    std::string encoded = elle::format::base64::encode(buffin);
+    auto buffout = elle::format::base64::decode(encoded);
+    std::string decoded((char *) buffout.contents(), buffout.size());
+
+    BOOST_CHECK_EQUAL(to_encode.size(), decoded.size());
+    BOOST_CHECK_EQUAL(to_encode, decoded);
     to_encode += std::to_string(i);
   }
 }
