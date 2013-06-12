9fe285619b24f55281a1a1f5c8694d6b5479b018
Glyn Matthews <glyn.matthews@gmail.com>
2013-06-12 20:36:59 +0200

    Added copyright attribution.

--- test/uri_resolve_test.cpp
+++ test/uri_resolve_test.cpp
@@ -1,4 +1,5 @@
-// Copyright (c) Glyn Matthews 2012, 2013.
+// Copyright 2012, 2013 Glyn Matthews.
+// Copyright 2013 Hannes Kamecke.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
