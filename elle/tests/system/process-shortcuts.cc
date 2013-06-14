23f9040201ce22561c5f01903234b73336b836da
mefyl <quentin.hocquet@infinit.io>
2013-06-14 14:29:55 +0200

    system: Use ENFORCE_EQ in test case to ease diagnostic.

--- elle/tests/system/process-shortcuts.cc
+++ elle/tests/system/process-shortcuts.cc
@@ -9,42 +9,42 @@ main()
 {
   using namespace elle::system;
 
-  ELLE_ENFORCE(
-    check_output("echo", "bite") == "bite\n"
+  ELLE_ENFORCE_EQ(
+    check_output("echo", "bite"), "bite\n"
   );
-  ELLE_ENFORCE(
-    check_output(command("echo", "bite2")) == "bite2\n"
+  ELLE_ENFORCE_EQ(
+    check_output(command("echo", "bite2")), "bite2\n"
   );
 
-  ELLE_ENFORCE(
+  ELLE_ENFORCE_EQ(
     check_output(
         command("echo", "bite")
       | command("tr", "beit", "BTIE", pipe_stdout_config)
-    ) == "BIET\n"
+    ), "BIET\n"
   );
 
 
-  ELLE_ENFORCE(
+  ELLE_ENFORCE_EQ(
     check_output(
         command("echo", "TROLLpifpifpifpifipfipf")
       | command("tr", "pif", "lol")
       | command("tr", "a-z", "A-Z", check_output_config)
-    ) == "TROLLLOLLOLLOLLOLOLLOLL\n"
+    ), "TROLLLOLLOLLOLLOLOLLOLL\n"
   );
 
-  ELLE_ENFORCE(
+  ELLE_ENFORCE_EQ(
     check_output(
         command("echo", "bite2")
       | command("tr", "beit", "BTIE")
       | command("tr", "TIEB", "abcd")
-    ) == "dbca2\n"
+    ), "dbca2\n"
   );
 
-  ELLE_ENFORCE(
+  ELLE_ENFORCE_EQ(
     execute(
       command("echo", "1"),
       command("echo", "2"),
       command("echo", "3")
-    ) == 0
+    ), 0
   );
 }
