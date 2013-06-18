7ac4b4a698d969bd7b2b1afddff17424f0a19254
Fabien `chipot' PICHOT <pichot.fabien@infinit.io>
2013-06-18 17:37:18 +0200

    curly: Make a global httpd, remove useless test.

--- curly/tests/test.cc
+++ curly/tests/test.cc
@@ -2,19 +2,54 @@
 #define BOOST_TEST_MODULE curly
 #include <boost/test/unit_test.hpp>
 
+#include <boost/algorithm/string.hpp>
+
 #include <curly/curly.hh>
 #include <elle/system/Process.hh>
 #include <elle/assert.hh>
+#include <elle/print.hh>
 
 #include <atomic>
 #include <iostream>
 #include <sstream>
 #include <thread>
 #include <mutex>
+#include <memory>
+
+class httpd
+{
+private:
+  std::unique_ptr<elle::system::Process> _httpd;
+public:
+
+  std::string port;
+  httpd()
+  {
+    this->port = "65458";
+    auto pc = elle::system::pipe_output_config;
+    this->_httpd.reset(new elle::system::Process{
+                       pc, "python3", {"-mhttp.server", this->port}
+                       });
+    sleep(1);
+  }
+
+  ~httpd()
+  {
+    this->_httpd->interrupt();
+  }
+
+  std::string
+  root_url()
+  {
+    return elle::sprintf("http://localhost:%s/", this->port);
+  }
+};
+
+httpd global_server;
 
 BOOST_AUTO_TEST_CASE(simple)
 {
-  std::string page = curly::get("http://meta.api.development.infinit.io:12345/");
+  std::string page = curly::get(global_server.root_url());
   BOOST_CHECK(page.empty() == false);
 }
 
@@ -22,7 +57,7 @@ BOOST_AUTO_TEST_CASE(complex)
 {
   auto rc = curly::make_get();
 
-  rc.url("http://meta.api.development.infinit.io:12345/");
+  rc.url(global_server.root_url());
   curly::request r(std::move(rc));
   BOOST_CHECK(r.code() == 200);
 }
@@ -31,38 +66,11 @@ BOOST_AUTO_TEST_CASE(timed)
 {
   auto rc = curly::make_get();
 
-  rc.url("http://meta.api.development.infinit.io:12345/");
+  rc.url(global_server.root_url());
   curly::request r(std::move(rc));
   BOOST_CHECK(r.code() == 200);
   std::cout << "time is " << r.time().count() << std::endl;
   BOOST_CHECK(r.time() != std::chrono::duration<double>(0));
-}
-
-BOOST_AUTO_TEST_CASE(threaded_lock)
-{
-  int const number_of_thread = 15;
-  std::vector<std::thread> vt;
-
-  auto pc = elle::system::normal_config;
-  elle::system::Process server{std::move(pc),
-    "python3", {"-mhttp.server", "8081"}};
-  sleep(1);
-  std::mutex mutex;
-  auto fn = [&]
-  {
-    for (int i = 0; i < 10 * number_of_thread; ++i)
-    {
-      std::lock_guard<std::mutex> guard{mutex};
-      std::string page = curly::get("http://meta.api.development.infinit.io:12345/");
-      BOOST_CHECK(page.empty() == false);
-    }
-  };
-  for (int i = 0; i < number_of_thread; ++i)
-    vt.emplace_back(fn);
-
-  for (auto& th: vt)
-    th.join();
-  server.interrupt();
 }
 
 BOOST_AUTO_TEST_CASE(threaded)
@@ -70,15 +78,12 @@ BOOST_AUTO_TEST_CASE(threaded)
   int const number_of_thread = 3;
   std::vector<std::thread> vt;
 
-  elle::system::Process server{"python3", {"-mhttp.server", "8080"}};
-  sleep(1);
   std::mutex mutex;
-
   auto fn = [&] {
     for (int i = 0; i < 10 * number_of_thread; ++i)
     {
       auto rc = curly::make_get();
-      rc.url("http://localhost:8080/");
+      rc.url(global_server.root_url());
       rc.option(CURLOPT_CONNECTTIMEOUT, 300000);
       rc.option(CURLOPT_TIMEOUT, 300000);
       rc.header("Connection", "close");
@@ -103,5 +108,4 @@ BOOST_AUTO_TEST_CASE(threaded)
 
   for (auto& th: vt)
     th.join();
-  server.interrupt();
 }
