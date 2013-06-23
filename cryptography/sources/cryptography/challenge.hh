c021dd4373d3e1f93857db10bff4ec632c8f3b46
Julien Quintard <julien.quintard@infinit.io>
2013-06-24 00:27:20 +0200

    cryptography: Introduce functions for easily setting up public-key-based authentication challenges.

---
+++ cryptography/sources/cryptography/challenge.hh
@@ -0,0 +1,86 @@
+#ifndef INFINIT_CRYPTOGRAPHY_CHALLENGE_HH
+# define INFINIT_CRYPTOGRAPHY_CHALLENGE_HH
+
+# include <cryptography/fwd.hh>
+
+# include <elle/types.hh>
+
+namespace infinit
+{
+  namespace cryptography
+  {
+    /// Provide functions for setting up a challenge so as to authenticate
+    /// a user.
+    ///
+    /// Note that the challenge mechanism relies on an asymetric cryptosystem
+    /// as detailed in the protocol below.
+    ///
+    /// First, the node acting as the server decides to authenticate another
+    /// node, referred to as the client.
+    ///
+    /// The server starts by generating a nonce, which is a random value used
+    /// exclusively for the purpose of this challenge.
+    ///
+    ///   elle::String nonce = nonce();
+    ///
+    /// Then, the server create a challenge for authenticating a user referred
+    /// to as the challengee, in opposition to the server which acts as the
+    /// challenger:
+    ///
+    ///   elle::String challenge = create(client_K, server_K, nonce);
+    ///
+    /// The challenge string can then be sent to the client.
+    ///
+    /// Upon reception, the client can accept the challenge by decrypting
+    /// it, extracting both the server's public key and nonce and re-encrypting
+    /// the nonce with it for the response to be decrypted by the server only.
+    ///
+    ///   elle::String response = accept(challenge, client_k);
+    ///
+    /// Finally, the response can be sent back to the server for decryption and
+    /// final verification after which the client will be considered
+    /// authenticated.
+    ///
+    ///   if (check(response, server_k, nonce) == false)
+    ///     throw Exception("failed challenge");
+    namespace challenge
+    {
+      /*----------.
+      | Functions |
+      `----------*/
+
+      /// Generate a random value for the purpose of a challenge.
+      ///
+      /// Note that a nonce should never be re-used for another challenge.
+      elle::String
+      nonce();
+      /// Create a challenge by encrypting both the challenger's public key
+      /// and the nonce with the challengee's public key for him only to be
+      /// able to decrypt the tuple.
+      elle::String
+      create(PublicKey const& challenger_K,
+             PublicKey const& challengee_K,
+             elle::String const& nonce);
+      /// Take a challenge string, decrypt it with the challengee's private key,
+      /// extract the nonce and re-encrypt it with the challenger's public key.
+      elle::String
+      accept(elle::String const& challenge,
+             PrivateKey const& challengee_k);
+      /// Accept a challenge and check that the challenger is the expected one
+      /// by verifying that the public key embedded in the challenge string is
+      /// the right one.
+      elle::String
+      accept(elle::String const& challenge,
+             PrivateKey const& challengee_k,
+             PublicKey const& challenger_K);
+      /// Decrypt the response with the challenger's private key, extract the
+      /// response nonce and check its validy given the original nonce.
+      elle::Boolean
+      check(elle::String const& response,
+            PrivateKey const& challenger_k,
+            elle::String const& nonce);
+    }
+  }
+}
+
+#endif
---
+++ cryptography/tests/cryptography/challenge.cc
@@ -0,0 +1,62 @@
+#include "cryptography.hh"
+
+#include <cryptography/challenge.hh>
+#include <cryptography/KeyPair.hh>
+
+/*--------.
+| Operate |
+`--------*/
+
+static
+void
+test_operate()
+{
+  infinit::cryptography::KeyPair challenger =
+    infinit::cryptography::KeyPair::generate(
+      infinit::cryptography::Cryptosystem::rsa,
+      2048);
+  infinit::cryptography::KeyPair challengee =
+    infinit::cryptography::KeyPair::generate(
+      infinit::cryptography::Cryptosystem::rsa,
+      1024);
+
+  elle::String nonce = infinit::cryptography::challenge::nonce();
+
+  elle::String challenge =
+    infinit::cryptography::challenge::create(challenger.K(),
+                                             challengee.K(),
+                                             nonce);
+
+  elle::String response =
+    infinit::cryptography::challenge::accept(challenge,
+                                             challengee.k(),
+                                             challenger.K());
+
+  BOOST_CHECK_EQUAL(infinit::cryptography::challenge::check(response,
+                                                            challenger.k(),
+                                                            nonce), true);
+}
+
+/*-----.
+| Main |
+`-----*/
+
+static
+bool
+test()
+{
+  boost::unit_test::test_suite* suite = BOOST_TEST_SUITE("challenge");
+
+  suite->add(BOOST_TEST_CASE(test_operate));
+
+  boost::unit_test::framework::master_test_suite().add(suite);
+
+  return (true);
+}
+
+int
+main(int argc,
+     char** argv)
+{
+  return (boost::unit_test::unit_test_main(test, argc, argv));
+}
