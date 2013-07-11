4d48b03c63571a57431fd11d6ee1f50e4b048fd1
Fabien `chipot' PICHOT <pichot.fabien@infinit.io>
2013-07-11 17:41:56 +0200

    elle.assert: Use boost auto test case.

--- elle/tests/assert.cc
+++ elle/tests/assert.cc
@@ -1,55 +1,26 @@
 #define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE assert
+#include <boost/test/unit_test.hpp>
 
 #include <elle/assert.hh>
 #include <elle/printf.hh>
 
-#include <boost/test/unit_test.hpp>
-
-static
-void
-success()
+BOOST_AUTO_TEST_CASE(success)
 {
   ELLE_ASSERT(true);
 }
 
-static
-void
-failure()
+BOOST_AUTO_TEST_CASE(failure)
 {
   BOOST_CHECK_THROW(ELLE_ASSERT(false), elle::AssertError);
 }
 
-static
-void
-success_eq()
+BOOST_AUTO_TEST_CASE(success_eq)
 {
   ELLE_ASSERT_EQ(42, 42);
 }
 
-static
-void
-failure_eq()
+BOOST_AUTO_TEST_CASE(failure_eq)
 {
   BOOST_CHECK_THROW(ELLE_ASSERT_EQ(42, 51), elle::AssertError);
-}
-
-static
-bool
-test_suite()
-{
-  boost::unit_test::test_suite* basics = BOOST_TEST_SUITE("Basics");
-  boost::unit_test::framework::master_test_suite().add(basics);
-
-  basics->add(BOOST_TEST_CASE(success));
-  basics->add(BOOST_TEST_CASE(failure));
-  basics->add(BOOST_TEST_CASE(success_eq));
-  basics->add(BOOST_TEST_CASE(failure_eq));
-
-  return true;
-}
-
-int
-main(int argc, char** argv)
-{
-	return ::boost::unit_test::unit_test_main(test_suite, argc, argv);
 }
