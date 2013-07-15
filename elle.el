ba3bcd062562dbd263fd27e3305ad2745f7dd7b7
mefyl <quentin.hocquet@infinit.io>
2013-07-15 17:12:34 +0200

    emacs: Indent wrt Infinit coding style.

--- elle.el
+++ elle.el
@@ -1,5 +1,30 @@
 (set-font-size 80)
 
+(require 'cc-mode)
+
+(defun c-lineup-C-comments-boxes (arg)
+  (let ((box (save-excursion
+               (goto-char (cdr arg))
+               (looking-at "/\\*-*\\.$"))))
+    (if box
+        0
+      (c-lineup-C-comments arg))))
+
+(add-to-list 'c-style-alist
+             '("infinit"
+               (c-basic-offset . 2)
+               (c-comment-only-line-offset . 0)
+               (c-hanging-braces-alist     . ((substatement-open before after)))
+               (c-offsets-alist . ((topmost-intro        . 0)
+                                   (substatement         . +)
+                                   (substatement-open    . 0)
+                                   (case-label           . +)
+                                   (access-label         . -)
+                                   (inclass              . +)
+                                   (inline-open          . 0)
+                                   (c                    . c-lineup-C-comments-boxes)))))
+(setq c-default-style "infinit")
+
 (c++-project
  '(
    "elle/src"
