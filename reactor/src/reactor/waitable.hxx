6a11d1844b16c20b5e40da3929b45401cb4ab8d3
Fabien `chipot' PICHOT <pichot.fabien@infinit.io>
2013-07-17 19:24:09 +0200

    reactor.Waitable: Make exception handling recursive.

--- reactor/src/reactor/waitable.hxx
+++ reactor/src/reactor/waitable.hxx
@@ -1,14 +1,38 @@
 #ifndef INFINIT_REACTOR_WAITABLE_HXX
 # define INFINIT_REACTOR_WAITABLE_HXX
 
+#include <exception>
+
 namespace reactor
 {
   template <typename Exception, typename... Args>
   void
   Waitable::_raise(Args&&... args)
   {
-    auto e = Exception{std::forward<Args>(args)...};
-    this->_exception = std::make_exception_ptr(std::move(e));
+    if (this->_exception)
+    {
+      try
+      {
+        try
+        {
+          std::rethrow_exception(this->_exception);
+        }
+        catch (...)
+        {
+          std::throw_with_nested(Exception{std::forward<Args>(args)...});
+        }
+      }
+      catch (...)
+      {
+        this->_exception = std::current_exception();
+      }
+    }
+    else
+    {
+      this->_exception = std::make_exception_ptr(Exception{
+                                                   std::forward<Args>(args)...
+                                                 });
+    }
   }
 } /* reactor */
 
