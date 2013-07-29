f0bcaae99dee8c4430ad78491f5123559d7a52db
mefyl <quentin.hocquet@infinit.io>
2013-07-29 15:48:57 +0200

    reactor.Barrier: document.

--- reactor/src/reactor/Barrier.hh
+++ reactor/src/reactor/Barrier.hh
@@ -5,6 +5,14 @@
 
 namespace reactor
 {
+  /// A closed or opened checkpoint threads can wait upon.
+  ///
+  /// Threads waiting on a barrier will sleep until the barrier is opened. If
+  /// the barrier is already opened, they will carry on without even stopping.
+  ///
+  /// A Barrier is similar to a Signal, except it holds a state, enabling to
+  /// detect if an event happens or already happened, while signals only pulse
+  /// when triggered.
   class Barrier
     : public Waitable
   {
@@ -12,30 +20,39 @@ namespace reactor
   | Types |
   `------*/
   public:
+    /// Our own type.
     typedef Barrier Self;
+    /// The parent type.
     typedef Waitable Super;
 
   /*-------------.
   | Construction |
   `-------------*/
   public:
+    /// Create a Barrier with the given name.
+    /// @param name The barrier name, for pretty-printing purpose.
     Barrier(const std::string& name = std::string());
 
   /*--------.
   | Waiting |
   `--------*/
   public:
+    /// Open this, letting current and future threads go past it.
     void open();
+    /// Close this, stopping future threads waiting it.
     void close();
   protected:
+    /// Stop the thread if and only if this is closed.
     virtual bool _wait(Thread* thread);
   private:
+    /// Whether this is opened.
     ELLE_ATTRIBUTE_R(bool, opened);
 
   /*----------.
   | Printable |
   `----------*/
   public:
+    /// Pretty print this.
     virtual
     void
     print(std::ostream& stream) const override;
