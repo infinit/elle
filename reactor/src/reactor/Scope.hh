2a40620e24b4ab076bcfa9fa18ebdb0ad696c56d
mefyl <quentin.hocquet@infinit.io>
2013-09-10 12:08:14 +0200

    Scope: Document.

--- reactor/src/reactor/Scope.hh
+++ reactor/src/reactor/Scope.hh
@@ -7,20 +7,46 @@
 
 namespace reactor
 {
+  /// Manage a collection of Threads.
+  ///
+  /// A Scope enables to start parallel tasks in threads and make sure they are
+  /// terminated upon destruction.  As a Barrier, a Scope is open if and only if
+  /// all currently managed threads are done, enabling to easily join the group
+  /// of managed threads.  If an exception escapes from a thread, all other
+  /// threads are killed and waiting the Scope will re-throw the exception.
   class Scope:
     public reactor::Barrier
   {
+  /*-------------.
+  | Construction |
+  `-------------*/
   public:
+    /// Create a scope.
     Scope();
+    /// Destruct a scope, terminating all managed threads now.
+    ///
+    /// \throw Terminate if interrupted while killing managed threads.
     ~Scope();
+
+  /*--------.
+  | Threads |
+  `--------*/
+  public:
+    /// Start and manage a thread named \a name executing \a a.
+    ///
+    /// If an exception escapes \a a, all other threads are killed and waiting
+    /// the Scope re-throws the exception.
+    ///
+    /// \param name The name of the managed thread.
+    /// \param a    The action run by the managed thread.
     void
     run_background(std::string const& name,
                    Thread::Action const& a);
-
-
   private:
-    std::vector<Thread*> _threads;
-    int _running;
+    /// Managed threads.
+    ELLE_ATTRIBUTE(std::vector<Thread*>, threads);
+    /// Number of running threads.
+    ELLE_ATTRIBUTE(int, running);
   };
 }
 
