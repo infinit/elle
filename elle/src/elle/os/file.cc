6e0ebc344703c8947cbc42b0213b0ab97233da80
Antony MECHIN <antony.mechin@infinit.io>
2013-09-18 20:13:12 +0200

    elle.os.file: Throw ONLY elle::Exception

--- elle/src/elle/os/file.cc
+++ elle/src/elle/os/file.cc
@@ -1,6 +1,7 @@
 #include "file.hh"
 
 #include <elle/Exception.hh>
+#include <elle/printf.hh>
 
 #include <boost/filesystem.hpp>
 
@@ -14,23 +15,46 @@ namespace elle
       size(std::string const& path)
       {
         if (!boost::filesystem::exists(path))
-          throw elle::Exception{"file " + path + " doesn't exist"};
+          throw elle::Exception{elle::sprintf("file %s doesn't exist", path)};
 
-        if (!boost::filesystem::is_directory(path))
-          return boost::filesystem::file_size(path);
+        boost::system::error_code err;
+        if (!boost::filesystem::is_directory(path, err))
+        {
+          if (err)
+            throw elle::Exception{elle::sprintf("file %s doesn't exist", path)};
+
+          auto size = boost::filesystem::file_size(path, err);
+
+          if (err)
+            throw elle::Exception{
+              elle::sprintf("unable to retrieve %s size", path)};
+
+          return size;
+        }
 
         size_t size_ = 0;
         boost::filesystem::directory_iterator end_itr;
 
-        for (boost::filesystem::directory_iterator itr(path); itr != end_itr; ++itr)
+        for (boost::filesystem::directory_iterator itr(path, err);
+             itr != end_itr;
+             ++itr)
         {
-          if (boost::filesystem::is_directory(itr->status()))
+          if (err)
+            throw elle::Exception{
+              elle::sprintf("unable to iterate over %s", path)};
+
+          bool is_dir = boost::filesystem::is_directory(itr->status());
+          if (is_dir)
           {
             size_ += size(itr->path().string());
           }
           else
           {
-            size_ += boost::filesystem::file_size(itr->path());
+            size_ += boost::filesystem::file_size(itr->path(), err);
+
+            if (err)
+              throw elle::Exception{
+                elle::sprintf("unable to retrieve %s size", path)};
           }
         }
         return size_;
