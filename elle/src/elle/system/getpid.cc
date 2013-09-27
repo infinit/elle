0dd5430bdd35ffb68e0c9ad0f0ca8af043400031
Raphaël Londeix <raphael.londeix@infinit.io>
2013-09-27 18:29:17 +0200

    elle: Add portable getpid() function.

---
+++ elle/src/elle/system/getpid.cc
@@ -0,0 +1,25 @@
+#include "getpid.hh"
+
+#ifdef INFINIT_WINDOWS
+# include <windows.h>
+#else
+# include <unistd.h>
+#endif
+
+namespace elle
+{
+  namespace system
+  {
+
+    ProcessId
+    getpid()
+    {
+#ifdef INFINIT_WINDOWS
+      return ::GetCurrentProcessId();
+#else
+      return ::getpid();
+#endif
+    }
+
+  }
+}
---
+++ elle/src/elle/system/getpid.hh
@@ -0,0 +1,15 @@
+#ifndef ELLE_SYSTEM_GETPID_HH
+# define ELLE_SYSTEM_GETPID_HH
+
+namespace elle
+{
+  namespace system
+  {
+    typedef int ProcessId;
+
+    ProcessId
+    getpid();
+
+  }
+}
+#endif
