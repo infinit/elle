f4ccd1dde39b1f46dc97589433bed0b52e2ee315
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-09-29 00:03:01 +0400

    Fix missing string include error
    Found by compiling on os x 10.8.5 by clang 5.0 (500.2.75)

--- src/uri_errors.cpp
+++ src/uri_errors.cpp
@@ -3,6 +3,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
+#include <string> // std::string
 #include <network/uri/uri_errors.hpp>
 
 namespace network {
