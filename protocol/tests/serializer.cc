32fe77e4dce1a6713c87f90c9a7a43e460b9272f
mefyl <quentin.hocquet@infinit.io>
2013-10-01 18:25:41 +0200

    protocol.Serializer: Test harder.

--- protocol/tests/serializer.cc
+++ protocol/tests/serializer.cc
@@ -5,57 +5,157 @@
 
 #include <reactor/Barrier.hh>
 #include <reactor/Scope.hh>
-#include <reactor/scheduler.hh>
+#include <reactor/network/buffer.hh>
+#include <reactor/network/exception.hh>
 #include <reactor/network/tcp-server.hh>
+#include <reactor/scheduler.hh>
 
+#include <protocol/exceptions.hh>
 #include <protocol/Serializer.hh>
 
-/// Helper to connect two serializers.
-class Serializers
+ELLE_LOG_COMPONENT("infinit.protocol.test");
+
+class SocketInstrumentation
 {
 public:
-  Serializers():
-    _server()
+  SocketInstrumentation():
+    _router(*reactor::Scheduler::scheduler(), "router",
+            std::bind(&SocketInstrumentation::_route, this)),
+    _a_conf(),
+    _b_conf()
   {
-    auto& sched = *reactor::Scheduler::scheduler();
-    elle::With<reactor::Scope>() << [&](reactor::Scope& scope)
-    {
-      reactor::Barrier listening;
-      int port = 0;
-      scope.run_background(
-        "server", [&]
-        {
-          this->_server.listen();
-          port = this->_server.port();
-          listening.open();
-          this->_server_socket.reset(this->_server.accept());
-          this->alice.reset(
-            new infinit::protocol::Serializer(*this->_server_socket));
-        });
-      scope.run_background(
-        "client", [&]
-        {
-          listening.wait();
-          this->_client_socket.reset(
-            new reactor::network::TCPSocket(sched, "127.0.0.1", port));
-          this->bob.reset(
-            new infinit::protocol::Serializer(*this->_client_socket));
-        });
-      scope.wait();
-    };
+    this->_a_server.listen();
+    this->_b_server.listen();
+    this->_alice.reset(new reactor::network::TCPSocket("127.0.0.1",
+                                                       this->_a_server.port()));
+    this->_bob.reset(new reactor::network::TCPSocket("127.0.0.1",
+                                                     this->_b_server.port()));
+    reactor::yield();
+    reactor::yield();
   }
-  std::unique_ptr<infinit::protocol::Serializer> alice;
-  std::unique_ptr<infinit::protocol::Serializer> bob;
+
+  ~SocketInstrumentation()
+  {
+    this->_router.terminate_now();
+  }
+
+  reactor::network::TCPSocket&
+  alice()
+  {
+    return *this->_alice;
+  }
+
+  reactor::network::TCPSocket&
+  bob()
+  {
+    return *this->_bob;
+  }
+
+  void
+  alice_quota(int quota)
+  {
+    this->_a_conf.quota = quota;
+  }
+
+  void
+  bob_quota(int quota)
+  {
+    this->_b_conf.quota = quota;
+  }
+
+  void
+  alice_corrupt(int offset, char mask = 0xFF)
+  {
+    this->_a_conf.corrupt_offset = offset;
+    this->_a_conf.corrupt_mask = mask;
+  }
 
 private:
-  reactor::network::TCPServer _server;
-  std::unique_ptr<reactor::network::TCPSocket> _server_socket;
-  std::unique_ptr<reactor::network::TCPSocket> _client_socket;
+  void
+  _route()
+  {
+    using reactor::network::Buffer;
+
+    std::unique_ptr<reactor::network::TCPSocket> a(this->_a_server.accept());
+    std::unique_ptr<reactor::network::TCPSocket> b(this->_b_server.accept());
+
+    elle::With<reactor::Scope>() << [&](reactor::Scope& scope)
+    {
+      auto route = [&] (reactor::network::TCPSocket* a,
+                        reactor::network::TCPSocket* b,
+                        Conf& conf)
+        {
+          try
+          {
+            int routed = 0;
+            bool relay = true;
+            while (relay)
+            {
+              char buffer[1024];
+              int size = a->read_some(Buffer(buffer, sizeof(buffer)));
+              if (conf.corrupt_offset >= 0 && \
+                  conf.corrupt_offset >= routed && \
+                  conf.corrupt_offset < routed + size)
+              {
+                int offset = conf.corrupt_offset - routed;
+                ELLE_LOG("%s: corrupt byte %s", *this, offset);
+                buffer[offset] ^= conf.corrupt_mask;
+              }
+              routed += size;
+              if (conf.quota >= 0 && routed > conf.quota)
+              {
+                ELLE_LOG("%s: quota reached, interrupt", *this);
+                size = size - (routed - conf.quota);
+                relay = false;
+                routed = conf.quota;
+              }
+              ELLE_TRACE("%s: route %s bytes", *this, size);
+              b->write(Buffer(buffer, size));
+            }
+          }
+          catch (reactor::network::ConnectionClosed const&)
+          {}
+          a->close();
+          b->close();
+        };
+      scope.run_background("A to B",
+                           std::bind(route, a.get(), b.get(),
+                                     std::ref(this->_a_conf)));
+      scope.run_background("B to A",
+                           std::bind(route, b.get(), a.get(),
+                                     std::ref(this->_b_conf)));
+      scope.wait();
+    };
+
+  }
+
+  reactor::network::TCPServer _a_server;
+  reactor::network::TCPServer _b_server;
+  std::unique_ptr<reactor::network::TCPSocket> _alice;
+  std::unique_ptr<reactor::network::TCPSocket> _bob;
+  reactor::Thread _router;
+
+  struct Conf
+  {
+    Conf():
+      quota(-1),
+      corrupt_offset(-1),
+      corrupt_mask(0)
+    {}
+
+    int quota;
+    int corrupt_offset;
+    char corrupt_mask;
+  };
+  Conf _a_conf;
+  Conf _b_conf;
 };
 
 static
 void
-exchange_packets()
+dialog(std::function<void (SocketInstrumentation&)> const& conf,
+       std::function<void (infinit::protocol::Serializer&)> const& a,
+       std::function<void (infinit::protocol::Serializer&)> const& b)
 {
   reactor::Scheduler sched;
 
@@ -63,51 +163,15 @@ exchange_packets()
     sched, "main",
     [&] ()
     {
-      Serializers serializers;
-      auto& alice = *serializers.alice;
-      auto& bob = *serializers.bob;
+      SocketInstrumentation sockets;
+      infinit::protocol::Serializer alice(sockets.alice());
+      infinit::protocol::Serializer bob(sockets.bob());
+      conf(sockets);
 
       elle::With<reactor::Scope>() << [&](reactor::Scope& scope)
       {
-        scope.run_background(
-          "alice", [&]
-          {
-            {
-              {
-                infinit::protocol::Packet p;
-                p << "some data 42";
-                alice.write(p);
-              }
-              {
-                infinit::protocol::Packet p = alice.read();
-                std::string res;
-                p >> res;
-                BOOST_CHECK_EQUAL(res, "goody");
-                BOOST_CHECK(!(p >> res));
-              }
-            }
-          });
-        scope.run_background(
-          "bob", [&]
-          {
-            {
-              infinit::protocol::Packet p = bob.read();
-              std::string some, data;
-              int i;
-              p >> some;
-              BOOST_CHECK_EQUAL(some, "some");
-              p >> data;
-              BOOST_CHECK_EQUAL(data, "data");
-              p >> i;
-              BOOST_CHECK_EQUAL(i, 42);
-              BOOST_CHECK(!(p >> data));
-            }
-            {
-              infinit::protocol::Packet p;
-              p << "goody";
-              bob.write(p);
-            }
-          });
+        scope.run_background("alice", std::bind(a, std::ref(alice)));
+        scope.run_background("bob", std::bind(b, std::ref(bob)));
         scope.wait();
       };
     });
@@ -119,62 +183,123 @@ static
 void
 exchange_packets()
 {
-  reactor::Scheduler sched;
+  dialog([] (SocketInstrumentation&) {},
+         [] (infinit::protocol::Serializer& s)
+         {
+           {
+             infinit::protocol::Packet p;
+             p << "some data 42";
+             s.write(p);
+           }
+           {
+             infinit::protocol::Packet p = s.read();
+             std::string res;
+             p >> res;
+             BOOST_CHECK_EQUAL(res, "goody");
+             BOOST_CHECK(!(p >> res));
+           }
+         },
+         [] (infinit::protocol::Serializer& s)
+         {
+           {
+             infinit::protocol::Packet p = s.read();
+             std::string some, data;
+             int i;
+             p >> some;
+             BOOST_CHECK_EQUAL(some, "some");
+             p >> data;
+             BOOST_CHECK_EQUAL(data, "data");
+             p >> i;
+             BOOST_CHECK_EQUAL(i, 42);
+             BOOST_CHECK(!(p >> data));
+           }
+           {
+             infinit::protocol::Packet p;
+             p << "goody";
+             s.write(p);
+           }
+         });
+}
 
-  reactor::Thread main(
-    sched, "main",
-    [&] ()
+static
+void
+connection_lost_reader()
+{
+  dialog(
+    [] (SocketInstrumentation& sockets)
     {
-      Serializers serializers;
-      auto& alice = *serializers.alice;
-      auto& bob = *serializers.bob;
-
-      elle::With<reactor::Scope>() << [&](reactor::Scope& scope)
-      {
-        scope.run_background(
-          "alice", [&]
-          {
-            {
-              {
-                infinit::protocol::Packet p;
-                p << "some data 42";
-                alice.write(p);
-              }
-              {
-                infinit::protocol::Packet p = alice.read();
-                std::string res;
-                p >> res;
-                BOOST_CHECK_EQUAL(res, "goody");
-                BOOST_CHECK(!(p >> res));
-              }
-            }
-          });
-        scope.run_background(
-          "bob", [&]
-          {
-            {
-              infinit::protocol::Packet p = bob.read();
-              std::string some, data;
-              int i;
-              p >> some;
-              BOOST_CHECK_EQUAL(some, "some");
-              p >> data;
-              BOOST_CHECK_EQUAL(data, "data");
-              p >> i;
-              BOOST_CHECK_EQUAL(i, 42);
-              BOOST_CHECK(!(p >> data));
-            }
-            {
-              infinit::protocol::Packet p;
-              p << "goody";
-              bob.write(p);
-            }
-          });
-        scope.wait();
-      };
+      sockets.alice_quota(96);
+    },
+    [] (infinit::protocol::Serializer& s)
+    {
+      infinit::protocol::Packet p;
+      char buffer[1025];
+      memset(buffer, 0xAA, sizeof(buffer));
+      buffer[sizeof(buffer) - 1] = 0;
+      p << buffer;
+      s.write(p);
+    },
+    [] (infinit::protocol::Serializer& s)
+    {
+      BOOST_CHECK_THROW(s.read(), reactor::network::ConnectionClosed);
     });
+}
 
-  sched.run();
+static
+void
+connection_lost_sender()
+{
+  dialog(
+    [] (SocketInstrumentation& sockets)
+    {
+      sockets.alice_quota(4);
+    },
+    [] (infinit::protocol::Serializer& s)
+    {
+      infinit::protocol::Packet p;
+      p << "data";
+      s.write(p);
+      // Getting an error from TCP writes is a bit touchy.
+      try
+      {
+        while (true)
+        {
+          reactor::sleep(100_ms);
+          s.write(p);
+        }
+      }
+      catch (reactor::network::ConnectionClosed const&)
+      {
+        return;
+      }
+      BOOST_FAIL("ConnectionClosed exception was expected");
+    },
+    [] (infinit::protocol::Serializer&)
+    {});
+}
+
+static
+void
+corruption()
+{
+  dialog(
+    [] (SocketInstrumentation& sockets)
+    {
+      sockets.alice_corrupt(1024);
+    },
+    [] (infinit::protocol::Serializer& s)
+    {
+      infinit::protocol::Packet p;
+      char buffer[1025];
+      memset(buffer, 0xAA, sizeof(buffer));
+      buffer[sizeof(buffer) - 1] = 0;
+      p << buffer;
+      s.write(p);
+    },
+    [] (infinit::protocol::Serializer& s)
+    {
+      BOOST_CHECK_THROW(s.read(), infinit::protocol::ChecksumError);
+    });
 }
 
 static
@@ -183,6 +308,9 @@ test_suite()
 {
   auto& suite = boost::unit_test::framework::master_test_suite();
   suite.add(BOOST_TEST_CASE(exchange_packets), 0, 3);
+  suite.add(BOOST_TEST_CASE(connection_lost_reader), 0, 3);
+  suite.add(BOOST_TEST_CASE(connection_lost_sender), 0, 3);
+  suite.add(BOOST_TEST_CASE(corruption), 0, 3);
   return true;
 }
 
