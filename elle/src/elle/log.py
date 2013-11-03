571b1f43b99ce47a98c974baa6277d8378865212
mefyl <quentin.hocquet@infinit.io>
2013-11-03 15:41:34 +0100

    elle.log: Add python binding.

---
+++ elle/src/elle/log.py
@@ -0,0 +1,31 @@
+import elle._log
+
+def message(level, type, message):
+  import inspect
+  frame = inspect.currentframe().f_back.f_back
+  file, line, function, source, index = inspect.getframeinfo(frame)
+  key = 'ELLE_LOG_COMPONENT'
+  if key in frame.f_locals:
+    component = frame.f_locals[key]
+  else:
+    component = frame.f_globals[key]
+  return elle._log.Send(level, type, component,
+                    file, line, function, message)
+
+def log(msg):
+  return message(elle._log.level.log, elle._log.type.info, msg)
+
+def trace(msg):
+  return message(elle._log.level.trace, elle._log.type.info, msg)
+
+def debug(msg):
+  return message(elle._log.level.debug, elle._log.type.info, msg)
+
+def dump(msg):
+  return message(elle._log.level.dump, elle._log.type.info, msg)
+
+def warn(msg):
+  return message(elle._log.level.log, elle._log.type.warning, msg)
+
+def err(msg):
+  return message(elle._log.level.log, elle._log.type.error, msg)
---
+++ elle/src/elle/log/python.cc
@@ -0,0 +1,114 @@
+#include <boost/python.hpp>
+
+#include <elle/log/Logger.hh>
+#include <elle/log/TextLogger.hh>
+#include <elle/log/Send.hh>
+#include <elle/memory.hh>
+
+using boost::python::class_;
+using boost::python::def;
+using boost::python::enum_;
+
+// Pacify -Wmissing-declarations
+extern "C"
+{
+  PyObject* PyInit__log();
+}
+
+class TextLogger: public elle::log::TextLogger
+{
+public:
+  TextLogger():
+    elle::log::TextLogger(std::cerr)
+  {}
+};
+
+static
+void
+set_logger(boost::python::object& object)
+{
+  elle::generic_unique_ptr<elle::log::Logger> logger(
+    boost::python::extract<elle::log::Logger*>(object),
+    [] (elle::log::Logger*)
+    {
+      /* The logger will be deleted by python itself upon exit */
+    });
+  boost::python::incref(object.ptr());
+  elle::log::logger(std::move(logger));
+}
+
+class Send: public elle::log::detail::Send
+{
+public:
+  Send(elle::log::Logger::Level level,
+       elle::log::Logger::Type type,
+       std::string const& component,
+       std::string const& file,
+       unsigned int line,
+       std::string const& function,
+       std::string const& message):
+    elle::log::detail::Send(
+      level,
+      type,
+      false,
+      component,
+      file.c_str(),
+      line,
+      function.c_str(),
+      message.c_str()) // FIXME: replace % by %%
+  {}
+
+  void
+  enter()
+  {
+    this->_indent();
+  }
+
+  void
+  exit(boost::python::object const&,
+       boost::python::object const&,
+       boost::python::object const&)
+  {
+    this->_unindent();
+  }
+};
+
+BOOST_PYTHON_MODULE(_log)
+{
+  enum_<elle::log::Logger::Level>("level")
+    .value("none", elle::log::Logger::Level::none)
+    .value("log", elle::log::Logger::Level::log)
+    .value("trace", elle::log::Logger::Level::trace)
+    .value("debug", elle::log::Logger::Level::debug)
+    .value("dump", elle::log::Logger::Level::dump)
+    ;
+  enum_<elle::log::Logger::Type>("type")
+    .value("info", elle::log::Logger::Type::info)
+    .value("warning", elle::log::Logger::Type::warning)
+    .value("error", elle::log::Logger::Type::error)
+    ;
+  class_<elle::log::Logger,
+         boost::noncopyable>
+    ("Logger", boost::python::no_init)
+    //.def("message", &elle::log::Logger::message)
+    ;
+  class_<TextLogger,
+         boost::python::bases<elle::log::Logger>,
+         boost::noncopyable>
+    ("TextLogger", boost::python::init<>())
+    ;
+  def("set_logger", &set_logger);
+  class_<Send,
+         boost::noncopyable>
+    ("Send", boost::python::init<
+     elle::log::Logger::Level,
+     elle::log::Logger::Type,
+     std::string const&,
+     std::string const&,
+     unsigned int,
+     std::string const&,
+     std::string const&>())
+    .def("__enter__", &Send::enter)
+    .def("__exit__", &Send::exit)
+    ;
+}
