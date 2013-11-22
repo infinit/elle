ad48865d0ddcd0cb82e0f253580d0e1c2e6f9cad
mefyl <quentin.hocquet@infinit.io>
2013-11-22 18:55:24 +0100

    elle.Version: Fix deserialization of old versions.

--- elle/src/elle/Version.hxx
+++ elle/src/elle/Version.hxx
@@ -5,15 +5,30 @@
 
 ELLE_SERIALIZE_STATIC_FORMAT(elle::Version, 1);
 
-ELLE_SERIALIZE_SIMPLE(elle::Version,
-                      archive,
-                      value,
-                      version)
+ELLE_SERIALIZE_SPLIT(elle::Version);
+
+ELLE_SERIALIZE_SPLIT_SAVE(elle::Version,
+                          archive,
+                          value,
+                          version)
 {
   archive & value._major;
   archive & value._minor;
-  if (version == 1)
+  if (version >= 1)
     archive & value._subminor;
+}
+
+ELLE_SERIALIZE_SPLIT_LOAD(elle::Version,
+                          archive,
+                          value,
+                          version)
+{
+  archive & value._major;
+  archive & value._minor;
+  if (version >= 1)
+    archive & value._subminor;
+  else
+    value._subminor = 0;
 }
 
 #endif
