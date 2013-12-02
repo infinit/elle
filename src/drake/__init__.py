aa5d89d0c23997397e8381bee4486dfea89dc9d3
mefyl <mefyl@gruntech.org>
2013-12-02 10:41:37 +0100

    Copy: assert the source is not the target.

--- src/drake/__init__.py
+++ src/drake/__init__.py
@@ -2561,6 +2561,9 @@ class Copy(Builder):
     """
     self.__source = source
     self.__target = source.clone(to.canonize())
+    if self.__source is self.__target:
+      raise Exception('source and destination are the same: %s' %
+                      self.__source)
     try:
       Builder.__init__(self, [self.__source], [self.__target])
     except BuilderRedefinition:
