cd21313a82c3b7c79ebd8ccd1dbec249fbf90771
Louis Feuvrier <louis.feuvrier@infinit.io>
2013-12-04 17:43:10 +0100

    reactor.network: Basic test of data transfer is now working.

--- reactor/tests/reactor/ssl.cc
+++ reactor/tests/reactor/ssl.cc
@@ -5,6 +5,8 @@
 #define BOOST_TEST_MODULE SSL_Socket
 
 #include <reactor/scheduler.hh>
+#include <reactor/Scope.hh>
+#include <reactor/Barrier.hh>
 #include <reactor/thread.hh>
 #include <reactor/network/ssl-socket.hh>
 #include <reactor/network/ssl-server.hh>
@@ -23,24 +25,49 @@ using reactor::network::SSLServer;
 BOOST_AUTO_TEST_CASE(basic)
 {
   reactor::Scheduler sched;
-
-  auto serv_func = []
-  {
-    SSLCertif certif(SERVCERT, SERVKEY, DHFILE);
-
-    certif.release();
-  };
-
-  auto client_func = [] (reactor::Thread* serv)
-  {
-    SSLCertif certif(CERTAUTH);
-    certif.release();
-
-    serv->terminate_now();
-  };
-
-  reactor::Thread* server = new reactor::Thread(sched, "server", serv_func);
-  new reactor::Thread(sched, "client", std::bind(client_func, server));
-
+  reactor::Barrier listening;
+  int port = 0;
+  reactor::Thread t(
+    sched, "main",
+    [&]
+    {
+      elle::With<reactor::Scope>() << [&] (reactor::Scope& scope)
+      {
+        scope.run_background(
+          "server",
+          [&]
+          {
+            SSLCertif certif(SERVCERT, SERVKEY, DHFILE);
+              reactor::network::SSLServer server(certif);
+              server.listen(0);
+              port = server.port();
+              listening.open();
+              std::unique_ptr<reactor::network::SSLSocket> socket(
+                server.accept());
+              static char servdata[5] = { 0 };
+              socket->std::iostream::read(servdata, 4);
+              BOOST_CHECK(std::string(servdata) == std::string("lulz"));
+              socket->write(std::string("lol"));
+              socket->write(std::string("lulz"));
+            certif.release();
+          });
+        scope.run_background(
+          "client",
+          [&]
+          {
+            SSLCertif certif;
+              reactor::wait(listening);
+              reactor::network::SSLSocket socket("127.0.0.1", std::to_string(port), certif);
+              socket.write(std::string("lulz"));
+              static char clientdata[5] = { 0 };
+              socket.std::iostream::read(clientdata, 3);
+              BOOST_CHECK(std::string(clientdata) == std::string("lol"));
+              socket.std::iostream::read(clientdata, 4);
+              BOOST_CHECK(std::string(clientdata) == std::string("lulz"));
+            certif.release();
+          });
+        reactor::wait(scope);
+      };
+    });
   sched.run();
 }
