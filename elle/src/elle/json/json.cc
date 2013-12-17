9eb8d9dd8610c537434d9ac137231b0dafb24ed3
Christopher Crone <christopher.crone@infinit.io>
2013-12-17 09:13:55 +0100

    elle.json: Add unsigned int and unsigned long long types.

--- elle/src/elle/json/json.cc
+++ elle/src/elle/json/json.cc
@@ -88,6 +88,10 @@ namespace elle
         return boost::any_cast<bool>(any);
       if (any.type() == typeid(int))
         return boost::any_cast<int>(any);
+      if (any.type() == typeid(unsigned int))
+        return boost::any_cast<unsigned int>(any);
+      if (any.type() == typeid(unsigned long long))
+          return boost::any_cast<unsigned long long>(any);
       if (any.type() == typeid(float))
         return boost::any_cast<float>(any);
       if (any.type() == typeid(double))
