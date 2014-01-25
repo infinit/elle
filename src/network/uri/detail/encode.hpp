341b56527a7541792549fadc2251954c036762f6
Glyn Matthews <glyn.matthews@gmail.com>
2014-01-25 16:28:39 +0100

    Added test for authority bug: https://github.com/cpp-netlib/cpp-netlib/issues/364.

--- src/network/uri/detail/encode.hpp
+++ src/network/uri/detail/encode.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) Glyn Matthews 2011, 2012, 2013.
+// Copyright (c) Glyn Matthews 2011, 2012, 2013, 2014.
 // Copyright 2012 Dean Michael Berris <dberris@google.com>
 // Copyright 2012 Google, Inc.
 // Distributed under the Boost Software License, Version 1.0.
