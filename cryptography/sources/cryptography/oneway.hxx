c17297ccd1c3dbc1f0053ce9e2ecbb6ad52faaf5
Christopher Crone <christopher.crone@infinit.io>
2014-02-25 17:18:31 +0100

    cryptography.oneway: Add HMAC function.

--- cryptography/sources/cryptography/oneway.hxx
+++ cryptography/sources/cryptography/oneway.hxx
@@ -39,6 +39,26 @@ namespace infinit
         ELLE_MEASURE_SCOPE("Hash the buffer");
         return (hash(Plain(elle::WeakBuffer(buffer)), algorithm));
       }
+
+      template <typename T>
+      Digest
+      hmac(T const& value,
+           Digest const& key,
+           Algorithm algorithm)
+      {
+        ELLE_LOG_COMPONENT("infinit.cryptography.oneway");
+        ELLE_DEBUG_FUNCTION(value, algorithm);
+
+        static_assert(!std::is_same<T, Plain>::value,
+                      "this call should never have occured");
+
+        elle::Buffer buffer;
+        ELLE_MEASURE("Serialize into the buffer")
+          buffer.writer() << value;
+
+        ELLE_MEASURE_SCOPE("HMAC the buffer");
+        return (hmac(Plain(elle::WeakBuffer(buffer)), key, algorithm));
+      }
     }
   }
 }
