545288e42d94e9b90dbab02cfec3cd12782cdc68
Christopher Crone <christopher.crone@infinit.io>
2014-02-25 17:20:45 +0100

    reactor.http.EscapedString: Add class for URI encoding strings.

---
+++ reactor/src/reactor/http/EscapedString.hh
@@ -0,0 +1,40 @@
+#ifndef REACTOR_HTTP_ESCAPED_STRING_HH
+# define REACTOR_HTTP_ESCAPED_STRING_HH
+
+# include <string>
+
+# include <elle/attribute.hh>
+# include <elle/Printable.hh>
+
+namespace reactor
+{
+  namespace http
+  {
+    class EscapedString:
+      public elle::Printable
+    {
+      /*-------------.
+      | Construction |
+      `-------------*/
+    public:
+      EscapedString(std::string const& str);
+
+      ELLE_ATTRIBUTE_R(std::string, escaped_string);
+
+    private:
+      std::string
+      _curl_format(std::string const& str);
+
+      /*----------.
+      | Printable |
+      `----------*/
+    public:
+      virtual
+      void
+      print(std::ostream& stream) const;
+
+    };
+  }
+}
+
+#endif
