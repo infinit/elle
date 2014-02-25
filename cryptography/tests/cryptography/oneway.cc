bb500dab4381ae0c6d4ac5a5caba044cd74f0308
Christopher Crone <christopher.crone@infinit.io>
2014-02-25 17:18:57 +0100

    cryptography.oneway.test: Add tests for HMAC function.

--- cryptography/tests/cryptography/oneway.cc
+++ cryptography/tests/cryptography/oneway.cc
@@ -14,7 +14,7 @@
 | Represent |
 `----------*/
 
-static elle::String const _input(
+static std::string const _input(
   "- Do you think she's expecting something big?"
   "- You mean, like anal?");
 
@@ -28,12 +28,11 @@ test_represent_n()
     infinit::cryptography::Digest digest =
       infinit::cryptography::oneway::hash(
         infinit::cryptography::Plain(
-          elle::WeakBuffer(reinterpret_cast<void*>(const_cast<char*>(_input.c_str())),
-                           _input.length())),
+          elle::ConstWeakBuffer(_input)),
         A);
-    elle::String representation =
+    std::string representation =
       elle::format::hexadecimal::encode(digest.buffer());
-    elle::printf("[representation %s] %s\n", N, representation);
+    elle::sprintf("[representation %s] %s\n", N, representation);
   }
 }
 
@@ -61,6 +60,47 @@ test_represent()
   test_represent_n<6, infinit::cryptography::oneway::Algorithm::sha384>();
   // SHA-512.
   test_represent_n<7, infinit::cryptography::oneway::Algorithm::sha512>();
+}
+
+template <elle::Natural32 N,
+          infinit::cryptography::oneway::Algorithm A>
+void
+test_represent_hmac_n()
+{
+  {
+    using namespace infinit::cryptography;
+    Digest key(elle::Buffer("key", 3));
+    Digest digest = oneway::hmac(Plain(elle::ConstWeakBuffer(_input)), key, A);
+    std::string representation =
+      elle::format::hexadecimal::encode(digest.buffer());
+    elle::sprintf("[representation %s] %s\n", N, representation);
+  }
+}
+
+static
+void
+test_hmac_represent()
+{
+  // WARNING: To uncomment only if one wants to update the representations.
+  return;
+
+  // These generate hexadecimal-based representations which can be used in
+  // other tests.
+
+  // MD5.
+  test_represent_hmac_n<1, infinit::cryptography::oneway::Algorithm::md5>();
+  // SHA.
+  test_represent_hmac_n<2, infinit::cryptography::oneway::Algorithm::sha>();
+  // SHA-1.
+  test_represent_hmac_n<3, infinit::cryptography::oneway::Algorithm::sha1>();
+  // SHA-224.
+  test_represent_hmac_n<4, infinit::cryptography::oneway::Algorithm::sha224>();
+  // SHA-256.
+  test_represent_hmac_n<5, infinit::cryptography::oneway::Algorithm::sha256>();
+  // SHA-384.
+  test_represent_hmac_n<6, infinit::cryptography::oneway::Algorithm::sha384>();
+  // SHA-512.
+  test_represent_hmac_n<7, infinit::cryptography::oneway::Algorithm::sha512>();
 }
 
 /*--------.
@@ -128,6 +168,59 @@ test_operate()
   test_operate_x<infinit::cryptography::oneway::Algorithm::sha512, 9223>("8bae3559218793c6634d17867d948069ecbc0a2d6b6d7faeb1b37b0cb14cda18e3375225809d9b0e2a68a68b65818fbe3e737a76537e55de8c288cf7cf52d47b");
 }
 
+template <infinit::cryptography::oneway::Algorithm A,
+          elle::Natural32 S>
+void
+test_hmac_operate_x(elle::String const& R)
+{
+  using namespace infinit::cryptography;
+  Digest key(elle::Buffer("key", 3));
+  // HMAC a plain.
+  {
+    Digest digest1 = oneway::hmac(Plain(elle::ConstWeakBuffer(_input)), key, A);
+    Digest digest2 = oneway::hmac(Plain(elle::ConstWeakBuffer(_input)), key, A);
+
+    BOOST_CHECK_EQUAL(digest1, digest2);
+
+    elle::Buffer buffer = elle::format::hexadecimal::decode(R);
+
+    BOOST_CHECK_EQUAL(digest1.buffer(), buffer);
+  }
+
+  // HMAC a complex type.
+  {
+    Sample const input(
+      42, infinit::cryptography::random::generate<elle::String>(S));
+
+    infinit::cryptography::Digest digest1 =
+      infinit::cryptography::oneway::hmac(input, key, A);
+    infinit::cryptography::Digest digest2 =
+      infinit::cryptography::oneway::hmac(input, key, A);
+
+    BOOST_CHECK_EQUAL(digest1, digest2);
+  }
+}
+
+static
+void
+test_hmac_operate()
+{
+  // MD5 based on [representation 1].
+  test_hmac_operate_x<infinit::cryptography::oneway::Algorithm::md5, 820>("f580b9c649f64331bd0add676e13e03d");
+  // SHA based on [representation 2].
+  test_hmac_operate_x<infinit::cryptography::oneway::Algorithm::sha, 31>("8f23a8944e8abf5062743cc8604ab1b37c4ed2d1");
+  // SHA-1 based on [representation 3].
+  test_hmac_operate_x<infinit::cryptography::oneway::Algorithm::sha1, 9028>("ad149753940c4a9e3faf56e4a7125acd28f0454f");
+  // SHA-224 based on [representation 4].
+  test_hmac_operate_x<infinit::cryptography::oneway::Algorithm::sha224, 630>("1293b424873159446fd0c23e1f0367f186591b7acaadd2668b492c10");
+  // SHA-256 based on [representation 5].
+  test_hmac_operate_x<infinit::cryptography::oneway::Algorithm::sha256, 73>("ea95ff42cb433311cadac495946fc03fd370f42c4f7214b56d6e5fb02029ad3b");
+  // SHA-384 based on [representation 6].
+  test_hmac_operate_x<infinit::cryptography::oneway::Algorithm::sha384, 933>("83461c33c0f18386dd2092a9937c07205db402a5c0c520981804fb5f893f5fe13f8ef6dfeb7490a44835305d3184989e");
+  // SHA-512 based on [representation 7].
+  test_hmac_operate_x<infinit::cryptography::oneway::Algorithm::sha512, 9223>("74ea6d290be470f30784675e2798376e662669189758afd03fd24d917152dba29f95f35e27d167a6dd1b7be28fdfe8ae68d54aa70ba6c90b03b0e3c8676b5c1d");
+}
+
 /*-----.
 | Main |
 `-----*/
@@ -139,5 +232,7 @@ ELLE_TEST_SUITE()
   auto& suite = boost::unit_test::framework::master_test_suite();
 
   suite.add(BOOST_TEST_CASE(test_represent));
+  suite.add(BOOST_TEST_CASE(test_hmac_represent));
   suite.add(BOOST_TEST_CASE(test_operate));
+  suite.add(BOOST_TEST_CASE(test_hmac_operate));
 }
