b9ced3bb63d0aeaa18e50a15a113676b0359ea77
Christopher Crone <christopher.crone@infinit.io>
2014-02-27 10:03:56 +0100

    aws.Credentials: Add check for expiry of credentials.

--- aws/src/aws/Credentials.cc
+++ aws/src/aws/Credentials.cc
@@ -1,16 +1,33 @@
 #include <aws/Credentials.hh>
 
 #include <elle/printf.hh>
+#include <elle/log.hh>
+
+ELLE_LOG_COMPONENT("aws.Credentials");
 
 namespace aws
 {
   Credentials::Credentials(std::string const& access_id,
                            std::string const& secret_access_key,
-                           std::string const& security_token):
+                           std::string const& security_token,
+                           std::string const& expiry):
     _access_id(access_id),
     _secret_access_key(secret_access_key),
-    _security_token(security_token)
-  {}
+    _security_token(security_token),
+    _expiry_str(expiry)
+  {
+    // Take the AWS server response and convert it to a ptime.
+    // eg: 2014-02-26T23:34:19Z -> 20140226T233419
+    std::string time_str;
+    for (auto c: this->_expiry_str)
+    {
+      if (c != '-' && c != ':' && c != 'Z')
+      {
+        time_str += c;
+      }
+    }
+    this->_expiry = boost::posix_time::from_iso_string(time_str);
+  }
 
   std::string
   Credentials::credential_string(RequestTime const& request_time,
@@ -27,5 +44,31 @@ namespace aws
       aws_service
     );
     return res;
+  }
+
+  bool
+  Credentials::valid()
+  {
+    using namespace boost::posix_time;
+    ptime now = second_clock::universal_time();
+    bool res;
+    if (this->_expiry < now)
+    {
+      ELLE_DEBUG("%s: credentials are have expired", *this);
+      res = false;
+    }
+    else
+    {
+      ELLE_DEBUG("%s: credentials are valid", *this);
+      res = true;
+    }
+    return res;
+  }
+
+  void
+  Credentials::print(std::ostream& stream) const
+  {
+    stream << "AWS credentials: access_id=" << this->_access_id
+           << "expiry=" << this->_expiry;
   }
 }
--- aws/src/aws/Credentials.hh
+++ aws/src/aws/Credentials.hh
@@ -6,12 +6,14 @@
 # include <boost/date_time/posix_time/posix_time.hpp>
 
 # include <elle/attribute.hh>
+# include <elle/Printable.hh>
 
 # include <aws/Keys.hh>
 
 namespace aws
 {
-  class Credentials
+  class Credentials:
+    public elle::Printable
   {
       /*-------------.
       | Construction |
@@ -19,16 +21,30 @@ namespace aws
     public:
       Credentials(std::string const& access_id,
                   std::string const& secret_access_key,
-                  std::string const& security_token);
+                  std::string const& security_token,
+                  std::string const& expiry);
 
       std::string
       credential_string(RequestTime const& request_time,
                         Region const& aws_region,
                         Service const& aws_service);
 
+      bool
+      valid();
+
       ELLE_ATTRIBUTE_R(std::string, access_id);
       ELLE_ATTRIBUTE_R(std::string, secret_access_key);
       ELLE_ATTRIBUTE_R(std::string, security_token);
+      ELLE_ATTRIBUTE_R(std::string, expiry_str);
+      ELLE_ATTRIBUTE_R(boost::posix_time::ptime, expiry);
+
+      /*----------.
+      | Printable |
+      `----------*/
+    public:
+      virtual
+      void
+      print(std::ostream& stream) const;
   };
 }
 
