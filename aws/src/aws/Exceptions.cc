462c0eae3caadcd6daeb4e0a59f9b0def8461991
Christopher Crone <christopher.crone@infinit.io>
2014-02-27 09:28:49 +0100

    aws.Exceptions: Add missing constructor.

--- aws/src/aws/Exceptions.cc
+++ aws/src/aws/Exceptions.cc
@@ -1,7 +1,14 @@
 #include <aws/Exceptions.hh>
 
+#include <elle/printf.hh>
+
 namespace aws
 {
+  AWSException::AWSException(std::string const& error):
+    Super(elle::sprintf("AWS error: %s", error)),
+    _error(error)
+  {}
+
   RequestError::RequestError(std::string const& error):
     AWSException(error)
   {}
