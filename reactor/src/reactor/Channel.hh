e605889c8642dcbb9269cf7f973320a959b3b258
Matthieu Nottale <matthieu.nottale@infinit.io>
2014-03-31 16:26:36 -0400

    reactor.Channel: Support max size and priority_queue as container.

--- reactor/src/reactor/Channel.hh
+++ reactor/src/reactor/Channel.hh
@@ -2,6 +2,7 @@
 #  define INFINIT_REACTOR_CHANNEL_HH
 
 #include <queue>
+#include <limits>
 
 #include <elle/attribute.hh>
 
@@ -9,7 +10,7 @@
 
 namespace reactor
 {
-  template <class T>
+  template <class T, class Container = std::queue<T>>
   class Channel
   {
   public:
@@ -18,12 +19,21 @@ namespace reactor
     put(T data);
     T
     get();
+    const T&
+    peek();
     unsigned int
     size() const;
 
   private:
-    ELLE_ATTRIBUTE(Barrier, barrier);
-    ELLE_ATTRIBUTE(std::queue<T>, queue);
+    ELLE_ATTRIBUTE(Barrier, read_barrier);
+    ELLE_ATTRIBUTE(Barrier, write_barrier);
+    ELLE_ATTRIBUTE(Container, queue);
+    /// Maximum size, will block writers *after* insertion if reached
+    ELLE_ATTRIBUTE_Rw(int, max_size);
+    enum
+    {
+      SizeUnlimited = std::numeric_limits<int>::max()
+    };
   };
 }
 
--- reactor/src/reactor/Channel.hxx
+++ reactor/src/reactor/Channel.hxx
@@ -5,43 +5,83 @@
 
 namespace reactor
 {
-  template <typename T>
-  Channel<T>::Channel():
-    _barrier(Barrier()),
-    _queue(std::queue<T>())
+  template <typename T, typename Container>
+  Channel<T, Container>::Channel()
+    : _read_barrier(Barrier())
+    , _write_barrier(Barrier())
+    , _queue(Container())
+    , _max_size(SizeUnlimited)
   {}
 
-  template <typename T>
+  template <typename T, typename Container>
   void
-  Channel<T>::put(T data)
+  Channel<T, Container>::put(T data)
   {
     this->_queue.push(std::move(data));
-    this->_barrier.open();
+    this->_read_barrier.open();
+    while (this->_queue.size() >= this->_max_size)
+      reactor::wait(this->_write_barrier);
   }
 
-  template <typename T>
+  namespace details
+  {
+    template<typename T>
+    typename T::value_type& queue_front(T& container)
+    {
+      return container.front();
+    }
+    template<typename T>
+    typename std::priority_queue<T>::value_type& queue_front(std::priority_queue<T>& container)
+    {
+      // top() returns const& because modifying the object would break the
+      // priority_queue invariants.
+      // Let's hope that poping it rigth after is safe. It would make sense.
+      return const_cast<typename std::priority_queue<T>::value_type&>(container.top());
+    }
+  }
+
+  template <typename T, typename Container>
   T
-  Channel<T>::get()
+  Channel<T, Container>::get()
   {
     /// In case of the barrier was opened
     /// with a last element, and closed immediatly
     /// Be sure the barrier is clearly opened before
     /// get element.
-    while(!this->_barrier.opened())
-      reactor::wait(this->_barrier);
+    while(!this->_read_barrier.opened())
+      reactor::wait(this->_read_barrier);
     ELLE_ASSERT(!this->_queue.empty());
-    T res(std::move(this->_queue.front()));
+    T res(std::move(details::queue_front(this->_queue)));
     this->_queue.pop();
     if (this->_queue.empty())
-      this->_barrier.close();
+      this->_read_barrier.close();
+    if (this->_queue.size() < this->_max_size)
+      this->_write_barrier.open();
     return res;
   }
 
-  template <typename T>
+  template <typename T, typename Container>
   unsigned int
-  Channel<T>::size() const
+  Channel<T, Container>::size() const
   {
     return this->_queue.size();
+  }
+
+  template <typename T, typename Container>
+  void
+  Channel<T, Container>::max_size(int ms)
+  {
+    this->_max_size = ms;
+    if (this->_max_size < this->_queue.size())
+      this->_write_barrier.open();
+    // no need to close, next write will do that
+  }
+
+  template <typename T, typename Container>
+  const T&
+  Channel<T, Container>::peek()
+  {
+    return details::queue_front(this->_queue);
   }
 }
 
