13d255382f43322a68c7fa3bd24c514e4efe245c
mefyl <quentin.hocquet@infinit.io>
2014-04-07 12:20:16 -0400

    elle.filesystem.TemporaryDirectory: Fix OSX compilation.

--- elle/src/elle/filesystem/TemporaryDirectory.cc
+++ elle/src/elle/filesystem/TemporaryDirectory.cc
@@ -1,5 +1,6 @@
 #if defined(INFINIT_MACOSX)
 # include <mach-o/dyld.h>
+# include <sys/param.h>
 #endif
 
 #include <elle/assert.hh>
@@ -11,7 +12,12 @@ static boost::filesystem::path executable_path()
 #if defined(INFINIT_LINUX)
   return boost::filesystem::read_symlink("/proc/self/exe");
 #elif defined(INFINIT_MACOSX)
-  return _NSGetExecutablePath();
+  uint32_t size = MAXPATHLEN;
+  char result[size];
+  if (_NSGetExecutablePath(result, &size) == 0)
+    return result;
+  else
+    throw elle::Exception("unable to get executable path");
 #elif defined(INFINIT_WINDOWS)
   char result[1024];
   auto size = GetModuleFileName(0, result, sizeof(result));
