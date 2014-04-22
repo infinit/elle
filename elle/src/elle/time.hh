ce56d09fc7376ab19a1a97cb17c19b21f4f4f07e
mefyl <quentin.hocquet@infinit.io>
2014-04-22 12:19:13 -0400

    elle.time: Use elle/windows.h.

--- elle/src/elle/time.hh
+++ elle/src/elle/time.hh
@@ -1,9 +1,7 @@
 #ifndef ELLE_TIME_HH
 # define ELLE_TIME_HH
 
-# ifdef INFINIT_WINDOWS
-#  include <winsock2.h>
-# endif
+# include <elle/windows.h>
 # include <boost/date_time/posix_time/posix_time.hpp>
 
 #endif
