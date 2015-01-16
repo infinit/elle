55a2db9dda1d1ef463c262cec1967abd9f21099f
mefyl <mefyl@gruntech.org>
2015-01-16 15:00:56 +0100

    cryptography.bn: Add BIGNUM serialization.

--- cryptography/sources/cryptography/bn.hh
+++ cryptography/sources/cryptography/bn.hh
@@ -5,13 +5,38 @@
 
 # include <openssl/bn.h>
 
+# include <elle/serialization/Serializer.hh>
+
 /*----------.
 | Operators |
 `----------*/
 
+
 std::ostream&
 operator <<(std::ostream& stream,
             ::BIGNUM const& bignum);
+
+/*--------------.
+| Serialization |
+`--------------*/
+
+namespace elle
+{
+  namespace serialization
+  {
+    template <>
+    struct Serialize<::BIGNUM*>
+    {
+      typedef std::string Type;
+      static
+      void
+      convert(::BIGNUM*& bignum, std::string& repr);
+      static
+      void
+      convert(std::string& repr, ::BIGNUM*& bn);
+    };
+  }
+}
 
 # include <cryptography/bn.hxx>
 
