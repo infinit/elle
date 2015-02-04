c07e44fd3e6a73afc392d0dc5bba414c847b5eca
mefyl <quentin.hocquet@infinit.io>
2015-02-04 10:23:54 +0100

    Builder: Simplify.

--- src/drake/__init__.py
+++ src/drake/__init__.py
@@ -1812,43 +1812,27 @@ class Builder:
                         drake.log.LogLevel.debug,
                         '%s: build static dependencies', self):
           run_builders = set()
-          built_dependencies = set()
           with sched.Scope() as scope:
             for node in chain(self.__sources.values(),
                               self.__vsrcs.values()):
               if node.skippable():
                 continue
-              skip = True
-              if node.builder not in run_builders:
-                skip = False
-                run_builders.add(node.builder)
-              if isinstance(node, Node):
-                for dep in node.dependencies:
-                  skip = False
-                  built_dependencies.add(dep)
-              if not skip:
-                scope.run(node.build, str(node))
+              if node.builder in run_builders and not node.dependencies:
+                continue
+              scope.run(node.build, str(node))
         # Build dynamic dependencies
         with logger.log('drake.Builder',
                         drake.log.LogLevel.debug,
                         '%s: build dynamic dependencies', self):
           run_builders = set()
-          built_dependencies = set()
           try:
             with sched.Scope() as scope:
               for node in self.__dynsrc.values():
                 if node.skippable():
                   continue
-                skip = True
-                if node.builder not in run_builders:
-                  skip = False
-                  run_builders.add(node.builder)
-                if isinstance(node, Node):
-                  for dep in node.dependencies:
-                    skip = False
-                    built_dependencies.add(dep)
-                if not skip:
-                  scope.run(node.build, str(node))
+                if node.builder in run_builders and not node.dependencies:
+                  continue
+                scope.run(node.build, str(node))
           except Exception as e:
             logger.log(
               'drake.Builder',
