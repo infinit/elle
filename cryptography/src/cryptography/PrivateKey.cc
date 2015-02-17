f9ad04a471d74f2c3e0798a82ebe7cbcdf884a79
Julien Quintard <julien.quintard@infinit.io>
2015-02-17 15:55:27 +0100

    cryptography: Rework the trace messages to avoid overloading in the logger.

--- cryptography/src/cryptography/PrivateKey.cc
+++ cryptography/src/cryptography/PrivateKey.cc
@@ -70,9 +70,11 @@ namespace infinit
     Clear
     PrivateKey::decrypt(Code const& code) const
     {
-      ELLE_TRACE_SCOPE("%s: decrypt", *this);
+      ELLE_TRACE_SCOPE("%s: decrypt()", *this);
       ELLE_DUMP("code: %x", code);
+
       ELLE_ASSERT_NEQ(this->_implementation, nullptr);
+
       return (this->_implementation->decrypt(code));
     }
 
