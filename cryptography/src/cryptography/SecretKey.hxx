8e90bfbf52da8924b09d711e12c0c7364b97f3f8
Julien Quintard <julien.quintard@infinit.io>
2015-02-18 16:56:51 +0100

    Rearranged some classes for legacy code to work.

--- cryptography/src/cryptography/SecretKey.hxx
+++ cryptography/src/cryptography/SecretKey.hxx
@@ -23,12 +23,22 @@ namespace infinit
 
       static_assert(std::is_same<T, Plain>::value == false,
                     "this call should never have occured");
+      /* XXX to put back once the legacy code below is no longer in use:
+  infinit::cryptography::Code
+  Frete::read(FileID f, FileOffset start, FileSize size)
+  {
+    ELLE_DEBUG("%s: read and encrypt block %s of size %s at offset %s with old key %s",
+    *this, f, size, start, this->_impl->old_key());
+    return this->_impl->old_key().encrypt(this->cleartext_read(f, start, size));
+  }
+
       static_assert(std::is_same<T, elle::Buffer>::value == false,
                     "this call should never have occured");
       static_assert(std::is_same<T, elle::WeakBuffer>::value == false,
                     "this call should never have occured");
       static_assert(std::is_same<T, elle::ConstWeakBuffer>::value == false,
                     "this call should never have occured");
+      */
 
       // Serialize the value.
       elle::Buffer buffer;
@@ -47,12 +57,23 @@ namespace infinit
 
       static_assert(std::is_same<T, Clear>::value == false,
                     "this call should never have occured");
+
+      /* XXX to put back once the legacy code below is no longer in use:
+  infinit::cryptography::Code
+  Frete::read(FileID f, FileOffset start, FileSize size)
+  {
+    ELLE_DEBUG("%s: read and encrypt block %s of size %s at offset %s with old key %s",
+    *this, f, size, start, this->_impl->old_key());
+    return this->_impl->old_key().encrypt(this->cleartext_read(f, start, size));
+  }
+
       static_assert(std::is_same<T, elle::Buffer>::value == false,
                     "this call should never have occured");
       static_assert(std::is_same<T, elle::WeakBuffer>::value == false,
                     "this call should never have occured");
       static_assert(std::is_same<T, elle::ConstWeakBuffer>::value == false,
                     "this call should never have occured");
+      */
 
       // Decrypt the code leading to a clear containing an archive.
       Clear clear(this->decrypt(code));
