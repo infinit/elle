72b462f7f7ca3d0026a7a01b2182c3fca8bc3dd3
mefyl <quentin.hocquet@infinit.io>
2015-04-09 15:19:50 +0200

    das.Object: Improve unit test.

--- das/tests/das.cc
+++ das/tests/das.cc
@@ -9,33 +9,59 @@ class Device
 public:
   das::Variable<std::string> name;
   elle::UUID id;
-  void
-  serialize(elle::serialization::Serializer& s)
-  {
-    s.serialize("name", this->name);
-  }
+  Device(std::string name_, elle::UUID id_)
+    : name(std::move(name_))
+    , id(std::move(id_))
+  {}
 };
 
 DAS_MODEL_FIELD(Device, name);
+DAS_MODEL_FIELD(Device, id);
+
+typedef das::Object<
+  Device,
+  das::Field<Device, elle::UUID, &Device::id>,
+  das::Field<Device, das::Variable<std::string>, &Device::name>
+  >
+DasDevice;
 
 static
 void
-simple()
+object_update()
 {
-  std::stringstream data("{}");
-  elle::serialization::json::SerializerIn input(data);
-  Device d;
-  typedef das::Object<
-    Device,
-    das::Field<Device, das::Variable<std::string>, &Device::name>
-    >
-    DasDevice;
-  DasDevice::Update u(input);
-  u.apply(d);
+  auto id = elle::UUID::random();
+  Device d("device-name", id);
+  {
+    DasDevice::Update u;
+    u.apply(d);
+    BOOST_CHECK_EQUAL(d.name.value(), "device-name");
+    BOOST_CHECK_EQUAL(d.id, id);
+  }
+  {
+    DasDevice::Update u;
+    u.name = "new-name";
+    u.apply(d);
+    BOOST_CHECK_EQUAL(d.name.value(), "new-name");
+    BOOST_CHECK_EQUAL(d.id, id);
+  }
+}
+
+static
+void
+object_update_serialization()
+{
+  {
+    std::stringstream data("{}");
+    elle::serialization::json::SerializerIn input(data);
+    DasDevice::Update u(input);
+    BOOST_CHECK(!u.id);
+    BOOST_CHECK(!u.name);
+  }
 }
 
 ELLE_TEST_SUITE()
 {
   auto& suite = boost::unit_test::framework::master_test_suite();
-  suite.add(BOOST_TEST_CASE(simple), 0, valgrind(1));
+  suite.add(BOOST_TEST_CASE(object_update), 0, valgrind(1));
+  suite.add(BOOST_TEST_CASE(object_update_serialization), 0, valgrind(1));
 }
