80339363962ba45aa60c932019d42815543e56d9
Antony MECHIN <antony.mechin@infinit.io>
2015-04-14 11:28:28 +0200

    tests.http_server: Automatically remove session-id from cookies.

--- reactor/tests/http_server.hh
+++ reactor/tests/http_server.hh
@@ -375,6 +375,9 @@ namespace reactor
               route->second.at(cmd.method())
                 (headers, cookies, cmd.params(), content),
               cookies);
+            auto it = this->headers().find("Set-Cookie");
+            if (it != this->headers().end())
+              this->headers().erase(it);
           }
           catch (Exception const& e)
           {
