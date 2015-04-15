775966ba28c953ead7057e119a4278b1b494f7d8
mefyl <quentin.hocquet@infinit.io>
2015-04-15 16:41:06 +0200

    elle.unordered_map: Add keys and values iterator.

--- elle/tests/unordered_map.cc
+++ elle/tests/unordered_map.cc
@@ -1,4 +1,5 @@
 #include <unordered_map>
+#include <unordered_set>
 
 #include <elle/test.hh>
 
@@ -12,8 +13,32 @@ contains()
   BOOST_CHECK(contains(map, 2));
 }
 
+static
+void
+keys_iterator()
+{
+  std::unordered_map<int, int> map({{0, 10}, {1, 11}, {2, 12}});
+  std::unordered_set<int> keys({0, 1, 2});
+  for (auto it = iter_keys(map); it != map.end(); ++it)
+    BOOST_CHECK(keys.erase(*it));
+  BOOST_CHECK(keys.empty());
+}
+
+static
+void
+values_iterator()
+{
+  std::unordered_map<int, int> map({{0, 10}, {1, 11}, {2, 12}});
+  std::unordered_set<int> values({10, 11, 12});
+  for (auto it = iter_values(map); it != map.end(); ++it)
+    BOOST_CHECK(values.erase(*it));
+  BOOST_CHECK(values.empty());
+}
+
 ELLE_TEST_SUITE()
 {
   auto& master = boost::unit_test::framework::master_test_suite();
   master.add(BOOST_TEST_CASE(contains), 0, 1);
+  master.add(BOOST_TEST_CASE(keys_iterator), 0, 1);
+  master.add(BOOST_TEST_CASE(values_iterator), 0, 1);
 }
