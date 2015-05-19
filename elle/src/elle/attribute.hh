5b537ae798586077693b0132235bc188f5573297
mefyl <quentin.hocquet@infinit.io>
2015-05-19 11:17:33 +0200

    elle.attribute: Allow parenthesizing type argument.

--- elle/src/elle/attribute.hh
+++ elle/src/elle/attribute.hh
@@ -22,16 +22,31 @@ namespace elle
   }
 }
 
+# define ELLE_ATTRIBUTE_APPLY(macro, args) ELLE_ATTRIBUTE_APPLY_I(macro, args)
+# define ELLE_ATTRIBUTE_APPLY_I(macro, args) macro args
+# define ELLE_ATTRIBUTE_STRIP_PARENS(x) ELLE_ATTRIBUTE_EVAL((ELLE_ATTRIBUTE_STRIP_PARENS_I x), x)
+# define ELLE_ATTRIBUTE_STRIP_PARENS_I(...) 1,1
+# define ELLE_ATTRIBUTE_EVAL(test, x) ELLE_ATTRIBUTE_EVAL_I(test, x)
+# define ELLE_ATTRIBUTE_EVAL_I(test, x) ELLE_ATTRIBUTE_MAYBE_STRIP_PARENS(ELLE_ATTRIBUTE_TEST_ARITY test, x)
+# define ELLE_ATTRIBUTE_TEST_ARITY(...) ELLE_ATTRIBUTE_APPLY(ELLE_ATTRIBUTE_TEST_ARITY_I, (__VA_ARGS__, 2, 1))
+# define ELLE_ATTRIBUTE_TEST_ARITY_I(a,b,c,...) c
+# define ELLE_ATTRIBUTE_MAYBE_STRIP_PARENS(cond, x) ELLE_ATTRIBUTE_MAYBE_STRIP_PARENS_I(cond, x)
+# define ELLE_ATTRIBUTE_MAYBE_STRIP_PARENS_I(cond, x) BOOST_PP_CAT(ELLE_ATTRIBUTE_MAYBE_STRIP_PARENS_, cond)(x)
+# define ELLE_ATTRIBUTE_MAYBE_STRIP_PARENS_1(x) x
+# define ELLE_ATTRIBUTE_MAYBE_STRIP_PARENS_2(x) ELLE_ATTRIBUTE_APPLY(ELLE_ATTRIBUTE_MAYBE_STRIP_PARENS_2_I, x)
+# define ELLE_ATTRIBUTE_MAYBE_STRIP_PARENS_2_I(...) __VA_ARGS__
+
 /// Define a private attribute.
 # define ELLE_ATTRIBUTE(_type_, _name_)                                 \
   private:                                                              \
-  _type_ BOOST_PP_CAT(_, _name_);
+  ELLE_ATTRIBUTE_STRIP_PARENS(_type_) BOOST_PP_CAT(_, _name_);
 
 /// Define an accessor returning a const reference on the attribute.
 # define ELLE_ATTRIBUTE_r_ACCESSOR(_type_, _name_)                      \
   public:                                                               \
-  typename ::elle::_detail::attribute_r_type<_type_>::type              \
-  _name_() const
+  typename ::elle::_detail::attribute_r_type                            \
+  <ELLE_ATTRIBUTE_STRIP_PARENS(_type_)>::type                           \
+  _name_() const                                                        \
 
 /// Define and implement an accessor returning a const reference
 /// on the attribute.
@@ -44,7 +59,7 @@ namespace elle
 /// Define and implement a thread safe accessor returning the attribute.
 # define ELLE_ATTRIBUTE_R_TS_ACCESSOR(_type_, _name_)                   \
   public:                                                               \
-  _type_                                                                \
+  ELLE_ATTRIBUTE_STRIP_PARENS(_type_)                                   \
   _name_() const                                                        \
   {                                                                     \
     std::unique_lock<elle::threading::read_mutex> lock                  \
@@ -56,7 +71,8 @@ namespace elle
 # define ELLE_ATTRIBUTE_w_ACCESSOR(_type_, _name_)                      \
   public:                                                               \
   void                                                                  \
-  _name_(typename boost::call_traits<_type_>::param_type name)          \
+  _name_(typename boost::call_traits                                    \
+         <ELLE_ATTRIBUTE_STRIP_PARENS(_type_)>::param_type name)        \
 
 /// Define and implement an accessor enabling one to set the attribute.
 # define ELLE_ATTRIBUTE_W_ACCESSOR(_type_, _name_)                      \
@@ -79,7 +95,8 @@ namespace elle
 /// enabling one to potentially modify the attribute outside the class.
 # define ELLE_ATTRIBUTE_x_ACCESSOR(_type_, _name_)                      \
   public:                                                               \
-  typename boost::call_traits<_type_>::reference                        \
+  typename boost::call_traits                                           \
+  <ELLE_ATTRIBUTE_STRIP_PARENS(_type_)>::reference                      \
   _name_()                                                              \
 
 /// Define and implement an accessor returning a reference on the attribute i.e
