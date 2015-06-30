fa844dfe98dd759866d056c359a57f8423fb95b4
Matthieu Nottale <matthieu.nottale@infinit.io>
2015-06-30 11:17:44 +0200

    Bench: New simple benchmark class.

---
+++ elle/src/elle/bench.cc
@@ -0,0 +1,62 @@
+#include <elle/bench.hh>
+#include <elle/log.hh>
+
+namespace elle
+{
+  Bench::Bench(std::string const& name,
+               boost::posix_time::time_duration log_interval,
+               int roundto)
+    : _name(name)
+    , _sum(0)
+    , _count(0)
+    , _min(0)
+    , _max(0)
+    , _log_interval(log_interval)
+    , _roundfactor(std::pow(10, roundto))
+  {
+    _start = boost::posix_time::microsec_clock::universal_time();
+  }
+
+  void Bench::add(double val)
+  {
+    if (!_count) _min = _max = val;
+    ++_count;
+    _sum += val;
+    _min = std::min (val, _min);
+    _max = std::max (val, _max);
+    if (_log_interval != boost::posix_time::time_duration()
+      && boost::posix_time::microsec_clock::universal_time() - _start
+    > _log_interval)
+    {
+      log();
+      reset();
+    }
+  }
+  void Bench::reset()
+  {
+    _sum=_count=_min=_max=0;
+    _start = boost::posix_time::microsec_clock::universal_time();
+  }
+  void Bench::log()
+  {
+    ELLE_LOG_COMPONENT(_name.c_str());
+    ELLE_LOG("%s: AVG %s, MIN %s, MAX %s, COUNT %s", _name,
+      std::round(double(_sum*_roundfactor/_count)) / _roundfactor,
+      _min, _max, _count);
+  }
+  void Bench::show(std::ostream& os)
+  {
+    os << _name << " AVG " << (_sum/_count) << " MIN " << _min << " MAX " << _max << " COUNT " << _count;
+  }
+
+  Bench::BenchScope::BenchScope(Bench& owner)
+  :_owner(owner)
+  {
+    _start = boost::posix_time::microsec_clock::universal_time();
+  }
+  Bench::BenchScope::~BenchScope()
+  {
+    auto d = boost::posix_time::microsec_clock::universal_time() - _start;
+    _owner.add(d.total_microseconds());
+  }
+}
---
+++ elle/src/elle/bench.hh
@@ -0,0 +1,50 @@
+#ifndef ELLE_BENCH_HH
+# define ELLE_BENCH_HH
+
+#include <elle/attribute.hh>
+#include <elle/time.hh>
+namespace elle
+{
+
+  /** Bench a block of code or display statistics about some data.
+  */
+  class Bench
+  {
+  public:
+    /** Contructor.
+    * @param log_interval if set, bench will automatically log and reset at
+    *        given interval
+    * @param round round values to that many digits below 1.
+    */
+    Bench(std::string const& name,
+          boost::posix_time::time_duration log_interval = {},
+          int roundto = 2);
+    void
+    add(double val);
+    void
+    reset();
+    void
+    log(); ///< log with ELLE_LOG
+    void
+    show(std::ostream& os);
+    struct BenchScope
+    {
+      BenchScope(Bench& owner);
+      ~BenchScope();
+    private:
+      boost::posix_time::ptime _start;
+      Bench& _owner;
+    };
+
+    ELLE_ATTRIBUTE_R(std::string, name);
+    ELLE_ATTRIBUTE_R(double, sum);
+    ELLE_ATTRIBUTE_R(long, count);
+    ELLE_ATTRIBUTE_R(double, min);
+    ELLE_ATTRIBUTE_R(double, max);
+    ELLE_ATTRIBUTE_R(boost::posix_time::ptime, start);
+    ELLE_ATTRIBUTE_R(boost::posix_time::time_duration, log_interval);
+    ELLE_ATTRIBUTE(double, roundfactor);
+  };
+}
+
+#endif
