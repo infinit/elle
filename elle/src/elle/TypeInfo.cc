e01701fbbe306a8e3fdc89d47ae3e68d305ea6db
Matthieu Nottale <matthieu.nottale@infinit.io>
2015-07-16 18:03:14 +0200

    TypeInfo: workaround for clang.

--- elle/src/elle/TypeInfo.cc
+++ elle/src/elle/TypeInfo.cc
@@ -2,6 +2,7 @@
 
 #include <elle/Backtrace.hh>
 
+
 namespace elle
 {
   std::string
@@ -13,13 +14,23 @@ namespace elle
   bool
   TypeInfo::operator ==(TypeInfo const& rhs) const
   {
+#ifdef __clang__
+    return strcmp(this->_info->name(), rhs._info->name()) == 0;
+#else
     return *this->_info == *rhs._info;
+#endif
   }
 
   bool
   TypeInfo::operator <(TypeInfo const& rhs) const
   {
+   // When using before on two different typeinfos for the same type,
+   // clang fails to treat them as equals.
+#ifdef __clang__
+    return strcmp(this->_info->name(), rhs._info->name()) < 0;
+#else
     return this->_info->before(*rhs._info);
+#endif
   }
 
   TypeInfo::TypeInfo(std::type_info const* info)
