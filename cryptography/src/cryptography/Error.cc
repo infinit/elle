5f780f70acfab0805f983e92502b687cf826e46b
Julien Quintard <julien.quintard@infinit.io>
2015-07-31 13:48:57 +0200

    Fix Error indentation.

--- cryptography/src/cryptography/Error.cc
+++ cryptography/src/cryptography/Error.cc
@@ -14,8 +14,8 @@ namespace infinit
       elle::Error(message)
     {}
 
-    Error::Error(elle::serialization::SerializerIn& input)
-    : elle::Error(input)
+    Error::Error(elle::serialization::SerializerIn& input):
+      elle::Error(input)
     {}
 
     static const elle::serialization::Hierarchy<elle::Exception>::
