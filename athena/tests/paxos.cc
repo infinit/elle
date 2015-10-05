5edef1c2c8a5d925bb2bb6495e404fe26c5cdabc
mefyl <mefyl@infinit.io>
2015-10-05 16:59:54 +0200

    athena.paxos.Paxos: Handle concurrency.

--- athena/tests/paxos.cc
+++ athena/tests/paxos.cc
@@ -1,6 +1,8 @@
 #include <elle/test.hh>
 
+#include <reactor/Barrier.hh>
 #include <reactor/scheduler.hh>
+#include <reactor/signal.hh>
 
 #include <athena/paxos/Paxos.hh>
 
@@ -12,6 +14,7 @@ template <typename T, typename ServerId>
 class Peer
   : public paxos::Paxos<T, ServerId>::Peer
 {
+public:
   virtual
   boost::optional<typename paxos::Paxos<T, ServerId>::Accepted>
   propose(ServerId const& sender, int round) override
@@ -27,25 +30,6 @@ class Peer
   }
 
   ELLE_ATTRIBUTE_RW((paxos::Paxos<T, ServerId>*), paxos);
-};
-
-template <typename T, typename ServerId>
-class UnavailablePeer
-  : public paxos::Paxos<T, ServerId>::Peer
-{
-  virtual
-  boost::optional<typename paxos::Paxos<T, ServerId>::Accepted>
-  propose(ServerId const& sender, int round) override
-  {
-    throw typename paxos::Paxos<T, ServerId>::Peer::Unavailable();
-  }
-
-  virtual
-  void
-  accept(ServerId const& sender, int round, T const& value) override
-  {
-    throw typename paxos::Paxos<T, ServerId>::Peer::Unavailable();
-  }
 };
 
 ELLE_TEST_SCHEDULED(all_is_well)
@@ -79,6 +63,25 @@ ELLE_TEST_SCHEDULED(all_is_well)
   BOOST_CHECK_EQUAL(paxos_2.choose(), 42);
   BOOST_CHECK_EQUAL(paxos_3.choose(), 42);
 }
+
+template <typename T, typename ServerId>
+class UnavailablePeer
+  : public paxos::Paxos<T, ServerId>::Peer
+{
+  virtual
+  boost::optional<typename paxos::Paxos<T, ServerId>::Accepted>
+  propose(ServerId const& sender, int round) override
+  {
+    throw typename paxos::Paxos<T, ServerId>::Peer::Unavailable();
+  }
+
+  virtual
+  void
+  accept(ServerId const& sender, int round, T const& value) override
+  {
+    throw typename paxos::Paxos<T, ServerId>::Peer::Unavailable();
+  }
+};
 
 ELLE_TEST_SCHEDULED(two_of_three)
 {
@@ -145,6 +148,75 @@ ELLE_TEST_SCHEDULED(already_chosen)
   BOOST_CHECK_EQUAL(paxos_3.choose(44), 42);
 }
 
+template <typename T, typename ServerId>
+class InstrumentedPeer
+  : public Peer<T, ServerId>
+{
+public:
+  virtual
+  boost::optional<typename paxos::Paxos<T, ServerId>::Accepted>
+  propose(ServerId const& sender, int round) override
+  {
+    this->propose_signal.signal();
+    reactor::wait(this->propose_barrier);
+    return Peer<T, ServerId>::propose(sender, round);
+  }
+
+  virtual
+  void
+  accept(ServerId const& sender, int round, T const& value) override
+  {
+    this->accept_signal.signal();
+    reactor::wait(this->accept_barrier);
+    return Peer<T, ServerId>::accept(sender, round, value);
+  }
+
+  reactor::Barrier propose_barrier, accept_barrier;
+  reactor::Signal propose_signal, accept_signal;
+};
+
+ELLE_TEST_SCHEDULED(concurrent)
+{
+  auto peer_1_2 = new InstrumentedPeer<int, int>();
+  auto peer_1_3 = new InstrumentedPeer<int, int>();
+  auto peer_2_1 = new Peer<int, int>();
+  auto peer_2_3 = new Peer<int, int>();
+  auto peer_3_1 = new Peer<int, int>();
+  auto peer_3_2 = new Peer<int, int>();
+  typedef paxos::Paxos<int, int>::Peers Peers;
+  Peers peers_1;
+  peers_1.push_back(std::unique_ptr<paxos::Paxos<int, int>::Peer>(peer_1_2));
+  peers_1.push_back(std::unique_ptr<paxos::Paxos<int, int>::Peer>(peer_1_3));
+  paxos::Paxos<int, int> paxos_1(1, std::move(peers_1));
+  Peers peers_2;
+  peers_2.push_back(std::unique_ptr<paxos::Paxos<int, int>::Peer>(peer_2_1));
+  peers_2.push_back(std::unique_ptr<paxos::Paxos<int, int>::Peer>(peer_2_3));
+  paxos::Paxos<int, int> paxos_2(2, std::move(peers_2));
+  Peers peers_3;
+  peers_3.push_back(std::unique_ptr<paxos::Paxos<int, int>::Peer>(peer_3_1));
+  peers_3.push_back(std::unique_ptr<paxos::Paxos<int, int>::Peer>(peer_3_2));
+  paxos::Paxos<int, int> paxos_3(3, std::move(peers_3));
+  peer_1_2->paxos(&paxos_2);
+  peer_1_3->paxos(&paxos_3);
+  peer_2_1->paxos(&paxos_1);
+  peer_2_3->paxos(&paxos_3);
+  peer_3_1->paxos(&paxos_1);
+  peer_3_2->paxos(&paxos_2);
+  peer_1_2->propose_barrier.open();
+  peer_1_2->accept_barrier.open();
+  peer_1_3->propose_barrier.open();
+  reactor::Thread::unique_ptr t1(
+    new reactor::Thread(
+      "1",
+      [&] { BOOST_CHECK_EQUAL(paxos_1.choose(42), 42); }));
+  reactor::wait(
+    reactor::Waitables({&peer_1_2->accept_signal, &peer_1_3->accept_signal}));
+  BOOST_CHECK_EQUAL(paxos_3.choose(43), 42);
+  BOOST_CHECK_EQUAL(paxos_2.choose(), 42);
+  peer_1_3->accept_barrier.open();
+  reactor::wait(*t1);
+}
+
 ELLE_TEST_SUITE()
 {
   auto& suite = boost::unit_test::framework::master_test_suite();
@@ -152,4 +224,5 @@ ELLE_TEST_SUITE()
   suite.add(BOOST_TEST_CASE(two_of_three), 0, valgrind(1));
   suite.add(BOOST_TEST_CASE(one_of_three), 0, valgrind(1));
   suite.add(BOOST_TEST_CASE(already_chosen), 0, valgrind(1));
+  suite.add(BOOST_TEST_CASE(concurrent), 0, valgrind(1));
 }
