743e732dd48682908a26dfc43195f14838ad74cc
Matthieu Nottale <matthieu.nottale@infinit.io>
2015-10-14 10:19:05 +0200

    dh hash(): Dont go through serialization to hash a pointer.

--- cryptography/src/cryptography/dh/PrivateKey.hxx
+++ cryptography/src/cryptography/dh/PrivateKey.hxx
@@ -20,16 +20,8 @@ namespace std
     operator ()(infinit::cryptography::dh::PrivateKey const& value) const
     {
       std::stringstream stream;
-      {
-        elle::serialization::binary::SerializerOut output(stream, false); // XXX
-
-        // Note that this is not a great way to represent a key but OpenSSL
-        // does not provide DH-specific DER functions while Diffie Hellman keys
-        // are not exactly supposed to be serialized since used for one-time
-        // key exchanges.
-        ELLE_ASSERT(value.key()->pkey.dh->priv_key != nullptr);
-        output.serialize("value", value.key()->pkey.dh->priv_key);
-      }
+      ELLE_ASSERT(value.key()->pkey.dh->priv_key != nullptr);
+      stream << value.key()->pkey.dh->priv_key;
 
       size_t result = std::hash<std::string>()(stream.str());
 
--- cryptography/src/cryptography/dh/PublicKey.hxx
+++ cryptography/src/cryptography/dh/PublicKey.hxx
@@ -20,16 +20,8 @@ namespace std
     operator ()(infinit::cryptography::dh::PublicKey const& value) const
     {
       std::stringstream stream;
-      {
-        elle::serialization::binary::SerializerOut output(stream, false); // XXX
-
-        // Note that this is not a great way to represent a key but OpenSSL
-        // does not provide DH-specific DER functions while Diffie Hellman keys
-        // are not exactly supposed to be serialized since used for one-time
-        // key exchanges.
-        ELLE_ASSERT(value.key()->pkey.dh->pub_key != nullptr);
-        output.serialize("value", value.key()->pkey.dh->pub_key);
-      }
+      ELLE_ASSERT(value.key()->pkey.dh->pub_key != nullptr);
+      stream << value.key()->pkey.dh->pub_key;
 
       size_t result = std::hash<std::string>()(stream.str());
 
