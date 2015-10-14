66a026137602a9a7e19d29c86d869a44b596116e
Matthieu Nottale <matthieu.nottale@infinit.io>
2015-10-14 10:38:00 +0200

    Revert "dh hash(): Dont go through serialization to hash a pointer."
    This reverts commit 7a0afae447e3baae8fb3f0641bdcc3b5b48a9b97.

--- cryptography/src/cryptography/dh/PublicKey.hxx
+++ cryptography/src/cryptography/dh/PublicKey.hxx
@@ -20,8 +20,16 @@ namespace std
     operator ()(infinit::cryptography::dh::PublicKey const& value) const
     {
       std::stringstream stream;
-      ELLE_ASSERT(value.key()->pkey.dh->pub_key != nullptr);
-      stream << value.key()->pkey.dh->pub_key;
+      {
+        elle::serialization::binary::SerializerOut output(stream, false); // XXX
+
+        // Note that this is not a great way to represent a key but OpenSSL
+        // does not provide DH-specific DER functions while Diffie Hellman keys
+        // are not exactly supposed to be serialized since used for one-time
+        // key exchanges.
+        ELLE_ASSERT(value.key()->pkey.dh->pub_key != nullptr);
+        output.serialize("value", value.key()->pkey.dh->pub_key);
+      }
 
       size_t result = std::hash<std::string>()(stream.str());
 
