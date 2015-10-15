ed04680e16cb4b537db0e79d79463d98a99be177
Christopher Crone <christopher.crone@infinit.io>
2015-10-15 16:17:02 +0200

    reactor.tests.filesystem: Work around OS X unmount issue.

--- reactor/tests/filesystem.cc
+++ reactor/tests/filesystem.cc
@@ -142,6 +142,11 @@ static void run_filesystem(reactor::filesystem::FileSystem &fs,
     barrier.wait();
     ELLE_TRACE("...unmounting...");
     fs.unmount();
+#ifdef INFINIT_MACOSX
+    // XXX: Waiting on fs object does not work for unmount on OS X.
+    // https://app.asana.com/0/5058234687067/58308026674516
+    reactor::sleep(300_ms);
+#endif
     ELLE_TRACE("...unmounted");
   });
   sched.run();
