5e24d913a4fea48e5a44ac78fa69aad23b61d10a
Julien Quintard <julien.quintard@infinit.io>
2015-10-17 13:38:18 +0200

    Remove temporary/invalid test.

--- cryptography/tests/cryptography/rsa/KeyPair.cc
+++ cryptography/tests/cryptography/rsa/KeyPair.cc
@@ -196,19 +196,6 @@ test_serialize()
                       infinit::cryptography::Error);
   }
 
-  // Deserialize a partially good key pair missing the private part.
-  {
-    elle::String representation(R"JSON({"rsa":"MIIBCgKCAQEAp0uoRP+SKv6ryQo8TLapW8SFVZfmc0clf2HxBSx+uKAvGASbW2/pGJsE7m7R2aiHlCUkvW19TbNtkRoZ8IlnOu1E52wJEDiZvEjUYm+3T6Re8eyFj2ZakCGjOzQWP7G8DGdVBHdKdtcljvmXaWUbJDDI5MMzk9v+C3q2NmQiRfRD/V0T3hRgCejN7fbYJ3cgySRxIEhaUr0SqHGJioD8jiRoYXLJ9w4xsSLkofFltmkbaqynimzvQ6rKnz4lKx2wl8X59YUDXT7UWb6yIPBMMhOnDxdOgojgQxbbkzp6ilGh+wS14KR6sp948jURYD7qDtdiluI78mlajzhEADuyTQIDAQAB"})JSON");
-    std::stringstream stream(representation);
-    typename elle::serialization::json::SerializerIn input(stream);
-
-    /* XXX
-    BOOST_CHECK_THROW(infinit::cryptography::rsa::KeyPair keypair(input),
-                      infinit::cryptography::Error);
-    */
-    infinit::cryptography::rsa::KeyPair keypair(input);
-  }
-
   // For every hard-coded strings [representation 1] in every format,
   // deserialize the key pairs, re-serialize them, make sure they can
   // be used and finally upgrade them: useful for detecting changes in formats.
