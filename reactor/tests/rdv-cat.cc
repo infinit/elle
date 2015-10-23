ccfb54a877bec294e010637325e0aa1f4241220a
Matthieu Nottale <matthieu.nottale@infinit.io>
2015-10-23 10:53:55 +0200

    prototype nat traversal.

---
+++ reactor/tests/rdv-cat.cc
@@ -0,0 +1,94 @@
+#include <reactor/network/rdv.hh>
+
+#include <elle/serialization/json.hh>
+
+#include <reactor/network/buffer.hh>
+#include <reactor/network/udp-socket.hh>
+#include <reactor/scheduler.hh>
+
+reactor::network::UDPSocket* sock = nullptr;
+Endpoint remote;
+
+static void send(std::string const& buf)
+{
+  while (!sock)
+    usleep(100000);
+  sock->send_to(reactor::network::Buffer(buf), remote);
+}
+
+static void run(int argc, char** argv)
+{
+  std::string rdvhost = argv[1];
+  int rdvport = 7890;
+  std::string id(argv[2]);
+  std::string remoteid(argv[3]);
+  Endpoint server_ep(boost::asio::ip::address::from_string(rdvhost), rdvport);
+  reactor::network::UDPSocket srv;
+  srv.close();
+  srv.bind(
+    boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 0));
+  Request req;
+  req.id = id;
+  req.command = Command::connect;
+  req.target = remoteid;
+  elle::Buffer buf = elle::serialization::json::serialize(req);
+  srv.send_to(reactor::network::Buffer(buf.contents(), buf.size()), server_ep);
+
+  while (true)
+  {
+    elle::Buffer buf;
+    buf.size(5000);
+    Endpoint ep;
+    int sz = srv.receive_from(reactor::network::Buffer(buf.mutable_contents(),
+                                                       buf.size()), ep);
+    buf.size(sz);
+    if (ep.port() == 7890)
+    { // rdv packet
+      Reply reply;
+      reply = elle::serialization::json::deserialize<Reply>(buf);
+      switch (reply.command)
+      {
+      case Command::ping:
+      case Command::pong:
+        break;
+      case Command::connect:
+      case Command::connect_requested:
+        if (reply.target_endpoint)
+        {
+          remote = *reply.target_endpoint;
+          sock = &srv;
+          srv.send_to(reactor::network::Buffer("RDV PING"), remote);
+        }
+        break;
+      }
+    }
+    else if (buf == "RDV PING")
+    {
+    }
+    else
+    { // chat packet
+      std::cout.write((const char*)buf.contents(), buf.size());
+      std::cout << std::endl;
+    }
+  }
+}
+
+int main(int argc, char** argv)
+{
+  reactor::Scheduler sched;
+  reactor::Thread t(sched, "main", [&]
+    {
+      run(argc, argv);
+    });
+  std::thread rs([&] {
+      while (std::cin.good())
+      {
+        std::string buf;
+        std::getline(std::cin, buf);
+        sched.mt_run("data",
+          boost::function<void()>([&] { send(buf);}));
+      }
+  });
+  sched.run();
+  rs.join();
+}
---
+++ reactor/tests/rdv-server.cc
@@ -0,0 +1,93 @@
+#include <reactor/network/rdv.hh>
+#include <boost/optional.hpp>
+
+#include <elle/serialization/json.hh>
+
+#include <reactor/network/buffer.hh>
+#include <reactor/network/udp-socket.hh>
+#include <reactor/scheduler.hh>
+
+
+ELLE_LOG_COMPONENT("rdv-server");
+    
+std::unordered_map<std::string, Endpoint> peers;
+
+static void run(int argc, char** argv)
+{
+  int port = 7890;
+  if (argc >1)
+    port = std::stoi(argv[1]);
+  reactor::network::UDPSocket srv;
+  srv.close();
+  srv.bind(
+    boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port));
+  elle::Buffer buf;
+  while (true)
+  {
+    buf.size(5000);
+    Endpoint source;
+    int sz = srv.receive_from(reactor::network::Buffer(buf.mutable_contents(), buf.size()),
+                                   source);
+    buf.size(sz);
+    elle::IOStream ios(buf.istreambuf());
+    elle::serialization::json::SerializerIn sin(ios);
+    Request req;
+    sin.serialize_forward(req);
+    peers[req.id] = source;
+    Reply reply;
+    reply.id = req.id;
+    reply.source_endpoint = source;
+    switch(req.command)
+    {
+    case Command::ping:
+      reply.command = Command::pong;
+      break;
+    case Command::pong:
+      break;
+    case Command::connect:
+      {
+        reply.command = Command::connect;
+        reply.target_address = req.target;
+        auto peer = peers.find(*req.target);
+        if (peer != peers.end())
+        {
+          reply.target_endpoint = peer->second;
+          Reply other;
+          other.command = Command::connect_requested;
+          other.id = *req.target;
+          other.source_endpoint = peer->second;
+          other.target_address = req.id;
+          other.target_endpoint = source;
+          elle::Buffer b;
+          {
+            elle::IOStream ios(b.ostreambuf());
+            elle::serialization::json::SerializerOut sout(ios);
+            sout.serialize_forward(other);
+          }
+          srv.send_to(reactor::network::Buffer(b.contents(), b.size()), peer->second);
+        }
+      }
+      break;
+    case Command::connect_requested:
+      ELLE_LOG("unexpected connect_requested");
+      break;
+    }
+    elle::Buffer b;
+    {
+      elle::IOStream ios(b.ostreambuf());
+      elle::serialization::json::SerializerOut sout(ios);
+      sout.serialize_forward(reply);
+    }
+    srv.send_to(reactor::network::Buffer(b.contents(), b.size()), source);
+  }
+}
+
+int main(int argc, char** argv)
+{
+  reactor::Scheduler sched;
+  reactor::Thread t(sched, "main", [&]
+    {
+      run(argc, argv);
+    });
+  sched.run();
+}
