f4a7e36a466b8138c1afb30134d968381e99f892
Matthieu Nottale <matthieu.nottale@infinit.io>
2015-12-23 15:40:32 +0100

    Add a LDAP client: wrapper over openldap.

---
+++ elle/src/elle/ldap.cc
@@ -0,0 +1,131 @@
+#include <elle/ldap.hh>
+#define ldap ldap_ldap
+#include <ldap.h>
+#undef ldap
+
+#include <boost/algorithm/string.hpp>
+
+#include <elle/log.hh>
+#include <elle/Error.hh>
+
+ELLE_LOG_COMPONENT("elle.ldap");
+
+namespace elle
+{
+  namespace ldap
+  {
+    DC::DC(std::string const& v)
+    {
+      std::vector<std::string> comps;
+      boost::algorithm::split(comps, v, boost::is_any_of("."));
+      for (auto& s: comps)
+        s = "dc=" + s;
+      value = boost::algorithm::join(comps, ",");
+    }
+
+    Attr operator / (Attr const& a, Attr const& b)
+    {
+      auto res = Attr(a.value + "," + b.value);
+      if (res.value[0] == ',')
+        res.value = res.value.substr(1);
+      if (res.value[res.value.size()-1] == ',')
+        res.value = res.value.substr(0, res.value.size()-1);
+      return res;
+    }
+
+    LDAPClient::LDAPClient(std::string const& url_,
+                         Attr const& domain,
+                         std::string const& user,
+                         std::string const& password)
+    : _domain(domain)
+    {
+      std::string url(url_);
+      auto p = url.find("://");
+      if (p == url.npos)
+      {
+        url = "ldap://" + url;
+        p = 4;
+      }
+      auto p2 = url.find_last_of(':');
+      if (p2 == p || p2 == url.npos)
+        url = url + ":389";
+      ELLE_TRACE("Using ldap url %s", url);
+      int rc;
+      rc = ldap_initialize(&_ld, url.c_str());
+      if(rc != LDAP_SUCCESS)
+      throw elle::Error(
+        elle::sprintf("ldap_initialize: %s", ldap_err2string(rc)));
+      int desired_version = 3;
+      rc = ldap_set_option(_ld, LDAP_OPT_PROTOCOL_VERSION, &desired_version);
+      if (rc != LDAP_OPT_SUCCESS)
+      throw elle::Error(
+        elle::sprintf("ldap_set_option: %s", ldap_err2string(rc)));
+      Attr authuser = CN(user) / domain;
+      ELLE_TRACE("binding with %s", authuser.value);
+      /*
+      std::string qbind = "cn=" + user + ",dc=" + domain;
+      rc = ldap_simple_bind_s(ld, qbind.c_str(), password.c_str());
+      */
+      struct berval cred;
+      cred.bv_val = (char*)password.c_str();
+      cred.bv_len = password.size();
+      rc = ldap_sasl_bind_s(_ld, authuser.value.c_str(),
+        0, &cred, NULL, NULL, NULL);
+      if (rc != LDAP_SUCCESS)
+      throw elle::Error(
+        elle::sprintf("ldap_sasl_bind: %s", ldap_err2string(rc)));
+    }
+
+    LDAPClient::~LDAPClient()
+    {
+      ldap_unbind_ext_s(_ld, 0, 0);
+    }
+
+    Result
+    LDAPClient::search(Attr base, Attr query, std::vector<std::string> const& attrs)
+    {
+      std::vector<char*> cattrs;
+      cattrs.resize(attrs.size()+1);
+      for (int i=0; i<signed(attrs.size()); ++i)
+        cattrs[i] = (char*)attrs[i].c_str();
+      cattrs[attrs.size()] = nullptr;
+      base = base / _domain;
+      LDAPMessage* msg;
+      int rc = ldap_search_ext_s(_ld, base.value.c_str(),
+        LDAP_SCOPE_SUBTREE,
+        query.value.c_str(),
+        &cattrs.front(), 0,
+        NULL, NULL, NULL,
+        0, &msg
+        );
+      if (rc != LDAP_SUCCESS)
+      throw elle::Error(
+        elle::sprintf("ldap_search: %s", ldap_err2string(rc)));
+      Result res;
+      LDAPMessage* entry;
+      for (entry = ldap_first_entry(_ld, msg); entry != NULL;
+        entry = ldap_next_entry(_ld, entry))
+      {
+        std::unordered_map<std::string, std::vector<std::string>> p;
+        BerElement* ber;
+        const char* attr;
+        for( attr = ldap_first_attribute(_ld, entry, &ber); attr != NULL;
+          attr = ldap_next_attribute(_ld, entry, ber))
+        {
+          std::vector<std::string> vals;
+          berval ** bvals = ldap_get_values_len(_ld, entry, attr);
+          if (bvals)
+          {
+            for (int i=0; bvals[i] != NULL; ++i)
+            {
+              vals.emplace_back(bvals[i]->bv_val, bvals[i]->bv_val + bvals[i]->bv_len);
+            }
+          }
+          p[attr] = std::move(vals);
+        }
+        res.emplace_back(std::move(p));
+      }
+      return res;
+    }
+  }
+}
---
+++ elle/src/elle/ldap.hh
@@ -0,0 +1,63 @@
+#ifndef ELLE_LDAP_HH
+# define ELLE_LDAP_HH
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+struct ldap_ldap;
+typedef struct ldap_ldap LDAP;
+
+namespace elle
+{
+  namespace ldap
+  {
+    class Attr
+    {
+    public:
+      Attr() {}
+      Attr(std::string const& k, std::string const& v)
+      : value(k + "=" + v) {}
+      Attr(std::string const& v) : value(v) {}
+      std::string value;
+    };
+    Attr operator / (Attr const& a, Attr const& b);
+
+    // results = [result];  result = {attrname->attrvalues};  attrvalues=[string]
+    typedef std::vector<std::unordered_map<std::string, std::vector<std::string>>>
+    Result;
+
+    class LDAPClient
+    {
+    public:
+      LDAPClient(std::string const& url, Attr const& domain,
+                 std::string const& user, std::string const& password);
+      ~LDAPClient();
+      Result search(Attr base, Attr query, std::vector<std::string> const& attrs = {});
+    private:
+      LDAP* _ld;
+      Attr _domain;
+    };
+
+    class CN: public Attr
+    {
+    public:
+      using Attr::Attr;
+      CN(std::string const& v) : Attr("cn", v) {}
+    };
+    class OU: public Attr
+    {
+    public:
+      using Attr::Attr;
+      OU(std::string const& v) : Attr("ou", v) {}
+    };
+    class DC: public Attr
+    {
+    public:
+      using Attr::Attr;
+      DC(std::string const& v);
+    };
+  }
+}
+
+#endif
