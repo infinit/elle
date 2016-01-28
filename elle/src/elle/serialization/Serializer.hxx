4e8519ad8ee97955b30230d40ded8960cb616c92
mefyl <mefyl@gruntech.org>
2016-01-28 17:00:26 +0100

    elle.serialization.Serializer: Fix uninitialized memory access.

--- elle/src/elle/serialization/Serializer.hxx
+++ elle/src/elle/serialization/Serializer.hxx
@@ -419,7 +419,7 @@ namespace elle
           self._serialize_anonymous(name, opt);
           filled = true;
         });
-      if (!filled && opt)
+      if (!filled)
       {
         ELLE_DEBUG("reset option");
         _details::option_reset(opt);
