7050cb0b820d5503180c54aba73d03df04ec99ab
Christopher Crone <christopher.crone@infinit.io>
2016-03-07 08:54:16 +0100

    elle.system.user_paths: Fix warning on OS X 10.7.

--- elle/src/elle/system/user_paths.mm
+++ elle/src/elle/system/user_paths.mm
@@ -9,38 +9,44 @@ namespace elle
     boost::filesystem::path
     home_directory()
     {
+      @autoreleasepool
+      {
 #if defined(INFINIT_MACOSX)
-      return NSHomeDirectory().UTF8String;
+        return NSHomeDirectory().UTF8String;
 #elif defined(INFINIT_IOS)
-      NSString* application_support_dir =
-        NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory,
-                                            NSUserDomainMask,
-                                            YES).firstObject;
-      return application_support_dir.UTF8String;
+        NSString* application_support_dir =
+          NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory,
+                                              NSUserDomainMask,
+                                              YES).firstObject;
+        return application_support_dir.UTF8String;
 #else
 # error Unknown plaform.
 #endif
+      }
     }
 
     boost::filesystem::path
     download_directory()
     {
+      @autoreleasepool
+      {
 #if defined(INFINIT_MACOSX)
-      NSString* download_dir =
-        NSSearchPathForDirectoriesInDomains(NSDownloadsDirectory,
-                                            NSUserDomainMask,
-                                            YES).firstObject;
+        NSString* download_dir =
+          NSSearchPathForDirectoriesInDomains(NSDownloadsDirectory,
+                                              NSUserDomainMask,
+                                              YES).firstObject;
 #elif defined(INFINIT_IOS)
-      NSString* doc_dir =
-        NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
-                                            NSUserDomainMask,
-                                            YES).firstObject;
-      NSString* download_dir =
-        [doc_dir stringByAppendingPathComponent:@"Downloads"];
+        NSString* doc_dir =
+          NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                              NSUserDomainMask,
+                                              YES).firstObject;
+        NSString* download_dir =
+          [doc_dir stringByAppendingPathComponent:@"Downloads"];
 #else
 # error Unknown plaform.
 #endif
-      return download_dir.UTF8String;
+        return download_dir.UTF8String;
+      }
     }
   }
 }
