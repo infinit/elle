26b41783de59d447bebc3ee7bd2a0fdb3153fac9
Glyn Matthews <glyn.matthews@gmail.com>
2016-03-20 11:01:36 +0100

    Added correct attribution to optional.

--- src/network/optional.hpp
+++ src/network/optional.hpp
@@ -1,4 +1,5 @@
 // Copyright 2016 Glyn Matthews.
+// Copyright (C) 2011 - 2012 Andrzej Krzemienski.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -6,6 +7,9 @@
 /**
  * \file
  * \brief Contains an implementation of C++17 optional (n3793).
+ *
+ * \sa https://github.com/akrzemi1/Optional
+ * \sa http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2014/n3848.html
  */
 
 #ifndef NETWORK_OPTIONAL_INC
