d7ddc6eed8944c3f63347e3c68a79b5252721735
mefyl <mefyl@infinit.io>
2016-04-21 13:27:26 +0200

    reactor.FDStream: Fix UB in unit test.

--- reactor/tests/fdstream.cc
+++ reactor/tests/fdstream.cc
@@ -15,7 +15,7 @@ main(int argc, char** argv)
       static const std::string expected("something");
       reactor::FDStream stdin_stream(0);
       ELLE_ASSERT(!stdin_stream.eof());
-      char content[1024];
+      char content[1024] = {0};
       stdin_stream.read(content, sizeof(content));
       ELLE_ASSERT_EQ(stdin_stream.gcount(), expected.size());
       ELLE_ASSERT_EQ(content, expected);
