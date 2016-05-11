7396d90b41ca14725ba973eb26a6e078aa20a74c
Christopher Crone <christopher.crone@infinit.io>
2016-05-11 13:47:47 +0200

    redhat: Remove unexpected files from BUILDROOT.

--- src/drake/redhat.py
+++ src/drake/redhat.py
@@ -21,12 +21,24 @@ class Packager(drake.Builder):
       {'base': basename, 'ver': version, 'dist': dist, 'arch': self.__arch}
     self.__destination = drake.Path(destination)
     self.__target = drake.node('%s/%s' % (destination, self.__rpm_name))
-    super().__init__(sources, [self.__target])
+    all_sources = list()
+    for s in sources:
+      all_sources.append(s)
+      if isinstance(s, drake.cxx.Executable):
+        for d in s.dependencies_recursive:
+          all_sources.append(d)
+    super().__init__(all_sources, [self.__target])
 
   def execute(self):
     rpm_dir = '%s/RPMS' % self.__top_dir
     if os.path.exists(rpm_dir):
       shutil.rmtree(rpm_dir)
+    install_dir = '%s/BUILDROOT' % self.__top_dir
+    for root, _, files in os.walk(install_dir):
+      for f in files:
+        f_path = ('%s/%s' % (root, f)).replace('%s/' % os.getcwd(), '', 1)
+        if f_path not in [str(s) for s in self.sources()]:
+          os.remove(str(f_path))
     for node in self.sources():
       if isinstance(node, drake.cxx.Executable):
         if not self.cmd('Undo prelink %s' % node, self.undo_prelink_cmd(node)):
@@ -54,3 +66,4 @@ class Packager(drake.Builder):
   @property
   def package(self):
     return self.__target
+
