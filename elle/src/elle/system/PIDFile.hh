4bb247a6e9decf3474ca39a5c26251a029ed6105
mefyl <mefyl@infinit.io>
2016-05-12 11:28:31 +0200

    elle.system: Add PIDFile.

---
+++ elle/src/elle/system/PIDFile.hh
@@ -0,0 +1,45 @@
+#ifndef ELLE_PIDFILE_HH
+# define ELLE_PIDFILE_HH
+
+# include <boost/filesystem.hpp>
+
+# include <elle/attribute.hh>
+
+namespace elle
+{
+  /* File containing a process ID
+   *
+   * The file only contains a numeric process ID in decimal, encoded latin-1. No
+   * terminating newline.
+   */
+  class PIDFile
+  {
+  /*-------------.
+  | Construction |
+  `-------------*/
+  public:
+    /** Create the PID file containing the current process id.
+     * \param path path where to create the file.
+     */
+    PIDFile(boost::filesystem::path path);
+    /// Delete the file.
+    ~PIDFile();
+    /// PID file path.
+    ELLE_ATTRIBUTE_R(boost::filesystem::path, path);
+
+
+  public:
+    /** Parse a PID file.
+     *
+     * \param path path to the PID file to parse.
+     * \return the PID contained in the file
+     * \throw elle::Error if the file is not readable or its content cannot be
+     *                    interpreted.
+     */
+    static
+    int
+    read(boost::filesystem::path const& path);
+  };
+}
+
+#endif
