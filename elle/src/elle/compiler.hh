0497da1581876a242d6c043e8a2e13cef57f6ccf
mefyl <mefyl@infinit.io>
2016-05-12 15:05:41 +0200

    elle.With: Align inner object.

--- elle/src/elle/compiler.hh
+++ elle/src/elle/compiler.hh
@@ -6,12 +6,14 @@
 #  define ELLE_COMPILER_ATTRIBUTE_ALWAYS_INLINE
 #  define ELLE_COMPILER_ATTRIBUTE_NO_INLINE __declspec((noinline))
 #  define ELLE_COMPILER_PRETTY_FUNCTION __FUNCSIG__
+#  define ELLE_COMPILER_ALIGN(Alignment) __declspec(align(Alignment
 #  define constexpr
 # else
 #  define ELLE_COMPILER_ATTRIBUTE_NORETURN __attribute__((noreturn))
 #  define ELLE_COMPILER_ATTRIBUTE_ALWAYS_INLINE __attribute__((always_inline))
 #  define ELLE_COMPILER_ATTRIBUTE_NO_INLINE __attribute__((noinline))
 #  define ELLE_COMPILER_PRETTY_FUNCTION __PRETTY_FUNCTION__
+#  define ELLE_COMPILER_ALIGN(Alignment) __attribute__ ((aligned(Alignment)))
 # endif
 
 # if defined(__GNUC__) && !defined(__clang__)
