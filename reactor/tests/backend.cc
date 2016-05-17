5fb3cf9c7683601d18e6d1616d72b4d9ea63df75
mefyl <mefyl@infinit.io>
2016-05-17 12:52:07 +0200

    reactor: Enable to switch windows coroutine backend.

--- reactor/tests/backend.cc
+++ reactor/tests/backend.cc
@@ -1,9 +1,8 @@
 #include <elle/test.hh>
 
-#include <reactor/backend/backend.hh>
-#ifdef INFINIT_WINDOWS
+#if defined(REACTOR_CORO_BACKEND_IO)
 # include <reactor/backend/coro_io/backend.hh>
-#else
+#elif defined(REACTOR_CORO_BACKEND_BOOST_CONTEXT)
 # include <reactor/backend/boost_context/backend.hh>
 #endif
 
@@ -107,14 +106,14 @@ ELLE_TEST_SUITE()
 {
   boost::unit_test::test_suite* backend = BOOST_TEST_SUITE("Backend");
   boost::unit_test::framework::master_test_suite().add(backend);
-#ifdef INFINIT_WINDOWS
+#if defined(REACTOR_CORO_BACKEND_IO)
 # define TEST(Name)                                                     \
   {                                                                     \
     backend->add(                                                       \
       BOOST_TEST_CASE(Name<reactor::backend::coro_io::Backend>),        \
       0, 10);                                                           \
   }
-#else
+#elif defined(REACTOR_CORO_BACKEND_BOOST_CONTEXT)
 # define TEST(Name)                                                     \
   {                                                                     \
     backend->add(                                                       \
