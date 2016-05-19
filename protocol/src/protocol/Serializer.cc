84e140b5801735e7db47efe6f40e2b758ff86088
Matthieu Nottale <matthieu.nottale@infinit.io>
2016-05-19 12:37:34 +0200

    protocol.Serializer: dont send interrupt control if we sent everything.

--- protocol/src/protocol/Serializer.cc
+++ protocol/src/protocol/Serializer.cc
@@ -469,13 +469,15 @@ namespace infinit
     void
     Version020Impl::_write(elle::Buffer const& packet)
     {
+      ELLE_DEBUG_SCOPE("chunk writer, sz=%s, chunk=%s", packet.size(),
+                       this->_chunk_size);
+      elle::Buffer::Size offset = 0;
       try
       {
-        elle::Buffer::Size offset = 0;
         auto send = [&]
           {
             auto to_send = std::min(this->_chunk_size, packet.size() - offset);
-            ELLE_DEBUG("send actual data")
+            ELLE_DEBUG("send actual data: %s", to_send)
             infinit::protocol::write(
               this->_stream, packet, false, offset, to_send);
             offset += to_send;
@@ -503,6 +505,7 @@ namespace infinit
         }
         while (offset < packet.size())
         {
+          ELLE_DEBUG("writing control: o=%s, size=%s", offset, packet.size());
           elle::With<reactor::Thread::NonInterruptible>() << [&]
           {
             write_control(this->_stream, Control::keep_going);
@@ -512,8 +515,13 @@ namespace infinit
       }
       catch (reactor::Terminate const&)
       {
-        write_control(this->_stream, Control::interrupt);
-        this->_stream.flush();
+        ELLE_DEBUG("terminated");
+        if (offset <  packet.size())
+        {
+          ELLE_DEBUG("writing control interrupt");
+          write_control(this->_stream, Control::interrupt);
+          this->_stream.flush();
+        }
         throw;
       }
     }
