3a72751fb254b42529bf960b99817cef9890878f
mefyl <mefyl@infinit.io>
2016-06-09 21:06:58 +0200

    elle.err: Add noreturn attribute.

--- elle/src/elle/err.hh
+++ elle/src/elle/err.hh
@@ -4,14 +4,17 @@
 # include <iosfwd>
 # include <utility>
 
+# include <elle/compiler.hh>
 # include <elle/printf-fwd.hh>
 
 namespace elle
 {
+  ELLE_COMPILER_ATTRIBUTE_NORETURN
   void
   err(std::string const& msg);
 
   template <typename ... Args>
+  ELLE_COMPILER_ATTRIBUTE_NORETURN
   void
   err(std::string const& fmt, Args&& ... args)
   {
