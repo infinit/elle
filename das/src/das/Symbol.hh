ab23ef6443363de18ed43e8a6c3a906425ee84ee
mefyl <mefyl@infinit.sh>
2016-11-20 18:37:46 +0100

    das.Symbol: Add effective symbols.

--- das/src/das/Symbol.hh
+++ das/src/das/Symbol.hh
@@ -5,6 +5,27 @@ namespace das
 {
   class Symbol
   {};
+
+  template <typename S>
+  class SpecificSymbol
+    : public Symbol
+  {
+  public:
+    template <typename E>
+    struct Effective
+    {
+      using Type = E;
+      using Formal = S;
+      Type value;
+    };
+
+    template <typename E>
+    Effective<E&&>
+    operator =(E&& v) const
+    {
+      return Effective<E&&>{std::forward<E>(v)};
+    }
+  };
 }
 
 # include <das/Symbol.hxx>
