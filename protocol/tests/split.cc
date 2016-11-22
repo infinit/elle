a47436c38b7a66725bc92df5fa72b8e173bcaa8b
Matthieu Nottale <matthieu.nottale@infinit.io>
2016-11-22 17:00:52 +0100

    protocol.ChanneledStream: Fix a race condition when reader gets terminated.

--- protocol/tests/split.cc
+++ protocol/tests/split.cc
@@ -16,6 +16,8 @@
 
 #include <protocol/Serializer.hh>
 #include <protocol/exceptions.hh>
+#include <protocol/Channel.hh>
+#include <protocol/ChanneledStream.hh>
 
 ELLE_LOG_COMPONENT("infinit.protocol.test");
 
@@ -139,9 +141,90 @@ ELLE_TEST_SCHEDULED(run_v020)
   }
 }
 
+ELLE_TEST_SCHEDULED(kill_reader)
+{
+  /* When channeledstream's effective reader is terminated, it
+   * used to wake a single thread that was waiting on the stream using signal().
+   * So if that thread terminates at that exact momemnt, there is no more
+   * any reader on the stream.
+  */
+  using namespace infinit::protocol;
+  auto v = elle::Version(0,2,0);
+  reactor::Barrier b;
+  reactor::network::TCPServer srv;
+  srv.listen(0);
+  std::unique_ptr<reactor::network::TCPSocket> s2;
+  std::unique_ptr<Serializer> ser2p;
+  std::unique_ptr<ChanneledStream> cs2p;
+  new reactor::Thread("accept", [&] {
+      s2 = srv.accept();
+      ser2p.reset(new Serializer(*s2, v, false));
+      cs2p.reset(new ChanneledStream(*ser2p));
+  }, true);
+
+  reactor::network::TCPSocket s1("127.0.0.1",
+                                 srv.local_endpoint().port());
+  Serializer ser1(s1, v, false);
+  ChanneledStream cs1(ser1);
+  while (!cs2p)
+    reactor::sleep(50_ms);
+  Serializer& ser2(*ser2p);
+  ChanneledStream& cs2(*cs2p);
+
+
+  int cid1, cid2, cid3;
+  bool r1 = false, r2 = false, r3 = false;
+  reactor::Thread::unique_ptr t1, t2, t3;
+  t1.reset(new reactor::Thread("t1", [&] {
+      infinit::protocol::Channel c(cs1);
+      b.open();
+      cid1 = c.id();
+      try {
+        c.read();
+      }
+      catch (std::exception const& e)
+      {
+        t3->terminate_now();
+        throw;
+      }
+      r1 = true;
+  }));
+  reactor::wait(b);
+  b.close();
+  //t1 is now the channel listener
+  t2.reset(new reactor::Thread("t2", [&] {
+      infinit::protocol::Channel c(cs1);
+      b.open();
+      cid2 = c.id();
+      c.write(elle::Buffer("foo"));
+      c.read();
+      r2 = true;
+  }));
+  // t2 is waiting
+  reactor::wait(b);
+  b.close();
+  t3.reset(new reactor::Thread("t3", [&] {
+      infinit::protocol::Channel c(cs1);
+      b.open();
+      cid3 = c.id();
+      c.read();
+      r3 = true;
+  }));
+  reactor::wait(b);
+  // t1 will kill t3, but it also works when killing from outside
+  t1->terminate_now();
+  // ensure t2 took over reading
+  Channel c = cs2.accept();
+  c.write(elle::Buffer("foo"));
+  while (!r2)
+    reactor::sleep(100_ms);
+  BOOST_CHECK(r2);
+}
+
 ELLE_TEST_SUITE()
 {
   auto& suite = boost::unit_test::framework::master_test_suite();
   suite.add(BOOST_TEST_CASE(run_v010), 0, valgrind(5, 25));
   suite.add(BOOST_TEST_CASE(run_v020), 0, valgrind(5, 25));
+  suite.add(BOOST_TEST_CASE(kill_reader), 0, valgrind(5, 25));
 }
