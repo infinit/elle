084997e654784c841c1758deb70090bf8c8c14d4
Antony MECHIN <antony.mechin@infinit.sh>
2016-11-25 15:02:02 +0100

    das: Improve example.

--- das/examples/das.cc
+++ das/examples/das.cc
@@ -8,6 +8,28 @@
 #include <das/printer.hh>
 #include <das/Symbol.hh>
 
+/*
+  This file shows:
+  - Symbols declaration
+  - Models declaration (nested, not nested)
+  - Serialization
+  - 'Printability'
+
+  Expected output:
+    ```
+    records representation:
+    Records(label = Favorites, records = [Record(title = Sandstorm, artist = Darube, id = 31900f3a-adaf-11e6-80f5-76304dec7eb7, release_date = 1999-Nov-26 00:00:00, tags = [Electronic, Dance]), Record(title = Never Gonna Give You Up, artist = Rick Astley, id = 1549b8a6-6cd7-453e-83de-49f3dc6b0e85, release_date = 1987-Jan-01 00:00:00, tags = ['80s Pop])])
+
+    records (in json):
+    {"label":"Favorites","records":[{"artist":"Darube","id":"31900f3a-adaf-11e6-80f5-76304dec7eb7","release_date":"1999-11-26T00:00:00","tags":["Electronic","Dance"],"title":"Sandstorm"},{"artist":"Rick Astley","id":"1549b8a6-6cd7-453e-83de-49f3dc6b0e85","release_date":"1987-01-01T00:00:00","tags":["'80s Pop"],"title":"Never Gonna Give You Up"}]}
+    ```
+
+  TODO:
+  - Custom models (DasShortRecord = ... <Symbol_title, Symbol_artist>)
+  - Named arguments: Record(title = "Darube")
+  - Static capabilities: Symbol_title.method_has<Record>()
+*/
+
 // Declare symbols (Symbol_*) needed by the Record::Model.
 DAS_SYMBOL(title);
 DAS_SYMBOL(artist);
@@ -40,7 +62,8 @@ struct Records
 };
 
 // The Model alse can be declared outside the class but will require
-// DAS_MODEL_DEFAULT.
+// DAS_MODEL_DEFAULT to define the default model for the class (this implies
+// that you can more than one model per class).
 DAS_SYMBOL(label);
 DAS_SYMBOL(records);
 using DasRecords = das::Model<Records,
@@ -78,8 +101,10 @@ main()
   std::cout << "records representation: " << std::endl << r << std::endl
             << std::endl;
   std::stringstream stream;
-  elle::serialization::json::SerializerOut serializer(stream, false);
-  das::serialize(r, serializer);
+  {
+    elle::serialization::json::SerializerOut serializer(stream, false);
+    das::serialize(r, serializer);
+  }
   std::cout << "records (in json): " << std::endl << stream.str() << std::endl;
 
   return 0;
