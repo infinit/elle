f2f2ace04866c3a82df35c09549579494531638b
Akim Demaille <akim.demaille@infinit.sh>
2016-11-28 13:06:48 +0100

    cryptography: remove clutter in the defintion of generate
    For some reason we were generating some templated struct around the
    generate functions.  Avoid this: directly instantiated the function we
    want.  This allows to move most of the compilation into the *.cc file.x

--- cryptography/src/cryptography/random.cc
+++ cryptography/src/cryptography/random.cc
@@ -17,12 +17,14 @@
 #include <openssl/err.h>
 #include <openssl/rand.h>
 
-#include <elle/system/platform.hh>
-#include <elle/os/environ.hh>
+#include <elle/Buffer.hh>
 #include <elle/log.hh>
+#include <elle/os/environ.hh>
+#include <elle/system/platform.hh>
 
-#include <cryptography/random.hh>
 #include <cryptography/Error.hh>
+#include <cryptography/cryptography.hh>
+#include <cryptography/random.hh>
 
 ELLE_LOG_COMPONENT("infinit.cryptography.random");
 
@@ -111,6 +113,115 @@ namespace infinit
                                     ::ERR_error_string(ERR_get_error(),
                                                        nullptr)));
       }
+
+      /*----------.
+      | Functions |
+      `----------*/
+
+      /// Val is in [T::min, T::max].  Bring it into [min, max].
+      template <typename T>
+      T
+      _rangify(T const Val,
+               T const min, T const max)
+      {
+        ELLE_ASSERT_LTE(min, max);
+        auto tmin = double(std::numeric_limits<T>::min());
+        auto tmax = double(std::numeric_limits<T>::max());
+        // The width of [T::min, T::max].
+        auto twidth = tmax - tmin;
+        // The width of [min, max].
+        auto width = double(max) - double(min);
+        return min + static_cast<T>((double(Val) - tmin) / twidth * width);
+      }
+
+      template <typename T>
+      T
+      _generate()
+      {
+        // Make sure the cryptographic system is set up.
+        cryptography::require();
+        T value;
+        fill(reinterpret_cast<unsigned char*>(&value), sizeof value);
+        return value;
+      }
+
+      /*-----------.
+      | Generators |
+      `-----------*/
+
+      template <typename T>
+      T
+      generate()
+      {
+        return _generate<T>();
+      }
+
+      template <typename T>
+      T
+      generate(T min, T max)
+      {
+        return _rangify(generate<T>(), min, max);
+      }
+
+      template <>
+      bool
+      generate<bool>()
+      {
+        // Because of RVO, we actually get a "bool" which ranges
+        // from 0 to 255 here.
+        return _generate<bool>() % 2;
+      }
+
+#define INFINIT_GENERATOR(Type)                         \
+      template Type generate<Type>();                   \
+      template Type generate<Type>(Type min, Type max)
+
+      INFINIT_GENERATOR(char);
+      INFINIT_GENERATOR(int8_t);
+      INFINIT_GENERATOR(int16_t);
+      INFINIT_GENERATOR(int32_t);
+      INFINIT_GENERATOR(int64_t);
+      INFINIT_GENERATOR(uint8_t);
+      INFINIT_GENERATOR(uint16_t);
+      INFINIT_GENERATOR(uint32_t);
+      INFINIT_GENERATOR(uint64_t);
+
+      INFINIT_GENERATOR(double);
+#undef INFINIT_GENERATOR
+
+      template <typename T>
+      T
+      generate(uint32_t const length);
+
+      template <>
+      std::string
+      generate<std::string>(uint32_t const length)
+      {
+        // Make sure the cryptographic system is set up.
+        cryptography::require();
+
+        static std::string const alphabet =
+          "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+          "0123456789@#$%^&*)_+=-\';:|.,<>?`~";
+
+        auto res = std::string{};
+        res.resize(length);
+        for (uint32_t i = 0; i < length; i++)
+          res[i] = alphabet[generate<int>(0, alphabet.length())];
+        return res;
+      }
+
+      template <>
+      elle::Buffer
+      generate<elle::Buffer>(uint32_t const size)
+      {
+        // Make sure the cryptographic system is set up.
+        cryptography::require();
+        auto res = elle::Buffer(int(size));
+        fill(reinterpret_cast<unsigned char*>(res.mutable_contents()),
+             res.size());
+        return res;
+      };
     }
   }
 }
