236a9aecbab0f69e752d8e9b71d68087bf2d0d36
Antony MECHIN <antony.mechin@infinit.sh>
2017-02-21 09:50:21 +0100

    dropbox, aws: Move services bindings to their new location.
    	* Move dropbox from ./dropbox to ./elle/src/elle/service/dropbox.
    	* Move aws from ./aws to ./elle/src/elle/service/aws.
    	* Use pragma instead of include guards.
    	* Change includes accordingly.

---
+++ src/elle/service/aws/S3.hh
@@ -0,0 +1,234 @@
+#pragma once
+
+#include <map>
+#include <vector>
+
+#include <boost/property_tree/ptree.hpp>
+
+#include <elle/Buffer.hh>
+#include <elle/Printable.hh>
+#include <elle/attribute.hh>
+#include <elle/service/aws/CanonicalRequest.hh>
+#include <elle/service/aws/Credentials.hh>
+#include <elle/service/aws/Exceptions.hh>
+#include <elle/service/aws/StringToSign.hh>
+
+namespace elle
+{
+  namespace service
+  {
+    namespace aws
+    {
+      struct URL
+      {
+        std::string scheme;
+        std::string domain;
+        std::string path;
+
+        std::string
+        join() const;
+      };
+      class S3:
+        public elle::Printable
+      {
+        /*------.
+        | Types |
+        `------*/
+      public:
+        using ProgressCallback = std::function<void (int)>;
+        typedef uint64_t FileSize;
+        typedef std::vector<std::pair<std::string, FileSize>> List;
+
+        enum class StorageClass
+        {
+          Standard,
+          StandardIA,
+          ReducedRedundancy,
+
+          Default, // Do not set the x-amz-storage-class header.
+        };
+
+        /*-------------.
+        | Construction |
+        `-------------*/
+      public:
+        /// Create a new S3 handler.
+        /// This requires a bucket name, remote folder and set of
+        /// credentials.
+        /// Use of this should be optimised based on S3 pricing:
+        /// http://aws.amazon.com/s3/pricing/
+        S3(Credentials const& credentials);
+        /// Version taking a function able to refresh credentials.
+        /// Bool argument is true on the first call, false on call caused by
+        /// expiration of current cached creds.
+        S3(std::function<Credentials(bool)> query_credentials);
+        /*-----------.
+        | Operations |
+        `-----------*/
+      public:
+        /// Put a block (as a file) into the remote folder.
+        /// This function does a single PUT call, so block size should be
+        /// something reasonable for an HTTP request.
+        /// @return the object ETag.
+        std::string
+        put_object(
+          elle::ConstWeakBuffer const& object,
+          std::string const& object_name,
+          RequestQuery const& query = RequestQuery(),
+          StorageClass storage_class = StorageClass::Default,
+          boost::optional<ProgressCallback> const& progress_callback = {});
+
+        /// Returns a list of all files names and their respective sizes inside
+        /// the remote folder.
+        /// This is limited to 1000 results but a starting offset (marker) can
+        /// be used if more are required. It is important to note that the files
+        /// are listed in alphabetical order.
+        std::vector<std::pair<std::string, FileSize>>
+        list_remote_folder(std::string const& marker = "");
+        /// List the full folder content.
+        std::vector<std::pair<std::string, FileSize>>
+        list_remote_folder_full();
+        /// Fetch an object from the remote folder.
+        /// The fetch is done in a single GET.
+        elle::Buffer
+        get_object(std::string const& object_name,
+                   RequestHeaders headers = RequestHeaders());
+        /// Fetch one chunk of an object
+        elle::Buffer
+        get_object_chunk(std::string const& object_name,
+                         FileSize offset, FileSize size);
+        /// Delete an object in the remote folder.
+        /// The folder itself can be deleted only once it is empty. This can be
+        /// done by setting the object_name to an empty string.
+        void
+        delete_object(std::string const& object_name,
+                      RequestQuery const& query = RequestQuery());
+        /// Delete folder and all its content
+        void
+        delete_folder();
+
+        /// Initialize multipart upload for given object
+        /// @return an upload key needed by further operations
+        std::string
+        multipart_initialize(
+          std::string const& object_name,
+          std::string const& mime_type = "binary/octet-stream",
+          StorageClass storage_class = StorageClass::Default);
+
+        typedef std::pair<int, std::string> MultiPartChunk;
+        /// Upload one part of a multipart upload
+        /// @return id for chunk information that needs to be passed to
+        /// multipart_finalize
+        std::string
+        multipart_upload(
+          std::string const& object_name,
+          std::string const& upload_key,
+          elle::ConstWeakBuffer const& object,
+          int chunk,
+          boost::optional<ProgressCallback> const& progress_callback = {});
+
+        /// Finalize a multipart by joining all parts together
+        void
+        multipart_finalize(std::string const& object_name,
+                           std::string const& upload_key,
+                           std::vector<MultiPartChunk> const& chunks);
+
+        /// Abort a multipart transfer, delete all chunks
+        void
+        multipart_abort(std::string const& object_name,
+                        std::string const& upload_key);
+
+        std::vector<MultiPartChunk>
+        multipart_list(std::string const& object_name,
+                       std::string const& upload_key);
+
+        /*-----------.
+        | Attributes |
+        `-----------*/
+      public:
+        /// Get the host including the http scheme to connect to depending on
+        /// the credentials.
+        virtual
+        URL
+        hostname(Credentials const& credentials,
+                 boost::optional<std::string> override_host = {}) const;
+        ELLE_ATTRIBUTE(Credentials, credentials);
+        ELLE_ATTRIBUTE(std::function<Credentials(bool)>, query_credentials);
+
+        /*--------.
+        | Helpers |
+        `--------*/
+      private:
+        enum class RequestKind
+        {
+          control, // Expected a small amount of data in/out
+          data // expected to transfer some amount of data in our out
+        };
+
+        std::string
+        _md5_digest(elle::ConstWeakBuffer const& buffer);
+
+        std::string
+        _sha256_hexdigest(elle::ConstWeakBuffer const& buffer);
+
+        std::string
+        _amz_date(RequestTime const& request_time);
+
+        std::vector<std::string>
+        _signed_headers(RequestHeaders const& headers);
+
+        StringToSign
+        _make_string_to_sign(RequestTime const& request_time,
+                             std::string const& canonical_request_sha256);
+
+        std::vector<std::pair<std::string, FileSize>>
+        _parse_list_xml(std::istream& stream);
+
+        reactor::http::Request::Configuration
+        _initialize_request(RequestKind kind,
+                            RequestTime request_time,
+                            CanonicalRequest const& canonical_request,
+                            const RequestHeaders& initial_headers,
+                            boost::posix_time::time_duration timeout);
+
+        /// Check return code and throw appropriate exception if error
+        /// ELLE_WARN the request response in case of error
+        /// Dumps it if dump_response is true, which eats the stream content.
+        void
+        _check_request_status(reactor::http::Request& request,
+                              std::string const& operation);
+
+        /// Build and emit request, retries in case of credentials expiry kind
+        /// is used to switch between global duration timeout and stall timeout.
+        std::unique_ptr<reactor::http::Request>
+        _build_send_request(
+          RequestKind kind,
+          std::string const& url,
+          std::string const& operation, // Used by exception message/log only.
+          reactor::http::Method method,
+          RequestQuery const& query = RequestQuery(),
+          RequestHeaders const& extra_headers = RequestHeaders(),
+          std::string const& content_type = "application/json",
+          elle::ConstWeakBuffer const& payload = elle::ConstWeakBuffer(),
+          boost::optional<boost::posix_time::time_duration> timeout =
+          boost::optional<boost::posix_time::time_duration>(),
+          boost::optional<ProgressCallback> const& progress_callback = {});
+
+        /*----------.
+        | Printable |
+        `----------*/
+      public:
+        virtual
+        void
+        print(std::ostream& stream) const;
+        // Callback invoked on all errors, transient or not
+        typedef
+        std::function<void(AWSException const&, bool will_retry)> ErrorCallback;
+        ELLE_ATTRIBUTE_RW(ErrorCallback, on_error);
+      };
+
+      std::string
+      uri_encode(std::string const& input, bool encodeSlash);
+    }
+  }
+}
--- src/elle/service/aws/tests/aws_requests.cc
+++ src/elle/service/aws/tests/aws_requests.cc
@@ -1,24 +1,23 @@
-#include <aws/CanonicalRequest.hh>
-#include <aws/Credentials.hh>
-#include <aws/Exceptions.hh>
-#include <aws/Keys.hh>
-#include <aws/S3.hh>
-#include <aws/SigningKey.hh>
-#include <aws/StringToSign.hh>
-
 #include <boost/date_time/gregorian/gregorian.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
+#include <elle/cryptography/hash.hh>
 #include <elle/format/hexadecimal.hh>
 #include <elle/json/json.hh>
 #include <elle/test.hh>
+#include <elle/service/aws/CanonicalRequest.hh>
+#include <elle/service/aws/Credentials.hh>
+#include <elle/service/aws/Exceptions.hh>
+#include <elle/service/aws/Keys.hh>
+#include <elle/service/aws/S3.hh>
+#include <elle/service/aws/SigningKey.hh>
+#include <elle/service/aws/StringToSign.hh>
 
 #include <reactor/scheduler.hh>
 #include <reactor/thread.hh>
 
-#include <elle/cryptography/hash.hh>
 
-ELLE_LOG_COMPONENT("aws.test");
+ELLE_LOG_COMPONENT("elle.services.aws.test");
 
 // Credentials:
 // AccessKeyId
@@ -38,7 +37,7 @@ static
 boost::posix_time::ptime _later = _now + boost::posix_time::hours(1);
 
 static
-aws::Credentials _GET_credentials(
+elle::service::aws::Credentials _GET_credentials(
   "ASIAJOU5RQKL2N6YKOXQ",
   "9yC/yEGXvjH+3At0p6GOt04K8cgeol+klLlrFkLY",
   "AQoDYXdzEGIagAPirqRMvxZjBVQrtGqwzzTKcfyU0cn+a5uyJPto88J09j/WYZdqrQ8YEr6MAk3sXcWlgtZ+k259fkSCqfkwg1ntS/SkrtDaC7GokhnOMRXsqGYV7Hf48QyWV3XpnIp3x0XgJKOvq2Mwfn1ZuFy7364Qk/5lkYLuQ/i2EqMXRgzZFH2TPkYk8+k4l4sNJf1c4/Vl9FapstpbwYha96oAFbZSd9GfTIkFXXhXMCxykE9uDxM2STYGkoFkE37doZUoZB3n6pjPdnV60Hn/KmgRTEC8fn/SdYrmpnpXIRcPi8tL+iOa3x74LcUv0KysNqVZuBIScVCmVnjNxhueSz7BdM9Zo7NsGNzrWBTUShjoXmw+a92eINvCKRv+buD4PYNGk+HekJxF3IGjUGaoBYJkfpwCYJMDsfs/R7DPG3qzqJN465bXDj/8RZmps7qb3ew8AhasWT8cMUDPr2ClGUwLaVkyH1KBvKxc6hhxzZOPuJq4ZzGQ/4axoM1CtP0UjerJdNMg/oDMmQU=",
@@ -50,7 +49,7 @@ aws::Credentials _GET_credentials(
 );
 
 static
-aws::Credentials _PUT_credentials(
+elle::service::aws::Credentials _PUT_credentials(
   "ASIAJAGQUOJVE3HO46ZA",
   "pG1iz87bCYOZ9sQ4fkryBgiS/wnS7K+2MJUmS7Hb",
   "AQoDYXdzEGIawAIayH3x2pmusA2EhnhOK+42f07Y0cQILf7yltnXMG6sJ6sbE+o0IgZW8lEkCcZRGcXD1fM6pGAEBEYgO9ZJVXvRG5+riMGXmb7RHWGEEYRidqylFqK7vepkU2+ocFBshxKoSOJMCl5yjU351J3INvkkeTNJ9lpz0mPBWpEv82uBYzo6j0oDJMJutaiFvmRfpS79KWHemOcgUxbjTKChms3askIDJf6zgDTyIymWaIGquFgKwNgCO6UNk2zCCiWf3vtryuPxrlERz/Xm9cCejC4B4FiTjes6fWH7jlxbDu0s+6s7ivdSga68Ex4OJbetpYIn3vOTMTRzyUN/0TvMJPZFRn8joW3cjnJOQNMtoEmtS7Hp1fvOBkk/f1qWAM/GrMvbcJThOAjPuhqbJY7DpP4SNQtGgdwhOjC04Z3fUDgqnyDR/8uZBQ==",
@@ -66,7 +65,7 @@ aws::Credentials _PUT_credentials(
 // Test derived from link below with their query string instead of none.
 // http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
 static
-aws::CanonicalRequest
+elle::service::aws::CanonicalRequest
 _make_canonical_request()
 {
   std::map<std::string, std::string> query;
@@ -98,7 +97,7 @@ _make_canonical_request()
   auto digest = elle::cryptography::hash(content,
                                          elle::cryptography::Oneway::sha256);
 
-  aws::CanonicalRequest request(
+  elle::service::aws::CanonicalRequest request(
     reactor::http::Method::POST,
     "/",
     query,
@@ -112,26 +111,26 @@ _make_canonical_request()
 
 // http://docs.aws.amazon.com/general/latest/gr/sigv4-create-string-to-sign.html
 static
-aws::StringToSign
+elle::service::aws::StringToSign
 _make_string_to_sign()
 {
   boost::posix_time::ptime request_time(
     boost::gregorian::date(2011, boost::gregorian::Sep, 9),
     boost::posix_time::hours(23) + boost::posix_time::minutes(36)
   );
-  aws::CanonicalRequest canonical_request = _make_canonical_request();
-  aws::CredentialScope credential_scope(request_time, aws::Service::iam,
+  elle::service::aws::CanonicalRequest canonical_request = _make_canonical_request();
+  elle::service::aws::CredentialScope credential_scope(request_time, elle::service::aws::Service::iam,
                                         "us-east-1");
-  aws::StringToSign string_to_sign(request_time, credential_scope,
+  elle::service::aws::StringToSign string_to_sign(request_time, credential_scope,
                                    canonical_request.sha256_hash(),
-                                   aws::SigningMethod::aws4_hmac_sha256);
+                                   elle::service::aws::SigningMethod::aws4_hmac_sha256);
   ELLE_DEBUG("string to sign: %s", string_to_sign);
   return string_to_sign;
 }
 
 ELLE_TEST_SCHEDULED(canonical_request)
 {
-  aws::CanonicalRequest request = _make_canonical_request();
+  elle::service::aws::CanonicalRequest request = _make_canonical_request();
   std::string expected_output = "POST\n/\nAction=ListUsers&Version=2010-05-08&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credentials=AKIAIOSFODNN7EXAMPLE%2F20110909%2Fus-east-1%2Fiam%2Faws4_request&X-Amz-Date=20110909T233600Z&X-Amz-SignedHeaders=content-type%3Bhost%3Bx-amz-date\ncontent-type:application/x-www-form-urlencoded; charset=utf-8\nhost:iam.amazonaws.com\nx-amz-date:20110909T233600Z\n\ncontent-type;host;x-amz-date\nb6359072c78d70ebee1e81adcbab4f01bf2c23245fa365ef83fe8f1f955085e2";
   std::string expected_hash = "d030ae173c82eeffc6a53f427a3ddafcef9aad71aef5ac9f82122e0742b8c1b8";
   BOOST_CHECK_EQUAL(request.canonical_request(), expected_output);
@@ -140,7 +139,7 @@ ELLE_TEST_SCHEDULED(canonical_request)
 
 ELLE_TEST_SCHEDULED(string_to_sign)
 {
-  aws::StringToSign string_to_sign = _make_string_to_sign();
+  elle::service::aws::StringToSign string_to_sign = _make_string_to_sign();
   std::string expected = "AWS4-HMAC-SHA256\n20110909T233600Z\n20110909/us-east-1/iam/aws4_request\nd030ae173c82eeffc6a53f427a3ddafcef9aad71aef5ac9f82122e0742b8c1b8";
   BOOST_CHECK_EQUAL(string_to_sign.string(), expected);
 }
@@ -151,9 +150,9 @@ ELLE_TEST_SCHEDULED(signing_key)
   std::string aws_secret("wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY");
   boost::posix_time::ptime request_time(
     boost::gregorian::date(2012, boost::gregorian::Feb,15));
-  aws::SigningKey signing_key(aws_secret, request_time,
+  elle::service::aws::SigningKey signing_key(aws_secret, request_time,
                               "us-east-1",
-                              aws::Service::iam);
+                              elle::service::aws::Service::iam);
   BOOST_CHECK_EQUAL(
     elle::format::hexadecimal::encode(signing_key.key()),
     "f4780e2d9f65fa895f9c67b32ce1baf0b0d8a43505a000a1a9e090d414db404d");
@@ -161,15 +160,15 @@ ELLE_TEST_SCHEDULED(signing_key)
 
 ELLE_TEST_SCHEDULED(sign_request)
 {
-  aws::StringToSign string_to_sign = _make_string_to_sign();
+  elle::service::aws::StringToSign string_to_sign = _make_string_to_sign();
   std::string aws_secret("wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY");
   boost::posix_time::ptime request_time(
     boost::gregorian::date(2011, boost::gregorian::Sep, 9),
     boost::posix_time::hours(23) + boost::posix_time::minutes(36)
   );
-  aws::SigningKey signing_key(aws_secret, request_time,
+  elle::service::aws::SigningKey signing_key(aws_secret, request_time,
                               "us-east-1",
-                              aws::Service::iam);
+                              elle::service::aws::Service::iam);
   BOOST_CHECK_EQUAL(
     signing_key.sign_message(string_to_sign.string()),
     "c9d1c4e90e9f0b65ae4020a33bada35341ee2f8188c70b2a976e6e767414ed1f");
@@ -178,7 +177,7 @@ ELLE_TEST_SCHEDULED(sign_request)
 // // Should only be run manually with generated crendentials.
 // ELLE_TEST_SCHEDULED(s3_put)
 // {
-//   aws::S3 s3_handler(_PUT_credentials);
+//   elle::service::aws::S3 s3_handler(_PUT_credentials);
 //   elle::ConstWeakBuffer object("a fair bit of stuff in a file!");
 //   BOOST_CHECK_NO_THROW(s3_handler.put_object(object, "test_object"));
 //   BOOST_CHECK_NO_THROW(s3_handler.put_object(object, "test_object_1"));
@@ -189,15 +188,15 @@ ELLE_TEST_SCHEDULED(sign_request)
 // // Should only be run manually with generated credentials.
 // ELLE_TEST_SCHEDULED(s3_list)
 // {
-//   aws::S3 s3_handler(_GET_credentials);
-//   std::vector<std::pair<std::string, aws::S3::FileSize>> all =
+//   elle::service::aws::S3 s3_handler(_GET_credentials);
+//   std::vector<std::pair<std::string, elle::service::aws::S3::FileSize>> all =
 //     s3_handler.list_remote_folder();
 //   BOOST_CHECK_EQUAL(all.size(), 4);
 //   for (auto const& item: all)
 //   {
 //     ELLE_LOG("ALL filename: %s (%s)", item.first, item.second);
 //   }
-//   std::vector<std::pair<std::string, aws::S3::FileSize>> some =
+//   std::vector<std::pair<std::string, elle::service::aws::S3::FileSize>> some =
 //     s3_handler.list_remote_folder("x");
 //   BOOST_CHECK_EQUAL(some.size(), 2);
 //   for (auto const& item: some)
@@ -209,16 +208,16 @@ ELLE_TEST_SCHEDULED(sign_request)
 // // Should only be run manually with generated credentials.
 // ELLE_TEST_SCHEDULED(s3_get)
 // {
-//   aws::S3 s3_handler(_GET_credentials);
+//   elle::service::aws::S3 s3_handler(_GET_credentials);
 //   elle::ConstWeakBuffer expected("a fair bit of stuff in a file!");
 //   BOOST_CHECK_EQUAL(s3_handler.get_object("test_object"), expected);
-//   BOOST_CHECK_THROW(s3_handler.get_object("doesnt_exist"), aws::FileNotFound);
+//   BOOST_CHECK_THROW(s3_handler.get_object("doesnt_exist"), elle::service::aws::FileNotFound);
 // }
 
 // // Should only be run manually with generated credentials.
 // ELLE_TEST_SCHEDULED(s3_delete)
 // {
-//   aws::S3 s3_handler(_GET_credentials);
+//   elle::service::aws::S3 s3_handler(_GET_credentials);
 //   BOOST_CHECK_NO_THROW(s3_handler.delete_object("test_object"));
 // }
 
