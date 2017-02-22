a9bc33010935895e25c996222501c0467ebd767d
Akim Demaille <akim.demaille@infinit.sh>
2017-02-22 12:57:48 +0100

    elle.range: add as_range
    It might be useful to introduce our own elle::equal_range that wraps
    the result.  Let's bet C++20's ranges will have this right.

---
+++ elle/src/elle/range.hh
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace elle
+{
+  namespace detail
+  {
+    /// A pair of iterators complying with the range-based for loop
+    /// protocol.
+    template <typename It>
+    struct range
+    {
+      It begin() const { return _begin; }
+      It end() const { return _end; }
+      It _begin, _end;
+    };
+  }
+
+  /// Turn a pair of iterator into a for-loop compatible range.
+  ///
+  /// Especially useful with the return type of std::equal_range.
+  template <typename It>
+  detail::range<It>
+  as_range(const std::pair<It, It> &p)
+  {
+    return {p.first, p.second};
+  }
+}
