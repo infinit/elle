f50dd82f415908fc55ce784eecefdba6764c7d23
Akim Demaille <akim.demaille@docker.com>
2017-03-16 10:48:25 +0100

    reactor: let resolve work on "hostname:port" strings

--- src/elle/reactor/network/resolve.cc
+++ src/elle/reactor/network/resolve.cc
@@ -66,7 +66,7 @@ namespace elle
               : Query(this->_hostname, this->_service);
             this->_resolver.async_resolve(
               query,
-              [this](const boost::system::error_code& error,
+              [this](boost::system::error_code const& error,
                      typename Resolver::iterator it)
               {
                 this->_wakeup(error, it);
@@ -75,7 +75,7 @@ namespace elle
 
         private:
           void
-          _wakeup(const boost::system::error_code& error,
+          _wakeup(boost::system::error_code const& error,
                   typename Resolver::iterator it)
           {
             auto const end
@@ -121,41 +121,80 @@ namespace elle
           ELLE_ATTRIBUTE_R(bool, ipv4_only);
         };
 
-        std::string
-        to_string(const Port& port)
-        {
-          if (auto i = boost::get<std::string>(&port))
-            return *i;
-          else
-            return std::to_string(boost::get<int>(port));
-        }
-
         template <typename Protocol>
         std::vector<typename Protocol::resolver::endpoint_type>
-        resolve(const std::string& hostname,
-                const Port& service,
+        resolve(std::string const& hostname,
+                std::string const& service,
                 bool ipv4_only)
         {
-          Resolution<Protocol> resolution(hostname, to_string(service), ipv4_only);
+          Resolution<Protocol> resolution(hostname, service, ipv4_only);
           resolution.run();
           return resolution.end_points();
         }
+
+        auto
+        host_port(std::string const& repr)
+        {
+          auto const sep = repr.find_last_of(':');
+          if (sep == std::string::npos || sep == repr.length())
+            elle::err("invalid endpoint: %s", repr);
+          return std::make_pair(repr.substr(0, sep), repr.substr(sep + 1));
+        }
       }
 
+      /*------.
+      | tcp.  |
+      `------*/
+
       std::vector<boost::asio::ip::tcp::endpoint>
-      resolve_tcp(const std::string& hostname,
-                  const Port& service,
+      resolve_tcp(std::string const& hostname,
+                  std::string const& service,
                   bool ipv4_only)
       {
         return resolve<boost::asio::ip::tcp>(hostname, service, ipv4_only);
       }
 
+      std::vector<boost::asio::ip::tcp::endpoint>
+      resolve_tcp(std::string const& hostname,
+                  int port,
+                  bool ipv4_only)
+      {
+        return resolve<boost::asio::ip::tcp>(hostname, std::to_string(port), ipv4_only);
+      }
+
+      std::vector<boost::asio::ip::tcp::endpoint>
+      resolve_tcp_repr(std::string const& repr, bool ipv4_only)
+      {
+        auto hp = host_port(repr);
+        return resolve<boost::asio::ip::tcp>(std::get<0>(hp), std::get<1>(hp), ipv4_only);
+      }
+
+      /*------.
+      | udp.  |
+      `------*/
+
       std::vector<boost::asio::ip::udp::endpoint>
-      resolve_udp(const std::string& hostname,
-                  const Port& service,
+      resolve_udp(std::string const& hostname,
+                  std::string const& service,
                   bool ipv4_only)
       {
         return resolve<boost::asio::ip::udp>(hostname, service, ipv4_only);
+      }
+
+      std::vector<boost::asio::ip::udp::endpoint>
+      resolve_udp(std::string const& hostname,
+                  int port,
+                  bool ipv4_only)
+      {
+        return resolve<boost::asio::ip::udp>(hostname, std::to_string(port), ipv4_only);
+      }
+
+      std::vector<boost::asio::ip::udp::endpoint>
+      resolve_udp_repr(std::string const& repr,
+                       bool ipv4_only)
+      {
+        auto hp = host_port(repr);
+        return resolve<boost::asio::ip::udp>(std::get<0>(hp), std::get<1>(hp), ipv4_only);
       }
     }
   }
