99ba4428e0972b02702e0353e5302127ca249274
Akim Demaille <akim.demaille@docker.com>
2017-03-27 11:40:41 +0200

    reactor.network: clean up UTP server
    Indentation fixes

--- src/elle/reactor/network/udp-server.hh
+++ src/elle/reactor/network/udp-server.hh
@@ -15,54 +15,54 @@ namespace elle
     {
       struct HashEndpoint
       {
-          std::size_t operator()(const boost::asio::ip::udp::endpoint&) const;
-          std::size_t operator()(const boost::asio::ip::tcp::endpoint&) const;
+        std::size_t operator()(const boost::asio::ip::udp::endpoint&) const;
+        std::size_t operator()(const boost::asio::ip::tcp::endpoint&) const;
       };
 
       class UDPServer: public Server, public ProtoServer<UDPSocket>
       {
-        public:
-          using Super = Server;
-          UDPServer(Scheduler& sched);
-          ~UDPServer();
+      public:
+        using Super = Server;
+        UDPServer(Scheduler& sched);
+        ~UDPServer();
 
-        /*----------.
-        | Listening |
-        `----------*/
-        public:
-          void listen(int port, bool enable_ipv6=false);
-          void listen(const EndPoint& end_point);
+      /*----------.
+      | Listening |
+      `----------*/
+      public:
+        void listen(int port, bool enable_ipv6=false);
+        void listen(const EndPoint& end_point);
 
-        /*----------.
-        | Accepting |
-        `----------*/
-        public:
-          UDPServerSocket* accept();
-        private:
-          friend class UDPServerSocket;
-          boost::asio::ip::udp::socket* _acceptor;
+      /*----------.
+      | Accepting |
+      `----------*/
+      public:
+        UDPServerSocket* accept();
+      private:
+        friend class UDPServerSocket;
+        boost::asio::ip::udp::socket* _acceptor;
 
-        /*---------------.
-        | Demultiplexing |
-        `---------------*/
-        private:
-          void _receive();
-          void _receive_handle(const boost::system::error_code& error,
-                               std::size_t bytes_transferred);
-          char _buffer[512];
-          EndPoint _peer;
-          using Clients = std::unordered_map<EndPoint, UDPServerSocket*,
-                                       HashEndpoint>;
-          Clients _clients;
-          Signal _accept;
-          using Sockets = std::vector<UDPServerSocket*>;
-          Sockets _accepted;
+      /*---------------.
+      | Demultiplexing |
+      `---------------*/
+      private:
+        void _receive();
+        void _receive_handle(const boost::system::error_code& error,
+                             std::size_t bytes_transferred);
+        char _buffer[512];
+        EndPoint _peer;
+        using Clients = std::unordered_map<EndPoint, UDPServerSocket*,
+                                           HashEndpoint>;
+        Clients _clients;
+        Signal _accept;
+        using Sockets = std::vector<UDPServerSocket*>;
+        Sockets _accepted;
 
-        /*---------.
-        | Printing |
-        `---------*/
-        public:
-          void print(std::ostream& s) const;
+      /*---------.
+      | Printing |
+      `---------*/
+      public:
+        void print(std::ostream& s) const;
       };
 
       std::ostream& operator << (std::ostream& s, const UDPServer& server);
