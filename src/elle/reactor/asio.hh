80a2f9d0cda859019bf06f3431999173cdf20372
Akim Demaille <akim.demaille@docker.com>
2017-05-02 14:38:41 +0200

    elle.reactor: cut some warnings about <sys/poll.h>

--- src/elle/reactor/asio.hh
+++ src/elle/reactor/asio.hh
@@ -1,10 +1,25 @@
 #pragma once
 
-#ifdef _MSC_VER
+#if defined _MSC_VER
 # include <SDKDDKVer.h>
 #endif
-#ifdef INFINIT_WINDOWS
+
+#if defined INFINIT_WINDOWS
 # include <winsock2.h>
 #endif
 
+#if defined __GCC__
+# pragma GCC diagnostic push
+
+// /usr/include/sys/poll.h:1:2: warning: #warning redirecting incorrect #include <sys/poll.h> to <poll.h> [-Wcpp]
+//  #warning redirecting incorrect #include <sys/poll.h> to <poll.h>
+//   ^~~~~~~
+
+# pragma GCC diagnostic ignored "-Wcpp"
+#endif
+
 #include <boost/asio.hpp>
+
+#if defined __GCC__
+# pragma GCC diagnostic pop
+#endif
