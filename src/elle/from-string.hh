7f0254c9b88175de7449c808cb83321ea8063768
Akim Demaille <akim@lrde.epita.fr>
2017-07-10 14:00:04 +0200

    elle.from-string: take care of the integral types

--- src/elle/from-string.hh
+++ src/elle/from-string.hh
@@ -1,30 +1,89 @@
 #pragma once
 
 #include <string>
+#include <type_traits>
+
+#include <elle/err.hh>
+#include <elle/meta.hh>
+#include <elle/printf.hh>
 
 // FIXME: C++17: migrate to std::string_view.  Explore std::from_chars
 namespace elle
 {
+  /// Extract a value of type `Dest` from `s`.
+  ///
+  /// @throws std::out_of_range or std::invalid_argument.
+  template <typename Dest>
+  Dest
+  from_string(std::string const& s);
+
   bool
   from_string(std::string const& s, bool*);
 
-#define ELLE_DEFINE(Type, Conversion)           \
-  inline                                        \
-  Type                                          \
-  from_string(std::string const& s, Type*)      \
-  {                                             \
-    return Conversion(s);                       \
+  // GCC 4.9 does not catch negative numbers as an error for unsigned
+  // types.
+  namespace
+  {
+    template <typename Type>
+    auto
+    check_non_negative(std::string const& s)
+    {
+      meta::static_if<std::is_unsigned<Type>{}>
+        ([&s]
+        {
+          for (auto c: s)
+            if (c == '-')
+              err<std::out_of_range>("invalid positive number: %s", s);
+            else if (!std::isspace(c))
+              break;
+        })();
+    }
   }
 
+  // Types that have their std::sto<TYPE> function.
+#define ELLE_DEFINE(Type, Conversion)                           \
+  inline                                                        \
+  Type                                                          \
+  from_string(std::string const& s, Type*)                      \
+  {                                                             \
+    check_non_negative<Type>(s);                                \
+    auto size = size_t{};                                       \
+    auto res = Conversion(s, &size);                            \
+    if (size < s.size())                                        \
+      err<std::invalid_argument>("invalid number: %s", s);      \
+    return res;                                                 \
+  }
+
+  ELLE_DEFINE(double, std::stod);
+  ELLE_DEFINE(float, std::stof);
   ELLE_DEFINE(int, std::stoi);
+  ELLE_DEFINE(long double, std::stold);
   ELLE_DEFINE(long long, std::stoll);
   ELLE_DEFINE(long, std::stol);
   ELLE_DEFINE(unsigned long long, std::stoull);
   ELLE_DEFINE(unsigned long, std::stoul);
   ELLE_DEFINE(unsigned, std::stou);
-  ELLE_DEFINE(float, std::stof);
-  ELLE_DEFINE(double, std::stod);
-  ELLE_DEFINE(long double, std::stold);
+
+#undef ELLE_DEFINE
+
+  // Types that don't have a standard conversion routine.
+#define ELLE_DEFINE(Type)                               \
+  inline                                                \
+  Type                                                  \
+  from_string(std::string const& s, Type*)              \
+  {                                                     \
+    auto v = from_string<int>(s);                       \
+    if (std::numeric_limits<Type>::min() <= v           \
+        && v <= std::numeric_limits<Type>::max())       \
+      return v;                                         \
+    else                                                \
+      err<std::out_of_range>("invalid number: %s", s);  \
+  }
+
+  ELLE_DEFINE(signed char);
+  ELLE_DEFINE(signed short);
+  ELLE_DEFINE(unsigned char);
+  ELLE_DEFINE(unsigned short);
 
 #undef ELLE_DEFINE
 
--- tests/elle/from-string.cc
+++ tests/elle/from-string.cc
@@ -3,9 +3,10 @@
 
 #include <elle/from-string.hh>
 
+using elle::from_string;
+
 BOOST_AUTO_TEST_CASE(bool_)
 {
-  using elle::from_string;
   BOOST_TEST(from_string<bool>("false") == false);
   BOOST_TEST(from_string<bool>("0")     == false);
   BOOST_TEST(from_string<bool>("no")    == false);
@@ -15,9 +16,44 @@ BOOST_AUTO_TEST_CASE(bool_)
   BOOST_TEST(from_string<bool>("yes")  == true);
 }
 
+namespace
+{
+  template <typename T>
+  void
+  check_number(T v)
+  {
+    BOOST_TEST(from_string<T>(std::to_string(v)) == v);
+  }
+
+  template <typename T>
+  void
+  check_limits()
+  {
+    check_number(std::numeric_limits<T>::min());
+    elle::meta::static_if<std::is_signed<T>{}>
+      ([](auto one)
+      {
+        // Signed.
+        check_number(-one);
+        check_number(T{42} * -one);
+      },
+       [](auto)
+      {
+        // Unsigned.
+        BOOST_CHECK_THROW(from_string<T>("-12"),
+                          std::out_of_range);
+      })(T{1});
+    check_number(T{0});
+    check_number(T{42});
+    check_number(std::numeric_limits<T>::max());
+
+    BOOST_CHECK_THROW(from_string<T>("0crap"),
+                      std::invalid_argument);
+  }
+}
+
 BOOST_AUTO_TEST_CASE(numbers)
 {
-  using elle::from_string;
   BOOST_TEST(from_string<int>("42")      == 42);
   BOOST_TEST(from_string<int>("  +42")   == 42);
   BOOST_TEST(from_string<int>("-42")     == -42);
@@ -25,7 +61,14 @@ BOOST_AUTO_TEST_CASE(numbers)
   BOOST_TEST(from_string<float>("42")    == 42.f);
   BOOST_TEST(from_string<double>("42")   == 42.);
 
-  // GCC 4.9 does not raise on this one.  We have to check by ourselves.
-  // BOOST_CHECK_THROW(from_string<unsigned>("-12"),
-  // std::out_of_range);
+  check_limits<int>();
+  check_limits<long long>();
+  check_limits<long>();
+  check_limits<signed char>();
+  check_limits<signed short>();
+  check_limits<unsigned char>();
+  check_limits<unsigned long long>();
+  check_limits<unsigned long>();
+  check_limits<unsigned short>();
+  check_limits<unsigned>();
 }
