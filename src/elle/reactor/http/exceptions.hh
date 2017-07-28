1b75f2dfa9017a89e76c46feffaf811f5d9d0817
Akim Demaille <akim@lrde.epita.fr>
2017-07-28 18:03:20 +0200

    http: prefer move to copy

--- src/elle/reactor/http/exceptions.hh
+++ src/elle/reactor/http/exceptions.hh
@@ -10,45 +10,44 @@ namespace elle
     namespace http
     {
       /// Fatal error on a request that was not successfully completed.
-      class RequestError:
-        public elle::Error
+      class RequestError
+        : public elle::Error
       {
       public:
         using Super = elle::Error;
-        RequestError(std::string const& url,
-                     std::string const& error);
+        RequestError(std::string url, std::string error);
       private:
         ELLE_ATTRIBUTE_R(std::string, url);
         ELLE_ATTRIBUTE_R(std::string, error);
       };
 
       /// The server didn't send any response.
-      class EmptyResponse:
-        public RequestError
+      class EmptyResponse
+        : public RequestError
       {
       public:
         using Super = RequestError;
-        EmptyResponse(std::string const& url);
+        EmptyResponse(std::string url);
       };
 
       /// The request timed out.
-      class Timeout:
-        public RequestError
+      class Timeout
+        : public RequestError
       {
       public:
         using Super = RequestError;
-        Timeout(std::string const& url,
+        Timeout(std::string url,
                 reactor::Duration const& timeout);
         ELLE_ATTRIBUTE_R(reactor::Duration, timeout);
       };
 
-      // DNS resolution failed.
-      class ResolutionFailure:
-        public RequestError
+      /// DNS resolution failed.
+      class ResolutionFailure
+        : public RequestError
       {
       public:
         using Super = RequestError;
-        ResolutionFailure(std::string const& url);
+        ResolutionFailure(std::string url);
       };
     }
   }
