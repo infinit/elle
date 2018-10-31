9c49a43e1621491621886108c78d33c680f1a84b
mefyl <mefyl@infinit.sh>
2018-10-31 11:04:23 +0100

    json: Add vector conversion.

--- src/elle/json/json.hxx
+++ src/elle/json/json.hxx
@@ -1,7 +1,15 @@
+#include <elle/make-vector.hh>
+
 namespace elle
 {
   namespace json
   {
+    template <typename T>
+    Json::Json(std::vector<T> v,
+               std::enable_if_t<is_jsonifiable<T>(), bool>)
+      : boost::any(elle::make_vector(v, [] (T const& e) { return Json(e); }))
+    {}
+
     template <typename T>
     Json::Json(T&& v)
       : boost::any(std::forward<T>(v))
