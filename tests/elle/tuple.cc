2d223b01b41724c792cc85ca8a0dd421a3c15c5d
mefyl <mefyl@gruntech.org>
2019-04-09 20:03:16 +0200

    tuple: Add operators.

--- tests/elle/tuple.cc
+++ tests/elle/tuple.cc
@@ -1,6 +1,9 @@
+#include <string>
 #include <tuple>
 
 #include <elle/test.hh>
+
+using namespace std::literals;
 
 static
 void
@@ -28,9 +31,23 @@ forward()
   forward_tuple([] (Nope n) { return n; }, std::make_tuple(Nope()));
 }
 
+static
+void
+operators()
+{
+  BOOST_TEST(
+    std::tuple(1, 2) + std::tuple(3, 4) + std::tuple(5, 6) ==
+    std::tuple(9, 12));
+  BOOST_TEST(
+    std::tuple("A"s, "D"s) + std::tuple("C", "C") == std::tuple("AC", "DC"));
+  BOOST_TEST(
+    std::tuple(1, 2) - std::tuple(4, 3) == std::tuple(-3, -1));
+}
+
 ELLE_TEST_SUITE()
 {
   auto& master = boost::unit_test::framework::master_test_suite();
   master.add(BOOST_TEST_CASE(print), 0, 1);
   master.add(BOOST_TEST_CASE(forward), 0, 1);
+  master.add(BOOST_TEST_CASE(operators), 0, 1);
 }
