9ebb5691c9746c17f9b92deecb467e86bd921d35
mefyl <mefyl@gruntech.org>
2019-04-28 16:49:09 +0200

    array: Add arithmetic operators and comparisons.

--- src/elle/array.hxx
+++ src/elle/array.hxx
@@ -21,4 +21,58 @@ namespace std
     out << ']';
     return out;
   }
+
+
+#define ELLE_ARRAY_OP(Op)                               \
+  template <typename T, std::size_t N, typename S>      \
+  auto                                                  \
+  operator Op(std::array<T, N> const& v, S&& s)         \
+    -> std::array<decltype(v[0] Op s), N>               \
+  {                                                     \
+    return std::apply(                                  \
+      [&] (auto&& ... elts)                             \
+      {                                                 \
+        return std::array{elts Op s ...};               \
+      },                                                \
+      v);                                               \
+  }                                                     \
+                                                        \
+  template <typename T, typename U, std::size_t N>      \
+  auto                                                  \
+  operator Op(std::array<T, N> const& lhs,              \
+              std::array<U, N>  const& rhs)             \
+  {                                                     \
+    return std::apply(                                  \
+      [&] (auto&& ... lhse)                             \
+      {                                                 \
+        return std::apply(                              \
+          [&] (auto&& ... rhse)                         \
+          {                                             \
+            return std::array{(lhse Op rhse) ...};      \
+          }, rhs);                                      \
+      }, lhs);                                          \
+  }                                                     \
+
+  ELLE_ARRAY_OP(+);
+  ELLE_ARRAY_OP(-);
+  ELLE_ARRAY_OP(*);
+  ELLE_ARRAY_OP(/);
+  ELLE_ARRAY_OP(%);
+#undef ELLE_ARRAY_OP
+
+  /// Compare array with any tuple-like.
+  template <typename T, std::size_t S, typename U>
+  std::enable_if_t<std::tuple_size<U>::value == S, bool>
+  operator ==(std::array<T, S> const& lhs, U const& rhs)
+  {
+    return std::apply(
+      [&] (auto const& ... lhse)
+      {
+        return std::apply(
+          [&] (auto const& ... rhse)
+          {
+            return ((lhse == rhse) && ... && true);
+          }, rhs);
+      } , lhs);
+  }
 }
--- tests/elle/array.cc
+++ tests/elle/array.cc
@@ -3,6 +3,42 @@
 #include <elle/print.hh>
 #include <elle/test.hh>
 
+namespace operators
+{
+  static
+  void
+  scalar()
+  {
+    using A = std::array<int, 3>;
+    BOOST_TEST((A{0, 1, 2}) + 1 == (A{1, 2, 3}));
+    BOOST_TEST((A{0, 1, 2}) - 1 == (A{-1, 0, 1}));
+    BOOST_TEST((A{0, 1, 2}) * 2 == (A{0, 2, 4}));
+    BOOST_TEST((A{0, 2, 4}) / 2 == (A{0, 1, 2}));
+    BOOST_TEST((A{0, 5, 8}) % 2 == (A{0, 1, 0}));
+  }
+
+  static
+  void
+  array()
+  {
+    using A = std::array<int, 3>;
+    BOOST_TEST((A{0, 1, 2} + A{3, 4, 5}) == (A{3, 5, 7}));
+    BOOST_TEST((A{0, 1, 2} - A{5, 4, 3}) == (A{-5, -3, -1}));
+    BOOST_TEST((A{0, 1, 2} * A{2, 3, 4}) == (A{0, 3, 8}));
+    BOOST_TEST((A{4, 8, 16} / A{2, 1, 4}) == (A{2, 8, 4}));
+    BOOST_TEST((A{4, 8, 16} % A{2, 3, 5}) == (A{0, 2, 1}));
+  }
+}
+
+static
+void
+compare()
+{
+  auto a = std::array<int, 3>{0, 1, 2};
+  BOOST_TEST(a == std::tuple(0, 1, 2));
+  BOOST_TEST(!(a == std::tuple(0, 1, 3)));
+}
+
 static
 void
 print()
@@ -14,5 +50,13 @@ print()
 ELLE_TEST_SUITE()
 {
   auto& suite = boost::unit_test::framework::master_test_suite();
+  suite.add(BOOST_TEST_CASE(compare));
+  {
+    boost::unit_test::test_suite* op = BOOST_TEST_SUITE("operators");
+    suite.add(op);
+    using namespace operators;
+    op->add(BOOST_TEST_CASE(scalar));
+    op->add(BOOST_TEST_CASE(array));
+  }
   suite.add(BOOST_TEST_CASE(print));
 }
