4fe1f6703202ca07a6df5b09d4d70daab59f4908
mefyl <mefyl@gruntech.org>
2019-05-07 16:33:52 +0200

    find: Support `std::find_if`.

--- src/elle/find.hh
+++ src/elle/find.hh
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+
 #include <elle/BoolIterator.hh>
 
 namespace elle
@@ -7,8 +9,20 @@ namespace elle
   template <typename C, typename E>
   auto
   find(C& c, E const& e)
+    -> BoolIterator<decltype(c.find(e))>
   {
     auto it = c.find(e);
+    return BoolIterator<decltype(it)>(it, it != c.end());
+  }
+
+  template <typename C, typename F>
+  auto
+  find(C& c, F const& f)
+    -> std::enable_if_t<
+      sizeof(decltype(f(*std::begin(c)))) >= 0,
+      BoolIterator<decltype(std::find_if(std::begin(c), std::end(c), f))>>
+  {
+    auto it = std::find_if(std::begin(c), std::end(c), f);
     return BoolIterator<decltype(it)>(it, it != c.end());
   }
 }
