561a65928629185c737d365ff4f4d8c714a8aa7f
mefyl <mefyl@gruntech.org>
2019-09-15 02:38:36 +0200

    Simplify project file.

--- elle.el
+++ elle.el
@@ -1,34 +1,22 @@
 (require 'cc-mode)
 
 (defun c-lineup-C-comments-boxes (arg)
-  (let ((box (save-excursion
-               (goto-char (cdr arg))
-               (looking-at "/\\*-*\\.$"))))
-    (if box
-        0
-      (c-lineup-C-comments arg))))
+  (let ((box (save-excursion (goto-char (cdr arg))
+                             (looking-at "/\\*-*\\.$"))))
+    (if box 0 (c-lineup-C-comments arg))))
 
-(add-to-list 'c-style-alist
-             '("infinit"
-               (c-basic-offset . 2)
-               (c-comment-only-line-offset . 0)
-               (c-hanging-braces-alist     . ((substatement-open before after)))
-               (c-offsets-alist . ((topmost-intro        . 0)
-                                   (substatement         . +)
-                                   (substatement-open    . 0)
-                                   (case-label           . +)
-                                   (access-label         . -)
-                                   (inclass              . +)
-                                   (inline-open          . 0)
-                                   (c                    . c-lineup-C-comments-boxes)))))
+(add-to-list 'c-style-alist '("infinit" (c-basic-offset . 2)
+                              (c-comment-only-line-offset . 0)
+                              (c-hanging-braces-alist     . ((substatement-open before after)))
+                              (c-offsets-alist . ((topmost-intro        . 0)
+                                                  (substatement         . +)
+                                                  (substatement-open    . 0)
+                                                  (case-label           . +)
+                                                  (access-label         . -)
+                                                  (inclass              . +)
+                                                  (inline-open          . 0)
+                                                  (c                    .
+                                                                        c-lineup-C-comments-boxes)))))
 (setq c-default-style "infinit")
 
-(c++-project
- '(
-   "src"
-   )
- "_build/linux64"
- "./drake -j 4 //build"
- ""
- '()
-)
+(c++-project '("src") "_build" "../drake/src/bin/drake .. //build" "" '())
